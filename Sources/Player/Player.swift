47c0241145e3cd03b46c53bcc4c99e4af1b01cdb
Samuel Défago <defagos@gmail.com>
2022-08-16 16:51:01 +0200

    Implement basic playback

--- Sources/Player/Player.swift
+++ Sources/Player/Player.swift
@@ -20,6 +20,10 @@ public final class Player: ObservableObject {
 
     public init(items: [AVPlayerItem] = []) {
         player = AVQueuePlayer(items: items)
+        Self.statePublisher(for: player)
+            .receive(on: DispatchQueue.main)
+            .map { State(from: $0) }
+            .assign(to: &$state)
     }
 
     public convenience init(item: AVPlayerItem) {
@@ -43,5 +47,24 @@ public final class Player: ObservableObject {
     }
 
     public func play() {
+        player.play()
+    }
+
+    private static func statePublisher(for player: AVPlayer) -> AnyPublisher<PlayerState, Never> {
+        return Publishers.CombineLatest(
+            player.publisher(for: \.currentItem)
+                .map { item -> AnyPublisher<ItemState, Never> in
+                    guard let item else {
+                        return Just(.unknown)
+                            .eraseToAnyPublisher()
+                    }
+                    return ItemState.publisher(for: item)
+                }
+                .switchToLatest(),
+            player.publisher(for: \.rate)
+        )
+        .map { PlayerState(itemState: $0, rate: $1) }
+        .prepend(PlayerState(itemState: .unknown, rate: player.rate))
+        .eraseToAnyPublisher()
     }
 }
--- Sources/Player/State.swift
+++ Sources/Player/State.swift
@@ -13,5 +13,23 @@ public extension Player {
         case paused
         case ended
         case failed(error: Error)
+
+        init(from playerState: PlayerState) {
+            switch playerState.itemState {
+            case .readyToPlay:
+                self = (playerState.rate == 0) ? .paused : .playing
+            case let .failed(error: error):
+                self = .failed(error: error)
+            case .ended:
+                self = .ended
+            case .unknown:
+                self = .idle
+            }
+        }
+    }
+
+    struct PlayerState {
+        let itemState: ItemState
+        let rate: Float
     }
 }
