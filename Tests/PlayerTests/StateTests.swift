1af667f5a20c32728c2eac257f4575fcc9bef803
Samuel Défago <defagos@gmail.com>
2022-08-16 15:46:31 +0200

    Use local streams

--- Tests/PlayerTests/StateTests.swift
+++ Tests/PlayerTests/StateTests.swift
@@ -21,19 +21,19 @@ final class StateTests: XCTestCase {
     }
 
     func testInitialStateWithItem() {
-        let item = AVPlayerItem(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8")!)
+        let item = AVPlayerItem(url: TestStreams.validStreamUrl)
         let player = Player(item: item)
         expect(player.state).to(beSimilarTo(.idle))
     }
 
     func testInitialPausedState() {
-        let item = AVPlayerItem(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8")!)
+        let item = AVPlayerItem(url: TestStreams.validStreamUrl)
         let player = Player(item: item)
         expect(player.state).toEventually(beSimilarTo(.paused), timeout: .seconds(2))
     }
 
     func testInitialPlayingState() {
-        let item = AVPlayerItem(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8")!)
+        let item = AVPlayerItem(url: TestStreams.validStreamUrl)
         let player = Player(item: item)
         player.play()
         expect(player.state).toEventually(beSimilarTo(.playing), timeout: .seconds(2))
