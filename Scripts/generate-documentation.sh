b61a6ae4e5f36671b69405f79cc19fb8e4928b70
Samuel Défago <defagos@gmail.com>
2022-08-19 18:52:46 +0200

    Display documentation errors

--- Scripts/generate-documentation.sh
+++ Scripts/generate-documentation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # This script generates the project documentation and fails if issues are detected in the output
-if ! output=$(swift package generate-documentation 2> /dev/null); then
+if ! output=$(swift package generate-documentation); then
    echo "Documentation generation failed."
    exit 1
 fi
 
-if echo "$output" | grep -E 'warning|error'; then
+if echo "$output" | grep -E 'warning'; then
     echo "Issues were found when generating the documentation."
     exit 1
 else
