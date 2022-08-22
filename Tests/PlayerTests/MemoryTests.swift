89b2cdf446e215c064af769f780201af59435227
Samuel Défago <defagos@gmail.com>
2022-08-22 13:26:57 +0200

    Make publisher filter object optional for more convenient use

--- Tests/PlayerTests/MemoryTests.swift
+++ Tests/PlayerTests/MemoryTests.swift
@@ -10,6 +10,10 @@ import AVFoundation
 import Nimble
 import XCTest
 
+private extension Notification.Name {
+    static let testNotification = Notification.Name("TestNotification")
+}
+
 final class MemoryTests: XCTestCase {
     func testPlayerRelease() {
         let item = AVPlayerItem(url: TestStreams.validStreamUrl)
@@ -20,5 +24,14 @@ final class MemoryTests: XCTestCase {
             player = nil
         }
         expect(weakPlayer).to(beNil())
+    }
+
+    func testWeakPublisherObjectRelease() throws {
+        let notificationCenter = NotificationCenter.default
+        try awaitPublisher(
+            notificationCenter.weakPublisher(for: .testNotification).first()
+        ) {
+            notificationCenter.post(Notification(name: .testNotification))
+        }
     }
 }
