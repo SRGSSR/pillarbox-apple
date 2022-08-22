9afdd956105b0e656815472d56d0d0366afea570
Samuel Défago <defagos@gmail.com>
2022-08-22 09:28:22 +0200

    Check correct player release

---
+++ Tests/PlayerTests/MemoryTests.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@testable import Player
+
+import AVFoundation
+import Nimble
+import XCTest
+
+final class MemoryTests: XCTestCase {
+    func testPlayerRelease() {
+        let item = AVPlayerItem(url: TestStreams.validStreamUrl)
+        var player: Player? = Player(item: item)
+
+        weak var weakPlayer = player
+        autoreleasepool {
+            player = nil
+        }
+        expect(weakPlayer).to(beNil())
+    }
+}
