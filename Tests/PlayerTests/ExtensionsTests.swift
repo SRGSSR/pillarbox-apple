f6e4956eec3f34610ece79f3e97fdb70fe0f5a45
Samuel Défago <defagos@gmail.com>
2022-08-25 08:35:33 +0200

    Gather extensions

--- Tests/PlayerTests/ExtensionsTests.swift
+++ Tests/PlayerTests/ExtensionsTests.swift
@@ -11,6 +11,16 @@ import Circumspect
 import Nimble
 import XCTest
 
+final class ClampedTests: XCTestCase {
+    func testClamped() {
+        expect((-1).clamped(to: 0...1)).to(equal(0))
+        expect(0.clamped(to: 0...1)).to(equal(0))
+        expect(0.5.clamped(to: 0...1)).to(equal(0.5))
+        expect(1.clamped(to: 0...1)).to(equal(1))
+        expect(2.clamped(to: 0...1)).to(equal(1))
+    }
+}
+
 final class NotificationCenterPublisherTests: XCTestCase {
     func testWithoutObject() throws {
         let notificationCenter = NotificationCenter.default
