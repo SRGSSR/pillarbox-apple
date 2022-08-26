df70c3950a3549ed46295945a717c7fea4d9b5b0
Samuel Défago <defagos@gmail.com>
2022-08-26 07:27:30 +0200

    Use more readable formalism
    var? for .some(var) is quite misleading IMHO

--- Sources/Circumspect/Matchers.swift
+++ Sources/Circumspect/Matchers.swift
@@ -13,11 +13,11 @@ public func equal<T>(_ expectedValue: T?, by areEquivalent: @escaping (T, T) -> 
     Predicate.define("equal <\(stringify(expectedValue))>") { actualExpression, message in
         let actualValue = try actualExpression.evaluate()
         switch (expectedValue, actualValue) {
-        case (nil, _?):
+        case (.none, .some(_)):
             return PredicateResult(status: .fail, message: message.appendedBeNilHint())
-        case (_, nil):
+        case (_, .none):
             return PredicateResult(status: .fail, message: message)
-        case let (expected?, actual?):
+        case let (.some(expected), .some(actual)):
             let matches = areEquivalent(expected, actual)
             return PredicateResult(bool: matches, message: message)
         }
@@ -29,11 +29,11 @@ public func equalDiff<T>(_ expectedValue: T?, by areEquivalent: @escaping (T, T)
     return Predicate.define("equal <\(stringify(expectedValue))>") { actualExpression, message in
         let actualValue = try actualExpression.evaluate()
         switch (expectedValue, actualValue) {
-        case (nil, _?):
+        case (.none, .some(_)):
             return PredicateResult(status: .fail, message: message.appendedBeNilHint())
-        case (_, nil):
+        case (_, .none):
             return PredicateResult(status: .fail, message: message)
-        case let (expected?, actual?):
+        case let (.some(expected), .some(actual)):
             return PredicateResult(
                 bool: areEquivalent(expected, actual),
                 message: ExpectationMessage.fail(diff(expectedValue, actualValue).joined(separator: ", "))
