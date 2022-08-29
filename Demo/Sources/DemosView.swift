e00eba61032158f1fac28c23739538bf3b8e980b
Samuel Défago <defagos@gmail.com>
2022-08-29 15:10:37 +0200

    Add on-demand audio example

--- Demo/Sources/DemosView.swift
+++ Demo/Sources/DemosView.swift
@@ -36,6 +36,11 @@ struct DemosView: View {
             url: URL(string: "https://rtsc3video.akamaized.net/hls/live/2042837/c3video/3/playlist.m3u8")!
         ),
         Media(
+            id: "urn:rts:audio:13306836",
+            title: "On en parle",
+            url: URL(string: "https://rts-aod-dd.akamaized.net/ww/13306839/63cc2653-8305-3894-a448-108810b553ef.mp3")!
+        ),
+        Media(
             id: "urn:rts:audio:3262363",
             title: "La 1ère",
             url: URL(string: "http://stream.srg-ssr.ch/m/la-1ere/mp3_128")!
