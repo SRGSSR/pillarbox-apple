3ecdd20951b1c9d367d497c047e05ef6785620ed
Samuel Défago <defagos@gmail.com>
2022-08-31 15:25:43 +0200

    Improve documentation

--- Sources/Player/PlaybackProgress.swift
+++ Sources/Player/PlaybackProgress.swift
@@ -10,6 +10,8 @@ import Foundation
 public struct PlaybackProgress: Equatable {
     /// The current progress as a value in 0...1.
     public var value: Float
-    /// Set to `true` when updating progress interactively, e.g. via a seek bar.
+    /// This value should be set to `true` when the value is updated interactively, e.g. via a seek bar. This informs
+    /// the player that internal time updates must be suspended so that the interactive value is not updated to another
+    /// value while interaction still takes place.
     public var isInteracting: Bool
 }
