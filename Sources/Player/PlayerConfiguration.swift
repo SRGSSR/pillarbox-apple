d577d4027e9203c520e442088382d6bd0c063086
Samuel Défago <defagos@gmail.com>
2022-09-03 11:59:19 +0200

    Improve documentation

--- Sources/Player/PlayerConfiguration.swift
+++ Sources/Player/PlayerConfiguration.swift
@@ -8,14 +8,14 @@ import CoreMedia
 
 /// Player configuration.
 public struct PlayerConfiguration {
-    /// The interval at which the player internally ticks.
+    /// The interval at which the player internally ticks. Defaults to 1 second, minimum is 1/10th second.
     public var tick: CMTime {
         get { _tick }
         set { _tick = CMTimeMaximum(newValue, CMTime(value: 1, timescale: 10)) }
     }
 
     /// The minimum duration below which a DVR window is collapsed to zero (so that the stream
-    /// is effectively seen as a livestream without DVR).
+    /// is effectively seen as a livestream without DVR). Defaults to `.zero`.
     public var dvrThreshold: CMTime {
         get { _dvrThreshold }
         set { _dvrThreshold = CMTimeMaximum(newValue, .zero) }
