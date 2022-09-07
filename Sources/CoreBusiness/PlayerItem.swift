242bd7844725d913429a7b0b83d60de33b4b007e
Samuel Défago <defagos@gmail.com>
2022-09-07 08:08:47 +0200

    Provide helper to create an asset with an associated delegate

--- Sources/CoreBusiness/PlayerItem.swift
+++ Sources/CoreBusiness/PlayerItem.swift
@@ -14,11 +14,12 @@ public extension AVPlayerItem {
     convenience init(urn: String, environment: Environment = .production) {
         let encodedUrn = urn.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
         let asset = AVURLAsset(url: URL(string: "urn://mediacomposition?urn=\(encodedUrn)")!)
+        self.init(asset: asset, associatedDelegate: AssetResourceLoaderDelegate(), queue: .global(qos: .userInteractive))
+    }
 
-        let resourceLoaderDelegate = AssetResourceLoaderDelegate()
-        objc_setAssociatedObject(asset, &resourceLoaderDelegateKey, resourceLoaderDelegate, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        asset.resourceLoader.setDelegate(resourceLoaderDelegate, queue: .global(qos: .userInitiated))
-
+    convenience init(asset: AVURLAsset, associatedDelegate delegate: AVAssetResourceLoaderDelegate, queue: DispatchQueue) {
+        objc_setAssociatedObject(asset, &resourceLoaderDelegateKey, delegate, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        asset.resourceLoader.setDelegate(delegate, queue: queue)
         self.init(asset: asset)
     }
 }
