113efa768be8bbab3a8c14ff0c4945d519a235a3
Samuel Défago <defagos@gmail.com>
2022-09-14 09:00:56 +0200

    Test buffering with queued items

---
+++ Tests/PlayerTests/BufferingPublisherQueueTests.swift
@@ -0,0 +1,42 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@testable import Player
+
+import AVFoundation
+import Circumspect
+import XCTest
+
+final class BufferingPublisherQueueTests: XCTestCase {
+    func testItems() {
+        let item1 = AVPlayerItem(url: TestStreams.shortOnDemandUrl)
+        let item2 = AVPlayerItem(url: TestStreams.onDemandUrl)
+        let player = AVQueuePlayer(items: [item1, item2])
+        expectEqualPublished(
+            // Next media can be buffered in advance
+            values: [false, true, false],
+            from: player.bufferingPublisher(),
+            during: 4
+        ) {
+            player.play()
+        }
+    }
+
+    func testFailure() {
+        let item1 = AVPlayerItem(url: TestStreams.shortOnDemandUrl)
+        let item2 = AVPlayerItem(url: TestStreams.unavailableUrl)
+        let item3 = AVPlayerItem(url: TestStreams.onDemandUrl)
+        let player = AVQueuePlayer(items: [item1, item2, item3])
+        expectEqualPublished(
+            // Next media cannot be buffered in advance because of the failure
+            values: [false, true, false, true, false],
+            from: player.bufferingPublisher(),
+            during: 4
+        ) {
+            player.play()
+        }
+    }
+}
