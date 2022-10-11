e80067b92525d3135e297776919d7bea3e34887f
Samuel Défago <defagos@gmail.com>
2022-10-11 11:14:26 +0200

    Prevent buffering to force paused livestreams to resume at the live edge

--- Sources/CoreBusiness/PlayerItem.swift
+++ Sources/CoreBusiness/PlayerItem.swift
@@ -16,13 +16,16 @@ public extension AVPlayerItem {
     /// Create a player item from a URN played in the specified environment.
     /// - Parameters:
     ///   - urn: The URN to play.
-    ///   - automaticallyLoadedAssetKeys: The asset keys to load before the item is ready to play, ensuring they are
-    ///     available, but increasing startup time.
+    ///   - automaticallyLoadedAssetKeys: The asset keys to load before the item is ready to play.
     ///   - environment: The environment which the URN is played from.
-    convenience init(urn: String, automaticallyLoadedAssetKeys: [String], environment: Environment = .production) {
+    convenience init(urn: String, automaticallyLoadedAssetKeys keys: [String]? = nil, environment: Environment = .production) {
         let asset = AVURLAsset(url: URLCoding.encodeUrl(fromUrn: urn))
         asset.resourceLoader.setDelegate(kAssetResourceLoaders[environment], queue: .global(qos: .userInitiated))
-        self.init(asset: asset, automaticallyLoadedAssetKeys: automaticallyLoadedAssetKeys)
+        self.init(asset: asset, automaticallyLoadedAssetKeys: keys)
+
+        // Limit buffering. This ensures that paused livestreams without DVR cannot be played from buffer. Instead
+        // force the player to return to the live edge when re-buffering live content.
         automaticallyPreservesTimeOffsetFromLive = true
+        preferredForwardBufferDuration = 1
     }
 }
