1598b881dbf7ef84417c48e41e7c7f5920b56827
Samuel Défago <defagos@gmail.com>
2022-10-20 15:46:15 +0200

    Add accumulators

--- Sources/Core/Publishers.swift
+++ Sources/Core/Publishers.swift
@@ -7,6 +7,56 @@
 import Combine
 import Foundation
 
+public extension Publishers {
+    /// Accumulate the results of a list of publishers and deliver them as a stream of arrays containing the latest
+    /// values in publisher order. The first array is emitted once all publishers have at least emitted a value once.
+    /// - Parameter publishers: The publishers to accumulate
+    /// - Returns: The publisher
+    static func AccumulateLatestMany<Upstream>(_ publishers: Upstream...) -> AnyPublisher<[Upstream.Output], Upstream.Failure> where Upstream: Publisher {
+        AccumulateLatestMany(publishers)
+    }
+
+    /// Accumulate the results of a list of publishers and deliver them as a stream of arrays containing the latest
+    /// values in publisher order. The first array is emitted once all publishers have at least emitted a value once.
+    /// - Parameter publishers: The publishers to accumulate
+    /// - Returns: The publisher
+    static func AccumulateLatestMany<Upstream, S>(_ publishers: S) -> AnyPublisher<[Upstream.Output], Upstream.Failure> where Upstream: Publisher, S: Swift.Sequence, S.Element == Upstream {
+        let publishersArray = Array(publishers)
+        switch publishersArray.count {
+        case 0:
+            return Just([])
+                .setFailureType(to: Upstream.Failure.self)
+                .eraseToAnyPublisher()
+        case 1:
+            return publishersArray[0]
+                .map { [$0] }
+                .eraseToAnyPublisher()
+        case 2:
+            return Publishers.CombineLatest(publishersArray[0], publishersArray[1])
+                .map { t1, t2 in
+                    [t1, t2]
+                }
+                .eraseToAnyPublisher()
+        case 3:
+            return Publishers.CombineLatest3(publishersArray[0], publishersArray[1], publishersArray[2])
+                .map { t1, t2, t3 in
+                    [t1, t2, t3]
+                }
+                .eraseToAnyPublisher()
+        default:
+            let half = publishersArray.count / 2
+            return Publishers.CombineLatest(
+                AccumulateLatestMany(Array(publishersArray[0..<half])),
+                AccumulateLatestMany(Array(publishersArray[half..<publishersArray.count]))
+            )
+            .map { array1, array2 in
+                array1 + array2
+            }
+            .eraseToAnyPublisher()
+        }
+    }
+}
+
 public extension NotificationCenter {
     /// The usual notification publisher retains the filter object, potentially creating cycles. The following
     /// publisher avoids this issue while still only observing the filter object (if any), even after it is
@@ -57,4 +107,4 @@ public extension Publisher {
     func weakCapture<T: AnyObject>(_ other: T?) -> AnyPublisher<(Output, T), Failure> {
         weakCapture(other, at: \T.self)
     }
- }
+}
