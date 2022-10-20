08ff2c36d64f1a5ff6eb0445d23cd58376c1a99b
Samuel Défago <defagos@gmail.com>
2022-10-20 09:30:56 +0200

    Improve similarity support

--- Tests/CircumspectTests/Tools.swift
+++ Tests/CircumspectTests/Tools.swift
@@ -4,8 +4,17 @@
 //  License information is available from the LICENSE file.
 //
 
+import Circumspect
 import Foundation
 
 extension Notification.Name {
     static let testNotification = Notification.Name("TestNotification")
+}
+
+struct NamedPerson: Similar {
+    static func ~= (lhs: NamedPerson, rhs: NamedPerson) -> Bool {
+        return lhs.name.localizedCaseInsensitiveContains(rhs.name)
+    }
+
+    let name: String
 }
