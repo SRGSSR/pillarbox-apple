58cffc23d1dbaa6ca11eac9117a5a722173a079f
Samuel Défago <defagos@gmail.com>
2022-10-24 11:44:27 +0200

    Fix resource loading error reporting (#118)

--- Sources/Player/Errors.swift
+++ Sources/Player/Errors.swift
@@ -22,3 +22,38 @@ extension Error {
         return NSError(domain: error.domain, code: error.code, userInfo: userInfo)
     }
 }
+
+extension NSError {
+    private static func domain(from error: Error) -> String {
+        (error as NSError).domain
+    }
+
+    private static func code(from error: Error) -> Int {
+        (error as NSError).code
+    }
+
+    private static func userInfo(from error: Error) -> [String: Any] {
+        // Returning (error as NSError).userInfo does not work
+        let bridgedError = error as NSError
+        var userInfo = [String: Any]()
+        userInfo[NSLocalizedDescriptionKey] = bridgedError.localizedDescription
+        userInfo[NSLocalizedFailureReasonErrorKey] = bridgedError.localizedFailureReason
+        userInfo[NSLocalizedRecoverySuggestionErrorKey] = bridgedError.localizedRecoverySuggestion
+        userInfo[NSHelpAnchorErrorKey] = bridgedError.helpAnchor
+        return userInfo
+    }
+
+    /// Convert any error to a true `NSError`. This is not the same as bridging an `Error` to `NSError` (which is
+    /// always possible but is not equivalent).
+    /// - Parameter error: The error to convert.
+    /// - Returns: The converted error (or the provided error if already an `NSError`).
+    static func error(from error: Error?) -> NSError? {
+        guard let error else { return nil }
+        guard type(of: error) != NSError.self else { return error as NSError }
+        return Self(
+            domain: domain(from: error),
+            code: code(from: error),
+            userInfo: userInfo(from: error)
+        )
+    }
+}
--- Tests/PlayerTests/ErrorsTests.swift
+++ Tests/PlayerTests/ErrorsTests.swift
@@ -10,32 +10,61 @@ import Nimble
 import XCTest
 
 final class ErrorsTests: XCTestCase {
-    private enum TestError: LocalizedError {
-        case message
+    private enum EnumError: LocalizedError {
+        case someError
 
         var errorDescription: String? {
-            "Message"
+            "Enum error description"
+        }
+
+        var failureReason: String? {
+            "Enum failure reason"
+        }
+
+        var recoverySuggestion: String? {
+            "Enum recovery suggestion"
+        }
+
+        var helpAnchor: String? {
+            "Enum help anchor"
         }
     }
 
     func testLocalizedNSError() {
         let error = NSError(domain: "domain", code: 1012, userInfo: [
-            NSLocalizedDescriptionKey: "Message"
+            NSLocalizedDescriptionKey: "Error description"
         ])
         expect(error.localized() as NSError).to(equal(error))
     }
 
     func testNonLocalizedNSError() {
         let error = NSError(domain: "domain", code: 1012, userInfo: [
-            "NSDescription": "Message"
+            "NSDescription": "Error description"
         ])
         let expectedError = NSError(domain: "domain", code: 1012, userInfo: [
-            NSLocalizedDescriptionKey: "Message"
+            NSLocalizedDescriptionKey: "Error description"
         ])
         expect(error.localized() as NSError).to(equal(expectedError))
     }
 
     func testSwiftError() {
-        expect(TestError.message.localized() as NSError).to(equal(TestError.message as NSError))
+        expect(EnumError.someError.localized() as NSError).to(equal(EnumError.someError as NSError))
+    }
+
+    func testNSErrorFromNSError() {
+        let error = NSError(domain: "domain", code: 1012, userInfo: [
+            NSLocalizedDescriptionKey: "Error description",
+            NSLocalizedFailureReasonErrorKey: "Failure reason",
+            NSUnderlyingErrorKey: NSError(domain: "inner.domain", code: 2024)
+        ])
+        expect(NSError.error(from: error)).to(equal(error))
+    }
+
+    func testNSErrorFromSwiftError() {
+        let error = NSError.error(from: EnumError.someError)!
+        expect(error.localizedDescription).to(equal("Enum error description"))
+        expect(error.localizedFailureReason).to(equal("Enum failure reason"))
+        expect(error.localizedRecoverySuggestion).to(equal("Enum recovery suggestion"))
+        expect(error.helpAnchor).to(equal("Enum help anchor"))
     }
 }
