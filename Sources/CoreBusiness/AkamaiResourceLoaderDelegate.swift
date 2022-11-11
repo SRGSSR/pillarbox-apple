8c8ba146a3f52dc2e940afc1981f0bbd7c45de6b
Samuel Défago <defagos@gmail.com>
2022-11-11 07:15:58 +0100

    Add Akamai resource loader delegate

---
+++ Sources/CoreBusiness/AkamaiResourceLoaderDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import AVFoundation
+
+final class AkamaiResourceLoaderDelegate: NSObject, AVAssetResourceLoaderDelegate {
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest
+    ) -> Bool {
+        true
+    }
+
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        shouldWaitForRenewalOfRequestedResource renewalRequest: AVAssetResourceRenewalRequest
+    ) -> Bool {
+        true
+    }
+}
