9254b58a3ab15bcff19052b2ec520dcda4516e06
Samuel Défago <defagos@gmail.com>
2022-11-22 11:31:06 +0100

    Fix linting issues

--- Demo/Sources/AppDelegate.swift
+++ Demo/Sources/AppDelegate.swift
@@ -14,20 +14,20 @@ import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     private var cancellables = Set<AnyCancellable>()
-    
+
     // swiftlint:disable:next discouraged_optional_collection
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         try? AVAudioSession.sharedInstance().setCategory(.playback)
         configureShowTime()
         return true
     }
-    
+
     private func configureShowTime() {
         UserDefaults.standard.publisher(for: \.presenterModeEnabled)
             .receiveOnMainThread()
-            .sink(receiveValue: { isEnabled in
+            .sink { isEnabled in
                 ShowTime.enabled = isEnabled ? .always : .never
-            })
+            }
             .store(in: &cancellables)
     }
 }
