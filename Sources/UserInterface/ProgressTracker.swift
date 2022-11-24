47d25d48083d9e1c6ec4db0c7ade9b2a209a6b57
Samuel Défago <defagos@gmail.com>
2022-11-24 09:03:39 +0100

    Inhibit updates during interaction

--- Sources/UserInterface/ProgressTracker.swift
+++ Sources/UserInterface/ProgressTracker.swift
@@ -16,66 +16,36 @@ import SwiftUI
 public final class ProgressTracker: ObservableObject {
     /// The player to attach. Use `View.bind(_:to:)` in SwiftUI code.
     @Published public var player: Player?
-
-    /// Must be set to `true` during interactions updating the progress (e.g. while holding a slider knob controlling
-    /// the progress).
-    @Published public var isInteracting: Bool = false
-
-    @Published private var _progress: Float? {
-        willSet {
-            guard let player, let _progress, let time = Self.time(forProgress: _progress, in: player.timeRange) else {
-                return
-            }
-            player.seek(to: time)
-        }
-    }
-
-    /// The current playback progress.
-    public var progress: Float {
-        get {
-            _progress ?? 0
-        }
-        set {
-            _progress = newValue
-        }
-    }
+    @Published public var isInteracting = false
+    @Published public var progress: Float?
 
     /// Range for progress values.
     public var range: ClosedRange<Float> {
-        _progress != nil ? 0...1 : 0...0
+        progress != nil ? 0...1 : 0...0
     }
 
     /// Create a tracker updating its progress at the specified interval.
     /// - Parameter interval: The interval at which progress must be updated.
-    public init(interval: CMTime) {
-        $player
-            .map { [$isInteracting] player -> AnyPublisher<Float?, Never> in
+    public init(interval: CMTime, seekBehavior: SeekBehavior = .deferred) {
+        Publishers.CombineLatest($player, $isInteracting)
+            .map { player, isInteracting -> AnyPublisher<Float?, Never> in
+                guard !isInteracting else {
+                    return Empty(completeImmediately: false).eraseToAnyPublisher()
+                }
                 guard let player else {
                     return Just(nil).eraseToAnyPublisher()
                 }
-                return Publishers.CombineLatest3(
-                    Self.progressPublisher(for: player, interval: interval),
-                    $isInteracting,
-                    player.$isSeeking
+                return Publishers.CombineLatest(
+                    player.periodicTimePublisher(forInterval: interval, queue: .global(qos: .default)),
+                    player.$timeRange
                 )
-                .filter { !$0.1 && !$0.2 }
-                .map(\.0)
+                .map { Self.progress(for: $0, in: $1) }
                 .eraseToAnyPublisher()
             }
             .switchToLatest()
+            .print()
             .receiveOnMainThread()
-            .assign(to: &$_progress)
-    }
-
-    private static func progressPublisher(for player: Player, interval: CMTime) -> AnyPublisher<Float?, Never> {
-        Publishers.CombineLatest(
-            player.periodicTimePublisher(forInterval: interval, queue: .global(qos: .default)),
-            player.$timeRange
-        )
-        .map { time, timeRange in
-            progress(for: time, in: timeRange)
-        }
-        .eraseToAnyPublisher()
+            .assign(to: &$progress)
     }
 
     private static func progress(for time: CMTime, in timeRange: CMTimeRange) -> Float? {
