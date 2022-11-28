ecebb3414a77db98efa4b70eb271cd7651209404
Samuel Défago <defagos@gmail.com>
2022-11-28 19:50:55 +0100

    Add failing stream type test

--- Tests/PlayerTests/PlayerStreamTypeTests.swift
+++ Tests/PlayerTests/PlayerStreamTypeTests.swift
@@ -33,4 +33,10 @@ final class PlayerStreamTypeTests: XCTestCase {
         let player = Player(item: item)
         expect(player.streamType).toEventually(equal(.dvr))
     }
+
+    func testStabilityAtStart() {
+        let item = PlayerItem(url: Stream.onDemand.url)
+        let player = Player(item: item)
+        expect(player.streamType).toNever(equal(.dvr), pollInterval: .microseconds(1))
+    }
 }
