9e161dbb29172411eb0ca02970e8840d1ba11fef
Samuel Défago <defagos@gmail.com>
2022-11-28 20:06:57 +0100

    Test stream type stability without automatically loaded asset keys

--- Tests/PlayerTests/PlayerStreamTypeTests.swift
+++ Tests/PlayerTests/PlayerStreamTypeTests.swift
@@ -39,4 +39,10 @@ final class PlayerStreamTypeTests: XCTestCase {
         let player = Player(item: item)
         expect(player.streamType).toNever(equal(.dvr), pollInterval: .microseconds(1))
     }
+
+    func testStabilityAtStartWithoutAutomaticallyLoadedAssetKeys() {
+        let item = PlayerItem(url: Stream.onDemand.url, automaticallyLoadedAssetKeys: [])
+        let player = Player(item: item)
+        expect(player.streamType).toNever(equal(.dvr), pollInterval: .microseconds(1))
+    }
 }
