0d7b4120f7067ccd604b544dfa6478fdc81b4710
Walid Kayhal <walid.kayhal@rts.ch>
2023-01-06 15:25:28 +0100

    Add Asset equality tests

--- Tests/PlayerTests/AssetTests.swift
+++ Tests/PlayerTests/AssetTests.swift
@@ -13,6 +13,28 @@ import Nimble
 import XCTest
 
 final class AssetTests: XCTestCase {
+    func testSimpleEquality() {
+        expect(Asset.simple(url: Stream.dvr.url)).to(equal(.simple(url: Stream.dvr.url)))
+        expect(Asset.simple(url: Stream.live.url)).notTo(equal(.simple(url: Stream.dvr.url)))
+        expect(Asset.simple(url: Stream.live.url)).notTo(equal(.custom(url: Stream.live.url, delegate: LoadingResourceLoaderDelegate())))
+    }
+
+    func testCustomEquality() {
+        let delegate1 = LoadingResourceLoaderDelegate()
+        expect(Asset.custom(url: Stream.dvr.url, delegate: delegate1)).to(equal(Asset.custom(url: Stream.dvr.url, delegate: delegate1)))
+        let delegate2 = LoadingResourceLoaderDelegate()
+        expect(Asset.custom(url: Stream.dvr.url, delegate: delegate1)).notTo(equal(Asset.custom(url: Stream.dvr.url, delegate: delegate2)))
+        expect(Asset.custom(url: Stream.live.url, delegate: delegate1)).notTo(equal(Asset.custom(url: Stream.dvr.url, delegate: delegate1)))
+    }
+
+    func testEncryptedEquality() {
+        let delegate1 = DummyContentKeySessionDelegate()
+        expect(Asset.encrypted(url: Stream.dvr.url, delegate: delegate1)).to(equal(Asset.encrypted(url: Stream.dvr.url, delegate: delegate1)))
+        let delegate2 = DummyContentKeySessionDelegate()
+        expect(Asset.encrypted(url: Stream.dvr.url, delegate: delegate1)).notTo(equal(Asset.encrypted(url: Stream.dvr.url, delegate: delegate2)))
+        expect(Asset.encrypted(url: Stream.live.url, delegate: delegate1)).notTo(equal(Asset.encrypted(url: Stream.dvr.url, delegate: delegate1)))
+    }
+
     func testNativePlayerItem() {
         let item = Asset.simple(url: Stream.onDemand.url).playerItem()
         _ = AVPlayer(playerItem: item)
