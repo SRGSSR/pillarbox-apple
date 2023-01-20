5b6595474b8daac8e5d14b3ec76f0cd7c5ed87a8
Samuel Défago <defagos@gmail.com>
2023-01-20 14:57:56 +0100

    Prepare tests for navigation checks

--- Tests/PlayerTests/NavigationTests.swift
+++ Tests/PlayerTests/NavigationTests.swift
@@ -6,56 +6,96 @@
 
 @testable import Player
 
+import CoreMedia
 import Nimble
 import XCTest
 
 final class NavigationTests: XCTestCase {
     func testCanReturnForOnDemandAtBeginning() {
-
+        let item = PlayerItem.simple(url: Stream.onDemand.url)
+        let player = Player(item: item)
+        expect(player.canReturnToPrevious()).to(beTrue())
     }
 
     func testCanReturnForOnDemandNotAtBeginning() {
+        let item = PlayerItem.simple(url: Stream.onDemand.url)
+        let player = Player(item: item)
+        expect(player.streamType).toEventually(equal(.onDemand))
 
+        waitUntil { done in
+            player.seek(to: CMTime(value: 5, timescale: 1), toleranceBefore: .zero, toleranceAfter: .zero) { finished in
+                done()
+            }
+        }
+
+        expect(player.canReturnToPrevious()).to(beTrue())
     }
 
     func testCanReturnForLiveWithPreviousItem() {
-
+        let item1 = PlayerItem.simple(url: Stream.onDemand.url)
+        let item2 = PlayerItem.simple(url: Stream.live.url)
+        let player = Player(items: [item1, item2])
+        player.advanceToNextItem()
+        expect(player.canReturnToPrevious()).to(beTrue())
     }
 
     func testCannotReturnForLiveWithoutPreviousItem() {
-
+        let item = PlayerItem.simple(url: Stream.live.url)
+        let player = Player(item: item)
+        expect(player.canReturnToPrevious()).to(beFalse())
     }
 
     func testCanReturnForDvrWithPreviousItem() {
-
+        let item1 = PlayerItem.simple(url: Stream.onDemand.url)
+        let item2 = PlayerItem.simple(url: Stream.dvr.url)
+        let player = Player(items: [item1, item2])
+        player.advanceToNextItem()
+        expect(player.canReturnToPrevious()).to(beTrue())
     }
 
     func testCannotReturnForDvrWithoutPreviousItem() {
-
+        let item = PlayerItem.simple(url: Stream.dvr.url)
+        let player = Player(item: item)
+        expect(player.canReturnToPrevious()).to(beFalse())
     }
 
     func testCanAdvanceForOnDemandWithNextItem() {
-
+        let item1 = PlayerItem.simple(url: Stream.onDemand.url)
+        let item2 = PlayerItem.simple(url: Stream.live.url)
+        let player = Player(items: [item1, item2])
+        expect(player.canAdvanceToNext()).to(beTrue())
     }
 
     func testCannotAdvanceForOnDemandWithoutNextItem() {
-
+        let item = PlayerItem.simple(url: Stream.onDemand.url)
+        let player = Player(item: item)
+        expect(player.canAdvanceToNext()).to(beFalse())
     }
 
     func testCanAdvanceForLiveWithNextItem() {
-
+        let item1 = PlayerItem.simple(url: Stream.live.url)
+        let item2 = PlayerItem.simple(url: Stream.onDemand.url)
+        let player = Player(items: [item1, item2])
+        expect(player.canAdvanceToNext()).to(beTrue())
     }
 
     func testCannotAdvanceForLiveWithoutNextItem() {
-
+        let item = PlayerItem.simple(url: Stream.live.url)
+        let player = Player(item: item)
+        expect(player.canAdvanceToNext()).to(beFalse())
     }
 
     func testCanAdvanceForDvrWithNextItem() {
-
+        let item1 = PlayerItem.simple(url: Stream.dvr.url)
+        let item2 = PlayerItem.simple(url: Stream.onDemand.url)
+        let player = Player(items: [item1, item2])
+        expect(player.canAdvanceToNext()).to(beTrue())
     }
 
     func testCannotAdvanceForDvrWithoutNextItem() {
-
+        let item = PlayerItem.simple(url: Stream.dvr.url)
+        let player = Player(item: item)
+        expect(player.canAdvanceToNext()).to(beFalse())
     }
 
     func testReturnForOnDemandAtBeginning() {
