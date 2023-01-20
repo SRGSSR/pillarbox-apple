3a6fb0bfafe58fb47b14eed81d8ac5d2574afb7b
Samuel Défago <defagos@gmail.com>
2023-01-20 14:35:49 +0100

    Prepare tests for new API

---
+++ Tests/PlayerTests/NavigationTests.swift
@@ -0,0 +1,108 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@testable import Player
+
+import Nimble
+import XCTest
+
+final class NavigationTests: XCTestCase {
+    func testCanReturnForOnDemandAtBeginning() {
+
+    }
+
+    func testCanReturnForOnDemandNotAtBeginning() {
+
+    }
+
+    func testCanReturnForLiveWithPreviousItem() {
+
+    }
+
+    func testCannotReturnForLiveWithoutPreviousItem() {
+
+    }
+
+    func testCanReturnForDvrWithPreviousItem() {
+
+    }
+
+    func testCannotReturnForDvrWithoutPreviousItem() {
+
+    }
+
+    func testCanAdvanceForOnDemandWithNextItem() {
+
+    }
+
+    func testCannotAdvanceForOnDemandWithoutNextItem() {
+
+    }
+
+    func testCanAdvanceForLiveWithNextItem() {
+
+    }
+
+    func testCannotAdvanceForLiveWithoutNextItem() {
+
+    }
+
+    func testCanAdvanceForDvrWithNextItem() {
+
+    }
+
+    func testCannotAdvanceForDvrWithoutNextItem() {
+
+    }
+
+    func testReturnForOnDemandAtBeginning() {
+
+    }
+
+    func testReturnForOnDemandNotAtBeginning() {
+
+    }
+
+    func testReturnForLiveWithPreviousItem() {
+
+    }
+
+    func testReturnForLiveWithoutPreviousItem() {
+
+    }
+
+    func testReturnForDvrWithPreviousItem() {
+
+    }
+
+    func testReturnForDvrWithoutPreviousItem() {
+
+    }
+
+    func testAdvanceForOnDemandWithNextItem() {
+
+    }
+
+    func testAdvanceForOnDemandWithoutNextItem() {
+
+    }
+
+    func testAdvanceForLiveWithNextItem() {
+
+    }
+
+    func testAdvanceForLiveWithoutNextItem() {
+
+    }
+
+    func testAdvanceForDvrWithNextItem() {
+
+    }
+
+    func testAdvanceForDvrWithoutNextItem() {
+
+    }
+}
