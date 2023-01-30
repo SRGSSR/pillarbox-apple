a0bd103b53f401d5d0fa85afc3665f67e38ee735
Samuel Défago <defagos@gmail.com>
2023-01-30 19:56:49 +0100

    Avoid pattern matching operator overload

--- Sources/Circumspect/Similarity.swift
+++ Sources/Circumspect/Similarity.swift
@@ -7,38 +7,40 @@
 import Foundation
 import Nimble
 
+infix operator ~~ : ComparisonPrecedence
+
 /// Defines similarity for types not conforming to `Equatable` and which need to be meaningfully compared in tests.
 public protocol Similar {
     /// Must return `true` iff the two sides can be considered to be similar.
-    static func ~= (lhs: Self, rhs: Self) -> Bool
+    static func ~~ (lhs: Self, rhs: Self) -> Bool
 }
 
 /// Nimble matcher displaying similarity differences in a friendly way.
 public func equalDiff<T: Similar>(_ expectedValue: T?) -> Predicate<T> {
-    equalDiff(expectedValue, by: ~=)
+    equalDiff(expectedValue, by: ~~)
 }
 
 /// Nimble matcher displaying mismatches as differences.
 public func equal<T: Similar>(_ expectedValue: T?) -> Predicate<T> {
-    equal(expectedValue, by: ~=)
+    equal(expectedValue, by: ~~)
 }
 
 extension Array: Similar where Element: Similar {
-    public static func ~= (lhs: Self, rhs: Self) -> Bool {
+    public static func ~~ (lhs: Self, rhs: Self) -> Bool {
         guard lhs.count == rhs.count else { return false }
-        return zip(lhs, rhs).allSatisfy(~=)
+        return zip(lhs, rhs).allSatisfy(~~)
     }
 }
 
 extension Optional: Similar where Wrapped: Similar {
-    public static func ~= (lhs: Wrapped?, rhs: Wrapped?) -> Bool {
+    public static func ~~ (lhs: Wrapped?, rhs: Wrapped?) -> Bool {
         switch (lhs, rhs) {
         case (.none, .none):
             return true
         case (.none, .some), (.some, .none):
             return false
         case let (.some(lhs), .some(rhs)):
-            return lhs ~= rhs
+            return lhs ~~ rhs
         }
     }
 }
