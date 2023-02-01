9b7b5d7bdc7c660fc28331a620bf6d273615282b
Samuel Défago <defagos@gmail.com>
2023-02-01 15:47:49 +0100

    Improve test reliability

--- Tests/PlayerTests/NowPlayingInfoCurrentTimePublisherTests.swift
+++ Tests/PlayerTests/NowPlayingInfoCurrentTimePublisherTests.swift
@@ -36,13 +36,11 @@ final class NowPlayingInfoCurrentTimePublisherTests: XCTestCase {
     func testInitialSeek() {
         let item = AVPlayerItem(url: Stream.onDemand.url)
         let player = QueuePlayer(playerItem: item)
-        expectAtLeastPublished(
-            values: [CMTime(value: 5, timescale: 1)],
-            from: player.nowPlayingInfoCurrentTimePublisher(interval: CMTime(value: 1, timescale: 2), queue: .main),
-            to: beClose(within: 0.1)
-        ) {
+        let publisher = player.nowPlayingInfoCurrentTimePublisher(interval: CMTime(value: 1, timescale: 1000), queue: .main)
+        let times = collectOutput(from: publisher, during: 3) {
             player.seek(to: CMTime(value: 5, timescale: 1), toleranceBefore: .zero, toleranceAfter: .zero, completionHandler: { _ in })
         }
+        expect(times.contains(CMTime(value: 5, timescale: 1))).to(beTrue())
     }
 
     func testSeekDuringPlayback() {
@@ -59,6 +57,7 @@ final class NowPlayingInfoCurrentTimePublisherTests: XCTestCase {
         let times = collectOutput(from: publisher, during: 3) {
             player.seek(to: CMTime(value: 5, timescale: 1), toleranceBefore: .zero, toleranceAfter: .zero, completionHandler: { _ in })
         }
+        expect(times.contains(CMTime(value: 5, timescale: 1))).to(beTrue())
         expect(times.sorted()).to(equal(times))
     }
 }
