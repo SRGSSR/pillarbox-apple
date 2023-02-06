f8df20130fabb6e1b24f9a2101c81d641e9bebc2
Walid Kayhal <walid.kayhal@gmail.com>
2023-02-06 10:49:40 +0100

    Improve linter git hook to check only swift files in the staging area (#230)

--- Scripts/check-quality.sh
+++ Scripts/check-quality.sh
@@ -3,7 +3,11 @@
 set -e
 
 echo "... checking Swift code..."
-swiftlint --quiet --strict
+if [ $# -eq 0 ]; then
+  swiftlint --quiet --strict
+elif [[ "$1" == "only-changes" ]]; then
+  git diff --staged --name-only | grep ".swift$" | xargs swiftlint lint --quiet --strict
+fi
 echo "... checking Ruby scripts..."
 bundle exec rubocop --format quiet
 echo "... checking Shell scripts..."
