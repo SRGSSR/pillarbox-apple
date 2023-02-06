776e7ba1f5607ec8829815bffddd1f5e7dbcac21
Walid Kayhal <walid.kayhal@gmail.com>
2023-02-06 10:26:31 +0100

    Update Scripts/check-quality.sh
    Co-authored-by: Samuel Défago <defagos@gmail.com>

--- Scripts/check-quality.sh
+++ Scripts/check-quality.sh
@@ -5,7 +5,7 @@ set -e
 echo "... checking Swift code..."
 if [ $# -eq 0 ]; then
   swiftlint --quiet --strict
-elif [[ "$1" == "forHook" ]]; then
+elif [[ "$1" == "only-changes" ]]; then
   git diff --staged --name-only | grep ".swift$" | xargs swiftlint lint --quiet --strict
 fi
 echo "... checking Ruby scripts..."
