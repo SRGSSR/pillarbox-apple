7c1ebbb21ed8d3dcd4b7042286fc84b9c238460c
Walid Kayhal <walid.kayhal@rts.ch>
2023-02-28 13:30:41 +0100

    Add ListsView

---
+++ Demo/Sources/ListsView.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SwiftUI
+
+struct ListsView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
