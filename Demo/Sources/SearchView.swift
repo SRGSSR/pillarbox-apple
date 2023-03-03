c3c6483851f1e161fc666dbd8584346bd73d24a8
Walid Kayhal <walid.kayhal@rts.ch>
2023-03-03 13:07:06 +0100

    Improve loadedView

--- Demo/Sources/SearchView.swift
+++ Demo/Sources/SearchView.swift
@@ -4,6 +4,7 @@
 //  License information is available from the LICENSE file.
 //
 
+import SRGDataProviderModel
 import SwiftUI
 
 struct SearchView: View {
@@ -13,17 +14,29 @@ struct SearchView: View {
             switch model.state {
             case .loading:
                 ProgressView()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             case let .loaded(medias: medias):
-                List(medias, id: \.urn) { media in
-                    Text(media.title)
-                }
+                loadedView(medias)
             case let .failed(error):
                 Text(error.localizedDescription)
             }
         }
         .navigationTitle("Search")
         .searchable(text: $model.text)
+    }
+
+    @ViewBuilder
+    func loadedView(_ medias: [SRGMedia]) -> some View {
+        if !medias.isEmpty {
+            List(medias, id: \.urn) { media in
+                Text(media.title)
+            }
+        }
+        else if model.text.isEmpty {
+            Text("Enter something to search.")
+        }
+        else {
+            Text("No results.")
+        }
     }
 }
 
