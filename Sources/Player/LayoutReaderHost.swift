779ff0fc06d171a84a99f56681e5928608f55fe4
Samuel Défago <defagos@gmail.com>
2023-03-17 17:38:51 +0100

    Inhibit layout info updates during rotation

--- Sources/Player/LayoutReaderHost.swift
+++ Sources/Player/LayoutReaderHost.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @available(tvOS, unavailable)
 final class LayoutReaderHostingController<Content: View>: UIHostingController<Content>, UIGestureRecognizerDelegate {
     var layoutInfo: Binding<LayoutInfo> = .constant(.none)
+    private var isTransitioning = false
 
     private static func parent(for viewController: UIViewController) -> UIViewController {
         if let parentViewController = viewController.parent {
@@ -25,8 +26,22 @@ final class LayoutReaderHostingController<Content: View>: UIHostingController<Co
         view.backgroundColor = .clear
     }
 
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateLayoutInfo()
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        isTransitioning = true
+        coordinator.animate(alongsideTransition: nil) { [weak self] _ in
+            self?.isTransitioning = false
+        }
+    }
+
+    private func updateLayoutInfo() {
+        guard !isTransitioning else { return }
+
         let frame = view.frame
 
         let parentFrame = Self.parent(for: self).view.frame
