377d0b4086e6863c44fd101592cbf45bf8d4e89e
Samuel Défago <defagos@gmail.com>
2023-03-17 07:36:39 +0100

    Use digit separators
    Co-authored-by: Walid Kayhal <walid.kayhal@gmail.com>

--- Tests/CircumspectTests/TimeIntervalTests.swift
+++ Tests/CircumspectTests/TimeIntervalTests.swift
@@ -13,8 +13,8 @@ import XCTest
 final class TimeIntervalTests: XCTestCase {
     func testDoubleConversion() {
         expect(DispatchTimeInterval.seconds(1).double()).to(equal(1))
-        expect(DispatchTimeInterval.milliseconds(1000).double()).to(equal(1))
-        expect(DispatchTimeInterval.microseconds(1000000).double()).to(equal(1))
-        expect(DispatchTimeInterval.nanoseconds(1000000000).double()).to(equal(1))
+        expect(DispatchTimeInterval.milliseconds(1_000).double()).to(equal(1))
+        expect(DispatchTimeInterval.microseconds(1_000_000).double()).to(equal(1))
+        expect(DispatchTimeInterval.nanoseconds(1_000_000_000).double()).to(equal(1))
     }
 }
