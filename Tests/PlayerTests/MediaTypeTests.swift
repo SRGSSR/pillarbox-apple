ac1bb983e4983ab2ab3ea7de82a6badb93284a14
Walid Kayhal <walid.kayhal@rts.ch>
2023-03-17 13:02:00 +0100

    Introduce MediaTypeTests

---
+++ Tests/PlayerTests/MediaTypeTests.swift
@@ -0,0 +1,18 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+
+final class MediaTypeTests: TestCase {
+    func testUnknown() {
+    }
+
+    func testAudio() {
+    }
+
+    func testVideo() {
+    }
+}
