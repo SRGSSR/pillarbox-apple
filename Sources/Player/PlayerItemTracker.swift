6ff8cd2d11c91ffd0066243c584b4ef018b94977
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-03-21 10:01:46 +0100

    Add enable & disable methods to the procotol

--- Sources/Player/PlayerItemTracker.swift
+++ Sources/Player/PlayerItemTracker.swift
@@ -6,5 +6,12 @@
 
 import Foundation
 
-/// The player item tracker.
-public protocol PlayerItemTracker {}
+/// A type that represents a tracker.
+public protocol PlayerItemTracker {
+  /// Called when a tracker is enabled for a player.
+  /// - Parameter player: The player to which the tracker must be attached.
+  func enable(for player: Player)
+
+  /// Called when a tracker is disabled for a player.
+  func disable()
+}
