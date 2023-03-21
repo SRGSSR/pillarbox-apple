d4baa235cef5bd56c285c565aaf2202056e972c1
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-03-21 11:16:31 +0100

    Improve some tests

--- Tests/PlayerTests/PlayerItemTrackerTests.swift
+++ Tests/PlayerTests/PlayerItemTrackerTests.swift
@@ -11,16 +11,19 @@ import Foundation
 final class PlayerItemTrackerTests: TestCase {
     func testWithInitialItem() {
         let tracker = TrackerMock()
-        _ = Player(item: .simple(url: Stream.shortOnDemand.url, trackers: [tracker]))
-        expectAtLeastEqualPublished(values: [.enabled], from: tracker.$state)
+        let player = Player(item: .simple(url: Stream.shortOnDemand.url, trackers: [tracker]))
+        expectAtLeastEqualPublished(values: [.enabled, .disabled], from: tracker.$state) {
+            player.play()
+        }
     }
 
     func testWithoutInitialItem() {
         let tracker = TrackerMock()
         let player = Player()
 
-        expectAtLeastEqualPublished(values: [.unknown, .enabled], from: tracker.$state) {
+        expectAtLeastEqualPublished(values: [.unknown, .enabled, .disabled], from: tracker.$state) {
             player.append(.simple(url: Stream.shortOnDemand.url, trackers: [tracker]))
+            player.play()
         }
     }
 }
