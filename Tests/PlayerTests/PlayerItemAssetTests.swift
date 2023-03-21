727af2a392154244648833f8a5f6c07fef60445b
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-03-21 10:06:35 +0100

    Fix typo

--- Tests/PlayerTests/PlayerItemAssetTests.swift
+++ Tests/PlayerTests/PlayerItemAssetTests.swift
@@ -12,7 +12,7 @@ import Combine
 import Nimble
 import XCTest
 
-final class PlayerItemsAssetTests: TestCase {
+final class PlayerItemAssetTests: TestCase {
     func testGenericItem() {
         let publisher = Just(Asset.simple(url: Stream.onDemand.url))
         let item = PlayerItem(publisher: publisher)
