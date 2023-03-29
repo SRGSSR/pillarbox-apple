26fd71c0848acdabb86546dfe77d012c2527c003
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-03-29 09:11:11 +0200

    Use the generic tracker and avoid to use a static state

--- Tests/PlayerTests/Analytics/PlayerTrackingTests.swift
+++ Tests/PlayerTests/Analytics/PlayerTrackingTests.swift
@@ -9,21 +9,25 @@ import Foundation
 @testable import Player
 
 import Circumspect
+import Combine
 
 final class PlayerTrackingTests: TestCase {
     func testTrackingDisabled() {
         let player = Player()
         player.isTrackingEnabled = false
+        let publisher = GenericTrackerMock<String>.StatePublisher()
 
         expectAtLeastEqualPublished(
             values: [.initialized, .updated("title")],
-            from: NonConfigurableTrackerMock.state
+            from: publisher
         ) {
             player.append(
                 .simple(
                     url: Stream.shortOnDemand.url,
                     metadata: AssetMetadataMock(title: "title"),
-                    trackerAdapters: [NonConfigurableTrackerMock.adapter { $0.title } ]
+                    trackerAdapters: [
+                        GenericTrackerMock.adapter(statePublisher: publisher) { $0.title }
+                    ]
                 )
             )
             player.play()
@@ -34,23 +38,27 @@ final class PlayerTrackingTests: TestCase {
         let player = Player()
         player.isTrackingEnabled = false
 
+        let publisher = GenericTrackerMock<String>.StatePublisher()
+
         expectEqualPublished(
             values: [.initialized, .updated("title")],
-            from: NonConfigurableTrackerMock.state,
+            from: publisher,
             during: .seconds(1)
         ) {
             player.append(
                 .simple(
                     url: Stream.shortOnDemand.url,
                     metadata: AssetMetadataMock(title: "title"),
-                    trackerAdapters: [NonConfigurableTrackerMock.adapter { $0.title } ]
+                    trackerAdapters: [
+                        GenericTrackerMock.adapter(statePublisher: publisher) { $0.title }
+                    ]
                 )
             )
         }
 
         expectAtLeastEqualPublished(
             values: [.enabled, .disabled],
-            from: NonConfigurableTrackerMock.state
+            from: publisher
         ) {
             player.isTrackingEnabled = true
             player.play()
@@ -61,23 +69,27 @@ final class PlayerTrackingTests: TestCase {
         let player = Player()
         player.isTrackingEnabled = true
 
+        let publisher = GenericTrackerMock<String>.StatePublisher()
+
         expectEqualPublished(
             values: [.initialized, .enabled, .updated("title")],
-            from: NonConfigurableTrackerMock.state,
+            from: publisher,
             during: .seconds(1)
         ) {
             player.append(
                 .simple(
                     url: Stream.shortOnDemand.url,
                     metadata: AssetMetadataMock(title: "title"),
-                    trackerAdapters: [NonConfigurableTrackerMock.adapter { $0.title } ]
+                    trackerAdapters: [
+                        GenericTrackerMock.adapter(statePublisher: publisher) { $0.title }
+                    ]
                 )
             )
         }
 
         expectAtLeastEqualPublished(
             values: [.disabled],
-            from: NonConfigurableTrackerMock.state
+            from: publisher
         ) {
             player.isTrackingEnabled = false
             player.play()
@@ -85,10 +97,12 @@ final class PlayerTrackingTests: TestCase {
     }
 
     func testTrackingEnabledTwice() {
-        let player = Player(item: .simple(url: Stream.shortOnDemand.url, trackerAdapters: [SimpleTrackerMock.adapter()]))
+        let publisher = EmptyTrackerMock.StatePublisher()
+
+        let player = Player(item: .simple(url: Stream.shortOnDemand.url, trackerAdapters: [EmptyTrackerMock.adapter(statePublisher: publisher)]))
         player.isTrackingEnabled = true
 
-        expectNothingPublished(from: SimpleTrackerMock.state, during: .seconds(1)) {
+        expectNothingPublished(from: publisher, during: .seconds(1)) {
             player.isTrackingEnabled = true
         }
     }
--- Tests/PlayerTests/Tools/GenericTrackerMock.swift
+++ Tests/PlayerTests/Tools/GenericTrackerMock.swift
@@ -8,9 +8,51 @@
 
 import Combine
 
-final class GenericTrackerMock<Parameter, Metadata>: PlayerItemTracker {
+final class EmptyTrackerMock: PlayerItemTracker {
+    typealias StatePublisher = PassthroughSubject<State, Never>
+
     enum State {
-        case initialized(Parameter)
+        case initialized
+        case enabled
+        case disabled
+        case updated
+        case deinitialized
+    }
+
+    struct Configuration {
+        let statePublisher: StatePublisher
+    }
+
+    private let configuration: Configuration
+    private var cancellables = Set<AnyCancellable>()
+
+    init(configuration: Configuration, metadataPublisher: AnyPublisher<Void, Never>) {
+        self.configuration = configuration
+        configuration.statePublisher.send(.initialized)
+        metadataPublisher.sink { _ in
+            configuration.statePublisher.send(.updated)
+        }
+        .store(in: &cancellables)
+    }
+
+    func enable(for player: Player) {
+        configuration.statePublisher.send(.enabled)
+    }
+
+    func disable() {
+        configuration.statePublisher.send(.disabled)
+    }
+
+    deinit {
+        configuration.statePublisher.send(.deinitialized)
+    }
+}
+
+final class GenericTrackerMock<Metadata: Equatable>: PlayerItemTracker {
+    typealias StatePublisher = PassthroughSubject<State, Never>
+
+    enum State: Equatable {
+        case initialized
         case enabled
         case disabled
         case updated(Metadata)
@@ -18,9 +60,7 @@ final class GenericTrackerMock<Parameter, Metadata>: PlayerItemTracker {
     }
 
     struct Configuration {
-        // swiftlint:disable:next private_subject
-        let statePublisher: PassthroughSubject<State, Never>
-        let parameter: Parameter
+        let statePublisher: StatePublisher
     }
 
     private let configuration: Configuration
@@ -28,8 +68,7 @@ final class GenericTrackerMock<Parameter, Metadata>: PlayerItemTracker {
 
     init(configuration: Configuration, metadataPublisher: AnyPublisher<Metadata, Never>) {
         self.configuration = configuration
-
-        configuration.statePublisher.send(.initialized(configuration.parameter))
+        configuration.statePublisher.send(.initialized)
         metadataPublisher.sink { metadata in
             configuration.statePublisher.send(.updated(metadata))
         }
@@ -46,5 +85,17 @@ final class GenericTrackerMock<Parameter, Metadata>: PlayerItemTracker {
 
     deinit {
         configuration.statePublisher.send(.deinitialized)
+    }
+}
+
+extension GenericTrackerMock {
+    static func adapter<M>(statePublisher: StatePublisher, mapper: @escaping (M) -> Metadata) -> TrackerAdapter<M> where M: AssetMetadata {
+        adapter(configuration: Configuration(statePublisher: statePublisher), mapper: mapper)
+    }
+}
+
+extension EmptyTrackerMock {
+    static func adapter(statePublisher: StatePublisher) -> TrackerAdapter<Never> {
+        adapter(configuration: Configuration(statePublisher: statePublisher))
     }
 }
