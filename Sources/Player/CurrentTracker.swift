b996fb1bf685d8a2bf4ee3dca89f26c690305e98
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-03-29 14:53:17 +0200

    Extract CurrentTracker from the Player extension

--- Sources/Player/CurrentTracker.swift
+++ Sources/Player/CurrentTracker.swift
@@ -6,57 +6,55 @@
 
 import Combine
 
-extension Player {
-    final class CurrentTracker {
-        private let item: PlayerItem
-        private var isEnabled = false
-        private var cancellables = Set<AnyCancellable>()
+final class CurrentTracker {
+    private let item: PlayerItem
+    private var isEnabled = false
+    private var cancellables = Set<AnyCancellable>()
 
-        init(item: PlayerItem, player: Player) {
-            self.item = item
-            configureTrackingPublisher(player: player)
-            configureAssetPublisher(for: item)
-        }
+    init(item: PlayerItem, player: Player) {
+        self.item = item
+        configureTrackingPublisher(player: player)
+        configureAssetPublisher(for: item)
+    }
 
-        deinit {
-            disableAssetIfNeeded()
-        }
-
-        private func configureTrackingPublisher(player: Player) {
-            player.$isTrackingEnabled
-                .sink { [weak self, weak player] enabled in
-                    guard let self, let player, self.isEnabled != enabled else { return }
-                    self.isEnabled = enabled
-                    if enabled {
-                        self.enableAsset(for: player)
-                    }
-                    else {
-                        self.disableAsset()
-                    }
+    private func configureTrackingPublisher(player: Player) {
+        player.$isTrackingEnabled
+            .sink { [weak self, weak player] enabled in
+                guard let self, let player, self.isEnabled != enabled else { return }
+                self.isEnabled = enabled
+                if enabled {
+                    self.enableAsset(for: player)
                 }
-                .store(in: &cancellables)
-        }
-
-        private func configureAssetPublisher(for item: PlayerItem) {
-            item.$asset
-                .sink { asset in
-                    asset.updateMetadata()
+                else {
+                    self.disableAsset()
                 }
-                .store(in: &cancellables)
-        }
-
-        private func enableAsset(for player: Player) {
-            item.asset.enable(for: player)
-        }
-
-        private func disableAsset() {
-            item.asset.disable()
-        }
-
-        private func disableAssetIfNeeded() {
-            if isEnabled {
-                disableAsset()
             }
+            .store(in: &cancellables)
+    }
+
+    private func configureAssetPublisher(for item: PlayerItem) {
+        item.$asset
+            .sink { asset in
+                asset.updateMetadata()
+            }
+            .store(in: &cancellables)
+    }
+
+    private func enableAsset(for player: Player) {
+        item.asset.enable(for: player)
+    }
+
+    private func disableAsset() {
+        item.asset.disable()
+    }
+
+    private func disableAssetIfNeeded() {
+        if isEnabled {
+            disableAsset()
         }
+    }
+
+    deinit {
+        disableAssetIfNeeded()
     }
 }
