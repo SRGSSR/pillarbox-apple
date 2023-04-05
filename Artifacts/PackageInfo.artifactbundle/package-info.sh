3c97393980a49134ece1c9bf10524c9ae7c1f8fe
Samuel Défago <defagos@gmail.com>
2023-04-05 19:24:17 +0200

    Extract short version

--- Artifacts/PackageInfo.artifactbundle/package-info.sh
+++ Artifacts/PackageInfo.artifactbundle/package-info.sh
@@ -10,6 +10,11 @@ if ! VERSION=$(git --git-dir "$1/.git" describe --tags 2> /dev/null); then
     exit 1
 fi
 
+if ! SHORT_VERSION=$(git --git-dir "$1/.git" describe --tags --abbrev=0 2> /dev/null); then
+    echo "No tag was found in the specified repository."
+    exit 1
+fi
+
 GENERATED_FILE_PATH="$2/PackageInfo.swift"
 
 cat > "$GENERATED_FILE_PATH" <<- EOF
@@ -18,5 +23,6 @@ import Foundation
 
 enum PackageInfo {
     static let version = "$VERSION"
+    static let shortVersion = "$SHORT_VERSION"
 }
 EOF
