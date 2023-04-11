97f6f4e7f93b3e016662024e90701988fec37bc6
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-04-11 14:52:25 +0200

    Replace previous tests by testLabels

--- Tests/AnalyticsTests/CommandersActEventTests.swift
+++ Tests/AnalyticsTests/CommandersActEventTests.swift
@@ -7,54 +7,33 @@
 @testable import Analytics
 
 import AnalyticsTestBridge
+import Nimble
 import XCTest
 
 final class CommandersActEventTests: CommandersActTestCase {
-    func testName() {
-        expectAtLeastEqual(values: ["my-event"], for: "event_name") { test in
-            test.sendEvent(name: "my-event")
-        }
-    }
-
-    func testType() {
-        expectAtLeastEqual(values: ["my-event-type"], for: "event_type") { test in
-            test.sendEvent(type: "my-event-type")
-        }
-    }
-
-    func testSource() {
-        expectAtLeastEqual(values: ["my-event-source"], for: "event_source") { test in
-            test.sendEvent(source: "my-event-source")
-        }
-    }
-
-    func testExtra1() {
-        expectAtLeastEqual(values: ["my-event-extra1"], for: "event_value_1") { test in
-            test.sendEvent(extra1: "my-event-extra1")
-        }
-    }
-
-    func testExtra2() {
-        expectAtLeastEqual(values: ["my-event-extra2"], for: "event_value_2") { test in
-            test.sendEvent(extra2: "my-event-extra2")
-        }
-    }
-
-    func testExtra3() {
-        expectAtLeastEqual(values: ["my-event-extra3"], for: "event_value_3") { test in
-            test.sendEvent(extra3: "my-event-extra3")
-        }
-    }
-
-    func testExtra4() {
-        expectAtLeastEqual(values: ["my-event-extra4"], for: "event_value_4") { test in
-            test.sendEvent(extra4: "my-event-extra4")
-        }
-    }
-
-    func testExtra5() {
-        expectAtLeastEqual(values: ["my-event-extra5"], for: "event_value_5") { test in
-            test.sendEvent(extra5: "my-event-extra5")
+    func testLabels() {
+        wait { test in
+            test.sendEvent(
+                name: "name",
+                type: "type",
+                value: "value",
+                source: "source",
+                extra1: "extra1",
+                extra2: "extra2",
+                extra3: "extra3",
+                extra4: "extra4",
+                extra5: "extra5"
+            )
+        } received: { labels in
+            expect(labels["event_name"] as? String).to(equal("name"))
+            expect(labels["event_type"] as? String).to(equal("type"))
+            expect(labels["event_value"] as? String).to(equal("value"))
+            expect(labels["event_source"] as? String).to(equal("source"))
+            expect(labels["event_value_1"] as? String).to(equal("extra1"))
+            expect(labels["event_value_2"] as? String).to(equal("extra2"))
+            expect(labels["event_value_3"] as? String).to(equal("extra3"))
+            expect(labels["event_value_4"] as? String).to(equal("extra4"))
+            expect(labels["event_value_5"] as? String).to(equal("extra5"))
         }
     }
 }
