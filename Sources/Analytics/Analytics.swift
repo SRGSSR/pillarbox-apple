a3d015b2d8c5e79bdbbd18da0b81999e9bdcd65d
Samuel Défago <defagos@gmail.com>
2023-04-17 09:33:38 +0200

    Extract category

--- Sources/Analytics/Analytics.swift
+++ Sources/Analytics/Analytics.swift
@@ -103,9 +103,3 @@ public class Analytics {
         commandersActService.sendStreamingEvent(name: name)
     }
 }
-
-extension String {
-    var isBlank: Bool {
-        trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-    }
-}
