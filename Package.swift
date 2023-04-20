9ef0e797eb661f7aca745059080f536f7bef6943
Samuel Défago <defagos@gmail.com>
2023-04-20 20:33:07 +0200

    Restore official Commanders Act SDK source

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/comScore/Comscore-Swift-Package-Manager.git", .upToNextMinor(from: "6.10.0")),
         .package(url: "https://github.com/SRGSSR/GoogleCastSDK-no-bluetooth.git", .upToNextMinor(from: "4.7.1-beta.1")),
-        .package(url: "https://github.com/SRGSSR/iOSV5.git", .upToNextMinor(from: "5.3.0")),
+        .package(url: "https://github.com/CommandersAct/iOSV5.git", .upToNextMinor(from: "5.3.0")),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.3")),
         .package(url: "https://github.com/krzysztofzablocki/Difference.git", exact: "1.0.1"),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "12.0.0")),
