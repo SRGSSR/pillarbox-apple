2cf035248221b698f4df7af5aaf8f19d120d1ebf
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-05-02 14:32:22 +0200

    Introduce CommandersActStreamingAnalyticsLiveTests & CommandersActStreamingAnalyticsOnDemandTests

---
+++ Tests/AnalyticsTests/CommandersAct/CommandersActStreamingAnalyticsOnDemandTests.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+
+// swiftlint:disable:next type_name
+private final class CommandersActStreamingAnalyticsOnDemandTests: CommandersActTestCase {
+}
