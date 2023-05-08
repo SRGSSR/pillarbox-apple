0b894982a14a0c5341212193d0a8d0392202e118
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-05-08 07:26:50 +0200

    Introduce CommandersActHeartbeatOnDemandTests

---
+++ Tests/AnalyticsTests/CommandersAct/CommandersActHeartbeatOnDemandTests.swift
@@ -0,0 +1,27 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+
+final class CommandersActHeartbeatOnDemandTests: CommandersActTestCase {
+    func testHeartbeatAfterPlay() {
+    }
+
+    func testNoHeartbeatAfterPause() {
+    }
+
+    func testNoHeartbeatAfterSeek() {
+    }
+
+    func testNoHeartbeatAfterEof() {
+    }
+
+    func testNoHeartbeatAfterStop() {
+    }
+
+    func testNoHeartbeatWhileBuffering() {
+    }
+}
