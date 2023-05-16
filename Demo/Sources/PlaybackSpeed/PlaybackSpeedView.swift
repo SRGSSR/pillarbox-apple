c6f7b97c29a9fb17fd8f8c8774a5c8b8661bde4e
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-05-16 07:08:36 +0200

    Introduce PlaybackSpeedView

---
+++ Demo/Sources/PlaybackSpeed/PlaybackSpeedView.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SwiftUI
+
+struct PlaybackSpeedView: View {
+    var body: some View {
+        ZStack {}
+    }
+}
+
+struct PlaybackSpeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaybackSpeedView()
+    }
+}
