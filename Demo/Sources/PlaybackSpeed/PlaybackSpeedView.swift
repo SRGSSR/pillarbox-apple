5c066796ca9e52df05f2a1de5f42249c69833393
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-05-16 07:21:44 +0200

    Introduce SettingsMenuView

--- Demo/Sources/PlaybackSpeed/PlaybackSpeedView.swift
+++ Demo/Sources/PlaybackSpeed/PlaybackSpeedView.swift
@@ -6,14 +6,28 @@
 
 import SwiftUI
 
+private struct SettingsMenuView<Content: View>: View {
+    let content: () -> Content
+
+    var body: some View {
+        Menu(content: content) {
+            Image(systemName: "ellipsis.circle")
+                .tint(.white)
+        }
+    }
+}
+
 struct PlaybackSpeedView: View {
     var body: some View {
-        ZStack {}
+        SettingsMenuView {
+            EmptyView()
+        }
     }
 }
 
 struct PlaybackSpeedView_Previews: PreviewProvider {
     static var previews: some View {
         PlaybackSpeedView()
+            .background(.black)
     }
 }
