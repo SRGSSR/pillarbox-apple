eccd55063449414f90e1ff3eb1c5801773b59cee
Samuel Défago <defagos@gmail.com>
2023-06-12 22:25:59 +0200

    Expose comScore labels publicly

--- Sources/Analytics/ComScore/ComScoreLabels.swift
+++ Sources/Analytics/ComScore/ComScoreLabels.swift
@@ -10,81 +10,81 @@ import Foundation
 public struct ComScoreLabels {
     let dictionary: [String: String]
 
-    var ns_st_ev: String? {
-        extract()
-    }
-
-    var ns_ap_ev: String? {
-        extract()
-    }
-
     var listener_session_id: String? {
+        extract()
+    }
+
+    public var ns_st_ev: String? {
+        extract()
+    }
+
+    public var ns_ap_ev: String? {
         extract()
     }
 
     // MARK: Common labels
 
     /// The value of `c2` (comScore account).
-    var c2: String? {
+    public var c2: String? {
         extract()
     }
 
     /// The value of `ns_ap_an` (application name).
-    var ns_ap_an: String? {
+    public var ns_ap_an: String? {
         extract()
     }
 
     // MARK: Mediapulse labels
 
     /// The value of `mp_brand` (vendor).
-    var mp_brand: String? {
+    public var mp_brand: String? {
         extract()
     }
 
     /// The value of `mp_v` (application version).
-    var mp_v: String? {
+    public var mp_v: String? {
         extract()
     }
 
     // MARK: Page view labels
 
     /// The value of `ns_category` (name of the section).
-    var ns_category: String? {
+    public var ns_category: String? {
         extract()
     }
 
     // MARK: Streaming labels
 
     /// The value of `ns_st_mp` (media player name).
-    var ns_st_mp: String? {
+    public var ns_st_mp: String? {
         extract()
     }
 
     /// The value of `ns_st_mv` (media player version).
-    var ns_st_mv: String? {
+    public var ns_st_mv: String? {
         extract()
     }
 
     /// The value of `ns_st_po` (playback position).
-    var ns_st_po: Double? {
+    public var ns_st_po: Double? {
         guard let value: Double = extract() else { return nil }
         return value / 1000
     }
 
     /// The value of `ns_st_ldw` (DVR window length).
-    var ns_st_ldw: Double? {
+    public var ns_st_ldw: Double? {
         guard let value: Double = extract() else { return nil }
         return value / 1000
     }
 
     /// The value of `ns_st_ldo` (DVR live edge offset).
-    var ns_st_ldo: Double? {
+    public var ns_st_ldo: Double? {
         guard let value: Double = extract() else { return nil }
         return value / 1000
     }
 
     /// The value of `ns_st_rt` (playback rate).
-    var ns_st_rt: Int? {
+    public var ns_st_rt: Int? {
         extract()
     }
 
