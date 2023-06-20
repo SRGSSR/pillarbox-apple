7affccc514c4816ec6c1fcb3f3c6c78605694be9
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-06-20 10:01:35 +0200

    Introduce ApplicationState

---
+++ Sources/Analytics/ApplicationState.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+
+enum ApplicationState {
+    case foreground
+    case background
+}
