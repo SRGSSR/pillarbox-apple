beaf013a589b6559650c9248f206e6798c5aa4f9
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-06-20 10:22:42 +0200

    Add applicationStatePublisher

---
+++ Sources/Analytics/UIApplication.swift
@@ -0,0 +1,23 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Combine
+import Core
+import UIKit
+
+extension UIApplication {
+    func applicationStatePublisher() -> AnyPublisher<ApplicationState, Never> {
+        Publishers.Merge(
+            NotificationCenter.default.weakPublisher(for: UIApplication.didEnterBackgroundNotification, object: self)
+                .map { _ in .background },
+            NotificationCenter.default.weakPublisher(for: UIApplication.didBecomeActiveNotification, object: self)
+                .map { _ in .foreground }
+        )
+        .prepend(applicationState == .background ? .background : .foreground)
+        .removeDuplicates()
+        .eraseToAnyPublisher()
+    }
+}
