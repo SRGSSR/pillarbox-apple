a395cf43a1e582653dfb3188bfeb043c8d727fc5
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-07-02 11:41:01 +0200

    Add minimum and maximum value text

--- Demo/Sources/Views/PlaybackSlider.swift
+++ Demo/Sources/Views/PlaybackSlider.swift
@@ -7,7 +7,28 @@
 import SwiftUI
 
 struct PlaybackSlider: View {
+    let minimumValueText: String = ""
+    let maximumValueText: String = ""
+
     var body: some View {
-        EmptyView()
+        HStack {
+            text(minimumValueText)
+            Rectangle()
+            text(maximumValueText)
+        }
+    }
+
+    @ViewBuilder
+    private func text(_ text: String) -> some View {
+        Text(text)
+            .font(.footnote)
+            .fixedSize()
+    }
+}
+
+struct PlaybackSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaybackSlider()
+            .padding(.horizontal, 5)
     }
 }
