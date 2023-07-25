587cd11dae6bde8fe2de7bfbb9c59cf077652e79
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-07-25 11:30:23 +0200

    Add YouTube auto paste (#480)
    Co-authored-by: Samuel Défago <defagos@gmail.com>

---
+++ Demo/Sources/YouTube/YouTube+View.swift
@@ -0,0 +1,40 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SwiftUI
+import YouTubeIdentifier
+
+extension View {
+#if os(iOS)
+    private static func paste(_ text: Binding<String>) {
+        guard UIPasteboard.general.hasYouTubeUrl,
+              let paste = UIPasteboard.general.string else { return }
+        text.wrappedValue = paste
+    }
+#endif
+
+    func youTubePaste(_ text: Binding<String>) -> some View {
+#if os(iOS)
+        onAppear {
+            Self.paste(text)
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            Self.paste(text)
+        }
+#else
+        self
+#endif
+    }
+}
+
+#if os(iOS)
+private extension UIPasteboard {
+    var hasYouTubeUrl: Bool {
+        guard let string, let url = URL(string: string) else { return false }
+        return YouTubeIdentifier.extract(from: url) != nil
+    }
+}
+#endif
