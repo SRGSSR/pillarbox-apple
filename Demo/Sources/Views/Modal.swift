a4715b4847a07a3fd5f4b3f4ca3942006c448900
Samuel Défago <defagos@gmail.com>
2023-07-25 09:07:22 +0200

    Display full-screen modal on iPad

---
+++ Demo/Sources/Views/Modal.swift
@@ -0,0 +1,39 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SwiftUI
+
+private struct PresentedView<Content>: View where Content: View {
+    @ViewBuilder var content: () -> Content
+    @Environment(\.dismiss) private var dismiss
+
+    var body: some View {
+        content()
+            .gesture(
+                DragGesture(minimumDistance: 100)
+                    .onEnded { value in
+                        guard value.translation.height > 0 else { return }
+                        dismiss()
+                    }
+            )
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func modal<Item, Content>(item: Binding<Item?>, @ViewBuilder content: @escaping (Item) -> Content) -> some View where Item: Identifiable, Content: View {
+        switch UIDevice.current.userInterfaceIdiom {
+        case .pad:
+            fullScreenCover(item: item) { item in
+                PresentedView {
+                    content(item)
+                }
+            }
+        default:
+            sheet(item: item, content: content)
+        }
+    }
+}
