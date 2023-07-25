60edbb2c786364aad7766532b9eda340165e9133
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-07-25 10:01:49 +0200

    Fix tvOS

--- Demo/Sources/YouTube/YouTube+View.swift
+++ Demo/Sources/YouTube/YouTube+View.swift
@@ -8,25 +8,33 @@ import SwiftUI
 import YouTubeIdentifier
 
 extension View {
+#if os(iOS)
     private static func paste(_ text: Binding<String>) {
         guard UIPasteboard.general.hasYouTubeUrl,
               let paste = UIPasteboard.general.string else { return }
         text.wrappedValue = paste
     }
+#endif
 
     func youTubePaste(_ text: Binding<String>) -> some View {
+#if os(iOS)
         onAppear {
             Self.paste(text)
         }
         .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
             Self.paste(text)
         }
+#else
+        self
+#endif
     }
 }
 
+#if os(iOS)
 private extension UIPasteboard {
     var hasYouTubeUrl: Bool {
         guard let string else { return false }
         return YouTubeIdentifier.extract(from: URL(string: string)!) != nil
     }
 }
+#endif
