62e5201b28fd3f80f8e2af1035c549100e762737
Samuel Défago <defagos@gmail.com>
2023-09-13 11:17:46 +0200

    Cleanup unused packages and dependencies (#565)

--- Scripts/generate-documentation.sh
+++ Scripts/generate-documentation.sh
@@ -5,7 +5,5 @@ xcodebuild docbuild -scheme Analytics -destination generic/platform=iOS
 xcodebuild docbuild -scheme Circumspect -destination generic/platform=iOS
 xcodebuild docbuild -scheme Core -destination generic/platform=iOS
 xcodebuild docbuild -scheme CoreBusiness -destination generic/platform=iOS
-xcodebuild docbuild -scheme Diagnostics -destination generic/platform=iOS
 xcodebuild docbuild -scheme Player -destination generic/platform=iOS
-xcodebuild docbuild -scheme UserInterface -destination generic/platform=iOS
 popd > /dev/null || exit
--- Sources/UserInterface/UserInterface.docc/UserInterface.md
+++
@@ -1,3 +0,0 @@
-# ``UserInterface``
-
-The UserInterface library provides standard playback user interface components.
