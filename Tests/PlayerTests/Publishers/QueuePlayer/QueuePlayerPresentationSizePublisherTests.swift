e9ca96f14182a06abdd062cbb62c42243b8ab4a1
Samuel Défago <defagos@gmail.com>
2023-09-21 14:18:22 +0200

    Merge presentation size publisher tests

--- Tests/PlayerTests/Publishers/QueuePlayer/QueuePlayerPresentationSizePublisherTests.swift
+++ Tests/PlayerTests/Publishers/QueuePlayer/QueuePlayerPresentationSizePublisherTests.swift
@@ -33,4 +33,41 @@ final class QueuePlayerPresentationSizePublisherTests: TestCase {
         let player = QueuePlayer(url: Stream.shortOnDemand.url)
         expectAtLeastEqualPublished(values: [nil, CGSize(width: 640, height: 360)], from: presentationSizePublisher(for: player))
     }
+
+    func testVideoFollowedByAudio() {
+        let item1 = AVPlayerItem(url: Stream.shortOnDemand.url)
+        let item2 = AVPlayerItem(url: Stream.mp3.url)
+        let player = QueuePlayer(items: [item1, item2])
+        expectAtLeastEqualPublished(
+            values: [nil, CGSize(width: 640, height: 360), nil, .zero],
+            from: presentationSizePublisher(for: player)
+        ) {
+            player.play()
+        }
+    }
+
+    func testVideosWithDifferentSizes() {
+        let item1 = AVPlayerItem(url: Stream.shortOnDemand.url)
+        let item2 = AVPlayerItem(url: Stream.squareOnDemand.url)
+        let player = QueuePlayer(items: [item1, item2])
+        expectAtLeastEqualPublished(
+            values: [nil, CGSize(width: 640, height: 360), CGSize(width: 360, height: 360)],
+            from: presentationSizePublisher(for: player)
+        ) {
+            player.play()
+        }
+    }
+
+    func testVideosWithIdenticalSizes() {
+        let item1 = AVPlayerItem(url: Stream.shortOnDemand.url)
+        let item2 = AVPlayerItem(url: Stream.onDemand.url)
+        let player = QueuePlayer(items: [item1, item2])
+        expectEqualPublished(
+            values: [nil, CGSize(width: 640, height: 360)],
+            from: presentationSizePublisher(for: player),
+            during: .seconds(2)
+        ) {
+            player.play()
+        }
+    }
 }
