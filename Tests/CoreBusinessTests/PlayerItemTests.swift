9af2b27f1c0e7a45409927181483cf96345ed3ce
Samuel Défago <defagos@gmail.com>
2023-09-26 10:05:37 +0200

    Restore tests

--- Tests/CoreBusinessTests/PlayerItemTests.swift
+++ Tests/CoreBusinessTests/PlayerItemTests.swift
@@ -13,64 +13,48 @@ import Player
 import XCTest
 
 final class PlayerItemTests: XCTestCase {
-    // FIXME: Quick and dirty
-//    func testUrnPlaybackHLS() {
-//        let item = PlayerItem.urn("urn:rts:video:6820736")
-//        let player = Player(item: item)
-//        expectAtLeastEqualPublished(
-//            values: [
-//                .idle,
-//                .paused
-//            ],
-//            from: player.$playbackState
-//        )
-//    }
-//
-//    func testUrnPlaybackMP3() {
-//        let item = PlayerItem.urn("urn:rsi:audio:1861947")
-//        let player = Player(item: item)
-//        expectAtLeastEqualPublished(
-//            values: [
-//                .idle,
-//                .paused
-//            ],
-//            from: player.$playbackState
-//        )
-//    }
-//
-//    func testUrnPlaybackUnknown() {
-//        let item = PlayerItem.urn("urn:srf:video:unknown")
-//        let player = Player(item: item)
-//        expectAtLeastEqualPublished(
-//            values: [
-//                .idle,
-//                .failed(error: NSError(
-//                    domain: "CoreBusiness.DataError",
-//                    code: 1,
-//                    userInfo: [
-//                        NSLocalizedDescriptionKey: "Not found"
-//                    ]
-//                ))
-//            ],
-//            from: player.$playbackState
-//        )
-//    }
-//
-//    func testUrnPlaybackNotAvailableAnymore() {
-//        let item = PlayerItem.urn("urn:rts:video:13382911")
-//        let player = Player(item: item)
-//        expectAtLeastEqualPublished(
-//            values: [
-//                .idle,
-//                .failed(error: NSError(
-//                    domain: "CoreBusiness.DataError",
-//                    code: 1,
-//                    userInfo: [
-//                        NSLocalizedDescriptionKey: "This content is not available anymore."
-//                    ]
-//                ))
-//            ],
-//            from: player.$playbackState
-//        )
-//    }
+    private func playbackStatePublisher(for player: Player) -> AnyPublisher<PlaybackState, Never> {
+        player.$context
+            .map(\.playbackState)
+            .removeDuplicates()
+            .eraseToAnyPublisher()
+    }
+
+    func testUrnPlaybackHLS() {
+        let item = PlayerItem.urn("urn:rts:video:6820736")
+        let player = Player(item: item)
+        expectAtLeastEqualPublished(
+            values: [.idle, .paused],
+            from: playbackStatePublisher(for: player)
+        )
+    }
+
+    func testUrnPlaybackMP3() {
+        let item = PlayerItem.urn("urn:rsi:audio:1861947")
+        let player = Player(item: item)
+        expectAtLeastEqualPublished(
+            values: [.idle, .paused],
+            from: playbackStatePublisher(for: player)
+        )
+    }
+
+    func testUrnPlaybackUnknown() {
+        let item = PlayerItem.urn("urn:srf:video:unknown")
+        let player = Player(item: item)
+        expectEqualPublished(
+            values: [.idle],
+            from: playbackStatePublisher(for: player),
+            during: .seconds(1)
+        )
+    }
+
+    func testUrnPlaybackNotAvailableAnymore() {
+        let item = PlayerItem.urn("urn:rts:video:13382911")
+        let player = Player(item: item)
+        expectEqualPublished(
+            values: [.idle],
+            from: playbackStatePublisher(for: player),
+            during: .seconds(1)
+        )
+    }
 }
