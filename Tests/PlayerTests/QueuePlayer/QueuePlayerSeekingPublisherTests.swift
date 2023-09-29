a28490813d8831bc2431758f0a5dc575bd8639a6
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-09-29 10:16:54 +0200

    Remove useless seek publisher tests

--- Tests/PlayerTests/QueuePlayer/QueuePlayerSeekingPublisherTests.swift
+++ Tests/PlayerTests/QueuePlayer/QueuePlayerSeekingPublisherTests.swift
@@ -12,68 +12,15 @@ import Nimble
 import Streams
 
 final class QueuePlayerSeekingPublisherTests: TestCase {
-    func testEmpty() {
-        let player = QueuePlayer()
-        expectAtLeastEqualPublished(
-            values: [false],
-            from: player.isSeekingPublisher()
-        )
-    }
-
     func testSeek() {
-        let item = AVPlayerItem(url: Stream.onDemand.url)
-        let player = QueuePlayer(playerItem: item)
-        let time = CMTime(value: 1, timescale: 1)
+        let player = QueuePlayer(
+            playerItem: .init(url: Stream.onDemand.url)
+        )
         expectAtLeastEqualPublished(
             values: [false, true, false],
             from: player.isSeekingPublisher()
         ) {
-            player.seek(to: time)
+            player.seek(to: .init(value: 1, timescale: 1))
         }
-    }
-
-    func testMultipleSeek() {
-        let item = AVPlayerItem(url: Stream.onDemand.url)
-        let player = QueuePlayer(playerItem: item)
-        let time1 = CMTime(value: 1, timescale: 1)
-        let time2 = CMTime(value: 2, timescale: 1)
-        expectAtLeastEqualPublished(
-            values: [false, true, false, true, false],
-            from: player.isSeekingPublisher()
-        ) {
-            player.seek(to: time1)
-            player.seek(to: time2)
-        }
-    }
-
-    func testMultipleSeeksWithinTimeRange() {
-        let item = AVPlayerItem(url: Stream.onDemand.url)
-        let player = QueuePlayer(playerItem: item)
-        player.play()
-        expect(item.timeRange).toEventuallyNot(equal(.invalid))
-
-        let time1 = CMTime(value: 1, timescale: 1)
-        let time2 = CMTime(value: 2, timescale: 1)
-        expectAtLeastEqualPublished(
-            values: [false, true, false],
-            from: player.isSeekingPublisher()
-        ) {
-            player.seek(to: time1)
-            player.seek(to: time2)
-        }
-    }
-
-    func testTargetSeekTimeWithMultipleSeeks() {
-        let item = AVPlayerItem(url: Stream.onDemand.url)
-        let player = QueuePlayer(playerItem: item)
-        expect(player.timeRange).toEventuallyNot(equal(.invalid))
-
-        let time1 = CMTime(value: 1, timescale: 1)
-        player.seek(to: time1)
-        expect(player.targetSeekTime).to(equal(time1))
-
-        let time2 = CMTime(value: 2, timescale: 1)
-        player.seek(to: time2)
-        expect(player.targetSeekTime).to(equal(time2))
     }
 }
