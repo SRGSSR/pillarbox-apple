96cf4698f0a4f819cd9263f83d77de40341b1e88
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-09-29 09:52:49 +0200

    Add PlaybackStateTests

---
+++ Tests/PlayerTests/Types/PlaybackStateTests.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@testable import Player
+
+import Nimble
+
+final class PlaybackStateTests: TestCase {
+    func testAllCases() {
+        expect(PlaybackState(itemState: .unknown, rate: 0)).to(equal(.idle))
+        expect(PlaybackState(itemState: .unknown, rate: 1)).to(equal(.idle))
+        expect(PlaybackState(itemState: .readyToPlay, rate: 0)).to(equal(.paused))
+        expect(PlaybackState(itemState: .readyToPlay, rate: 1)).to(equal(.playing))
+        expect(PlaybackState(itemState: .ended, rate: 0)).to(equal(.ended))
+        expect(PlaybackState(itemState: .ended, rate: 1)).to(equal(.ended))
+    }
+}
