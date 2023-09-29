555152f81119764dc6c447add6f32984c4b4df3e
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-09-29 15:38:13 +0200

    Merge some AVPlayer tests

--- Tests/PlayerTests/Publishers/AVPlayer/AVPlayerPublisherTests.swift
+++ Tests/PlayerTests/Publishers/AVPlayer/AVPlayerPublisherTests.swift
@@ -7,19 +7,41 @@
 @testable import Player
 
 import AVFoundation
-import Circumspect
 import Combine
 import Streams
 
-final class AVQueuePlayerTimeRangePublisherTests: TestCase {
-    private static func seekableTimeRangePublisher(for player: AVQueuePlayer) -> AnyPublisher<CMTimeRange, Never> {
+final class AVPlayerPublisherTests: TestCase {
+    private static func errorPublisher(for player: AVPlayer) -> AnyPublisher<Error?, Never> {
+        player.errorPublisher()
+            .removeDuplicates { $0 as? NSError == $1 as? NSError }
+            .eraseToAnyPublisher()
+    }
+
+    private static func seekableTimeRangePublisher(for player: AVPlayer) -> AnyPublisher<CMTimeRange, Never> {
         player.timePropertiesPublisher()
             .map(\.seekableTimeRange)
             .removeDuplicates()
             .eraseToAnyPublisher()
     }
 
-    func testEmpty() {
+    func testErrorEmpty() {
+        let player = AVQueuePlayer()
+        expectNothingPublished(from: Self.errorPublisher(for: player), during: .milliseconds(100))
+    }
+
+    func testError() {
+        let player = AVQueuePlayer(
+            playerItem: .init(url: Stream.unavailable.url)
+        )
+        expectPublished(
+            values: [nil, PlayerError.resourceNotFound],
+            from: Self.errorPublisher(for: player),
+            to: beEqual,
+            during: .milliseconds(500)
+        )
+    }
+
+    func testTimeRangeEmpty() {
         let player = AVQueuePlayer()
         expectAtLeastEqualPublished(
             values: [.invalid],
@@ -27,7 +49,7 @@ final class AVQueuePlayerTimeRangePublisherTests: TestCase {
         )
     }
 
-    func testItemLifeCycle() {
+    func testTimeRangeLifeCycle() {
         let player = AVQueuePlayer(
             playerItem: .init(url: Stream.shortOnDemand.url)
         )
