0cdcd0c17f756e7c88fced328af973ebe8ca3a18
Samuel Défago <defagos@gmail.com>
2023-10-01 18:20:06 +0200

    Document public APIs

--- Sources/Player/Types/PlayerProperties.swift
+++ Sources/Player/Types/PlayerProperties.swift
@@ -6,6 +6,7 @@
 
 import CoreMedia
 
+/// A type describing player propeerties.
 public struct PlayerProperties: Equatable {
     static var empty: Self {
         .init(
@@ -24,40 +25,57 @@ public struct PlayerProperties: Equatable {
     let playbackProperties: PlaybackProperties
     let isEmpty: Bool
 
+    /// A Boolean describing whether the player is currently seeking to another position.
     public let isSeeking: Bool
 
+    /// The player rate.
     public var rate: Float {
         playbackProperties.rate
     }
 
+    /// A Boolean describing whether the player is currently playing video in external playback mode.
     public var isExternalPlaybackActive: Bool {
         playbackProperties.isExternalPlaybackActive
     }
 
+    /// A Boolean describing whether the player is currently muted.
     public var isMuted: Bool {
         playbackProperties.isMuted
     }
 
+    /// The player playback state.
     public var playbackState: PlaybackState {
         .init(itemState: itemProperties.state, rate: rate)
     }
 
+    /// A Boolean describing whether the player is currently buffering.
     public var isBuffering: Bool {
         !isEmpty && !timeProperties.isPlaybackLikelyToKeepUp
     }
 
+    /// The buffer position.
+    ///
+    /// Returns a value between 0 and 1 indicating up to where content has been loaded and is available for
+    /// playback.
     public var buffer: Float {
         timeProperties.buffer
     }
 
+    /// A Boolean describing whether the player is currently busy (buffering or seeking).
     public var isBusy: Bool {
         isBuffering || isSeeking
     }
 
+    /// The duration of a chunk for the currently played item.
+    ///
+    /// Might be `.invalid` when no content is being played or when unknown.
     public var chunkDuration: CMTime {
         itemProperties.chunkDuration
     }
 
+    /// The current presentation size.
+    ///
+    /// Might be zero for audio content or `nil` when unknown.
     public var presentationSize: CGSize? {
         itemProperties.presentationSize
     }
