802d5dd7a2762ed53d8b53d6d055c3628bb75115
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-10-03 14:03:40 +0200

    Add a way to assign to a binding the value emitted by a publisher

---
+++ Sources/Core/View.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Combine
+import SwiftUI
+
+public extension View {
+    /// Assigns to a binding property a value emitted by the given publisher.
+    /// - Parameters:
+    ///   - publisher: The publisher to subscribe to.
+    ///   - keyPath: The key path to extract.
+    ///   - binding: The binding to which the value must be assigned.
+    ///   - Returns: A view that fills the given binding when the `publisher` emits an
+    ///   event.
+    func onReceive<P, T>(_ publisher: P, assign keyPath: KeyPath<P.Output, T>, to binding: Binding<T>) -> some View
+        where P: Publisher, P.Failure == Never, T: Equatable {
+            onReceive(publisher.slice(at: keyPath)) { output in
+                binding.wrappedValue = output
+            }
+    }
+}
