9a111b48b0a353c882d407e0d9f8f44e07189194
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-10-21 18:08:42 +0200

    Add two playback views

--- Demo/Sources/Showcase/PictureInPictureView.swift
+++ Demo/Sources/Showcase/PictureInPictureView.swift
@@ -11,7 +11,41 @@ struct PictureInPictureView: View {
     @ObservedObject var player: Player
 
     var body: some View {
-        EmptyView()
+        VStack(spacing: 0) {
+            playbackView()
+            playbackView()
+        }
+        .background(.black)
+        .onAppear {
+            play()
+        }
+    }
+}
+
+extension PictureInPictureView {
+    @ViewBuilder
+    private func playbackView() -> some View {
+        ZStack {
+            VideoView(player: player)
+            playbackButton()
+        }
+    }
+
+    @ViewBuilder
+    private func playbackButton() -> some View {
+        Button(action: player.togglePlayPause) {
+            Image(systemName: player.playbackState == .playing ? "pause.circle.fill" : "play.circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50)
+                .tint(.white)
+                .shadow(radius: 5)
+        }
+    }
+
+    private func play() {
+        player.append(.youTube(videoId: "ApM_KEr1ktQ"))
+        player.play()
     }
 }
 
