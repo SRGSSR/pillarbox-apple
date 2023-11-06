3b52a4fb6ce46c4928c7659d5e95c4f9a5c61774
Samuel Défago <defagos@gmail.com>
2023-11-06 08:31:12 +0100

    Wrap AVPlayerViewController in system video view

--- Sources/Player/UserInterface/SystemVideoView.swift
+++ Sources/Player/UserInterface/SystemVideoView.swift
@@ -7,49 +7,36 @@
 import AVKit
 import SwiftUI
 
+private struct _SystemVideoView: UIViewControllerRepresentable {
+    let player: Player
+
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        .init()
+    }
+
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        uiViewController.player = player.systemPlayer
+    }
+}
+
 /// A view providing the standard system playback user experience.
-///
-/// ### Remark
-///
-/// A bug in AVKit currently makes `SystemVideoView` leak resources after having interacted with the playback
-/// button on iOS 16. This issue has been reported to Apple as FB11934227.
-public struct SystemVideoView<VideoOverlay>: View where VideoOverlay: View {
+public struct SystemVideoView: View {
     private let player: Player
-    private let videoOverlay: VideoOverlay
 
     public var body: some View {
-        VideoPlayer(player: player.queuePlayer) {
-            videoOverlay
-        }
+        _SystemVideoView(player: player)
 #if os(tvOS)
-        .onDisappear {
-            // Avoid sound continuing in background on tvOS, see https://github.com/SRGSSR/pillarbox-apple/issues/520.
-            player.pause()
-        }
+            .onDisappear {
+                // Avoid sound continuing in background on tvOS, see https://github.com/SRGSSR/pillarbox-apple/issues/520
+                player.pause()
+            }
 #endif
     }
 
     /// Creates the view.
     ///
-    /// - Parameters:
-    ///   - player: The player whose contents will be rendered.
-    ///   - videoOverlay: A closure returning an overlay view to be placed atop the player's content. This view is
-    ///     fully interactive, but is placed below the system-provided playback controls and will only receive unhandled
-    ///     events.
-    public init(player: Player, @ViewBuilder videoOverlay: () -> VideoOverlay) {
+    /// - Parameter player: The player whose contents will be rendered.
+    public init(player: Player) {
         self.player = player
-        self.videoOverlay = videoOverlay()
-    }
-}
-
-public extension SystemVideoView where VideoOverlay == EmptyView {
-    /// Creates the view.
-    /// 
-    /// - Parameters:
-    ///   - player: The player whose contents will be rendered.
-    init(player: Player) {
-        self.init(player: player) {
-            EmptyView()
-        }
     }
 }
