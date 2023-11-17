0c817771d826ef093a37d58b2c787f3f15b0f2bf
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-11-17 10:54:39 +0100

    Fix tvOS

--- Demo/Sources/Showcase/SourceCodeViewable.swift
+++ Demo/Sources/Showcase/SourceCodeViewable.swift
@@ -28,6 +28,7 @@ protocol SourceCodeViewable {
 extension View {
     @ViewBuilder
     func sourceCode(of objectType: (any SourceCodeViewable.Type)?) -> some View {
+#if os(iOS)
         if let objectType, let url = gitHubUrl(for: objectType) {
             swipeActions {
                 SourceCodeButton {
@@ -38,6 +39,9 @@ extension View {
         else {
             self
         }
+#else
+        self
+#endif
     }
 
     private func gitHubUrl(for objectType: any SourceCodeViewable.Type) -> URL? {
