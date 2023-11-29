b8fac41f5104a11f454a16346961bcb9d6414291
Samuel Défago <defagos@gmail.com>
2023-11-29 17:50:38 +0100

    Add progress tracking tutorial steps

--- Sources/Player/Player.docc/Resources/Sources/progress-tracker-tutorial-2.swift
+++ Sources/Player/Player.docc/Resources/Sources/progress-tracker-tutorial-2.swift
@@ -1,13 +1,9 @@
 import Player
 import SwiftUI
 
-struct ProgressTrackerTutorial: View {
+struct ContentView: View {
     @StateObject private var player = Player(
         item: .simple(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!)
-    )
-
-    @StateObject private var progressTracker = ProgressTracker(
-        interval: .init(value: 1, timescale: 10)
     )
 
     var body: some View {
