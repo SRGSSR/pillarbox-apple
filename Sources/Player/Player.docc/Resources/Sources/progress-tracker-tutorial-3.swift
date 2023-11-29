f10ffdd207596d696561cfbc115afa859353adee
Samuel Défago <defagos@gmail.com>
2023-11-29 14:49:13 +0100

    Prepare tutorial template

---
+++ Sources/Player/Player.docc/Resources/Sources/progress-tracker-tutorial-3.swift
@@ -0,0 +1,18 @@
+import Player
+import SwiftUI
+
+struct ProgressTrackerTutorial: View {
+    @StateObject private var player = Player(
+        item: .simple(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!)
+    )
+
+    @StateObject private var progressTracker = ProgressTracker(
+        interval: .init(value: 1, timescale: 10)
+    )
+
+    var body: some View {
+        VideoView(player: player)
+            .onAppear(perform: player.play)
+            .bind(progressTracker, to: player)
+    }
+}
