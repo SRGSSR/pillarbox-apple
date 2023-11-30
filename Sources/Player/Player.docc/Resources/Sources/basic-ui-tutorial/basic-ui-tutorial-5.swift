0411eafb0107b050c142025c90c533c82efbb607
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-11-30 14:21:33 +0100

    Update section 1

--- Sources/Player/Player.docc/Resources/Sources/basic-ui-tutorial/basic-ui-tutorial-5.swift
+++ Sources/Player/Player.docc/Resources/Sources/basic-ui-tutorial/basic-ui-tutorial-5.swift
@@ -2,8 +2,9 @@ import Player
 import SwiftUI
 
 struct ContentView: View {
-    private let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!
-    @StateObject private var player = Player(item: .simple(url: url))
+    @StateObject private var player = Player(
+        item: .simple(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!)
+    )
 
     var body: some View {
         ZStack {
@@ -11,7 +12,8 @@ struct ContentView: View {
             Button(action: player.togglePlayPause) {
                 Image(systemName: "play.circle.fill")
                     .resizable()
-                    .frame(width: 50, height: 50)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50)
             }
         }
     }
--- Sources/Player/Player.docc/Resources/Sources/basic-ui-tutorial/basic-ui-tutorial-6.swift
+++ Sources/Player/Player.docc/Resources/Sources/basic-ui-tutorial/basic-ui-tutorial-6.swift
@@ -2,19 +2,19 @@ import Player
 import SwiftUI
 
 struct ContentView: View {
-    private let url = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!
-    @StateObject private var player = Player(item: .simple(url: url))
-    @State private var state: PlaybackState = .idle
+    @StateObject private var player = Player(
+        item: .simple(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!)
+    )
 
     var body: some View {
         ZStack {
             VideoView(player: player)
             Button(action: player.togglePlayPause) {
-                Image(systemName: "play.circle.fill")
+                Image(systemName: player.playbackState == .playing ? "pause.circle.fill" : "play.circle.fill")
                     .resizable()
-                    .frame(width: 50, height: 50)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50)
             }
         }
-        .onReceive(player: player, assign: \.playbackState, to: $state)
     }
 }
