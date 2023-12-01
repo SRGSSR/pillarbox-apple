f044e641514488233c153491de92da0886aa6fcd
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-12-01 19:11:32 +0100

    Add Control Center tutorial (#676)

---
+++ Sources/Player/Player.docc/Tutorials/integrating-with-control-center/integrating-with-control-center-2-1.swift
@@ -0,0 +1,21 @@
+import Player
+import SwiftUI
+
+private struct Metadata: AssetMetadata {}
+
+struct ContentView: View {
+    @StateObject private var player = Player(item: .simple(
+        url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!,
+        metadata: Metadata()
+    ))
+
+    var body: some View {
+        ZStack {
+            VideoView(player: player)
+        }
+        .onAppear {
+            player.play()
+            player.becomeActive()
+        }
+    }
+}
---
+++ Sources/Player/Player.docc/Tutorials/integrating-with-control-center/integrating-with-control-center-2-2.swift
@@ -0,0 +1,25 @@
+import Player
+import SwiftUI
+
+private struct Metadata: AssetMetadata {
+    func nowPlayingMetadata() -> NowPlayingMetadata {
+        .init(title: "🍎", subtitle: "🍏", image: .apple)
+    }
+}
+
+struct ContentView: View {
+    @StateObject private var player = Player(item: .simple(
+        url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!,
+        metadata: Metadata()
+    ))
+
+    var body: some View {
+        ZStack {
+            VideoView(player: player)
+        }
+        .onAppear {
+            player.play()
+            player.becomeActive()
+        }
+    }
+}
