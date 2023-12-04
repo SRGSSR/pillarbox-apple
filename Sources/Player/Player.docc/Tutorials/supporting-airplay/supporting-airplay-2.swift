5e2992c77a4cdd9c337877888dd5b0a608bbafbd
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-12-04 13:24:52 +0100

    Add code

---
+++ Sources/Player/Player.docc/Tutorials/supporting-airplay/supporting-airplay-2.swift
@@ -0,0 +1,18 @@
+import Player
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject private var player = Player(
+        item: .simple(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!)
+    )
+
+    var body: some View {
+        ZStack {
+            VideoView(player: player)
+            RoutePickerView()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+        }
+    }
+}
