8108748e5b24ccd1c3ab3c3af3b4975afc556725
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2023-12-05 15:36:37 +0100

    Update AirPlay tuto

--- Sources/Player/Player.docc/Tutorials/supporting-airplay/supporting-airplay-1-1.swift
+++ Sources/Player/Player.docc/Tutorials/supporting-airplay/supporting-airplay-1-1.swift
@@ -1,3 +1,11 @@
-import AVFoundation
+import AVFAudio
 
-try? AVAudioSession.sharedInstance().setCategory(.playback)
+/*
+You can activate the audio session at any time after setting its category,
+but it’s generally preferable to defer this call until your app begins audio playback.
+Deferring the call ensures that you won’t prematurely interrupt
+any other background audio that may be in progress.
+*/
+func configureAudioSession() {
+    try? AVAudioSession.sharedInstance().setCategory(.playback)
+}
