cdc0cf2dba19f94593a3ed8fe7360f22e2578e12
Samuel Défago <defagos@gmail.com>
2023-12-06 09:57:35 +0100

    Add official source key constants

--- Tests/AnalyticsTests/TestCase.swift
+++ Tests/AnalyticsTests/TestCase.swift
@@ -26,7 +26,7 @@ class TestCase: XCTestCase {
     override class func setUp() {
         PollingDefaults.timeout = .seconds(20)
         try? Analytics.shared.start(
-            with: .init(vendor: .SRG, sourceKey: "39ae8f94-595c-4ca4-81f7-fb7748bd3f04", appSiteName: "site"),
+            with: .init(vendor: .SRG, sourceKey: .developmentSourceKey, appSiteName: "site"),
             dataSource: dataSource
         )
     }
