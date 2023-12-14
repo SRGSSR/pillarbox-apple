4b7a54cff386760b71eaf0d0c09718d0441f641f
Samuel Défago <defagos@gmail.com>
2023-12-14 15:38:24 +0100

    Remove player layout setting

--- Demo/Sources/Settings/UserDefaults.swift
+++ Demo/Sources/Settings/UserDefaults.swift
@@ -20,11 +20,6 @@ enum SeekBehaviorSetting: Int {
 // For more information see https://stackoverflow.com/a/47856467/760435
 extension UserDefaults {
     static let presenterModeEnabledKey = "presenterModeEnabled"
-
-#if os(iOS)
-    static let playerLayoutKey = "playerLayout"
-#endif
-
     static let smartNavigationEnabledKey = "smartNavigationEnabled"
     static let seekBehaviorSettingKey = "seekBehaviorSetting"
     static let serverSettingKey = "serverSetting"
@@ -32,12 +27,6 @@ extension UserDefaults {
     @objc dynamic var presenterModeEnabled: Bool {
         bool(forKey: Self.presenterModeEnabledKey)
     }
-
-#if os(iOS)
-    @objc dynamic var playerLayout: PlayerLayout {
-        .init(rawValue: integer(forKey: Self.playerLayoutKey)) ?? .custom
-    }
-#endif
 
     @objc dynamic var smartNavigationEnabled: Bool {
         bool(forKey: Self.smartNavigationEnabledKey)
