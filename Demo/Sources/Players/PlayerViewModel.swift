31ae16003b5ce70018d35754d6fefb73e4d2809d
Samuel Défago <defagos@gmail.com>
2023-12-15 15:43:54 +0100

    Fix pauses (rough)

--- Demo/Sources/Players/PlayerViewModel.swift
+++ Demo/Sources/Players/PlayerViewModel.swift
@@ -4,17 +4,21 @@
 //  License information is available from the LICENSE file.
 //
 
+import Foundation
 import Player
 
 final class PlayerViewModel {
     var media: Media? {
         didSet {
             guard media != oldValue else { return }
-            if let playerItem = media?.playerItem() {
-                player.items = [playerItem]
-            }
-            else {
-                player.removeAllItems()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                print("--> update media")
+                if let playerItem = self.media?.playerItem() {
+                    self.player.items = [playerItem]
+                }
+                else {
+                    self.player.removeAllItems()
+                }
             }
         }
     }
