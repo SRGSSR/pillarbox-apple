33897b6b1bb8888493585fc0c7de71670bb9afd2
Samuel Défago <defagos@gmail.com>
2023-12-21 00:18:17 +0100

    Remove superfluous modifier

--- Demo/Sources/Views/InteractiveVideoView~ios.swift
+++ Demo/Sources/Views/InteractiveVideoView~ios.swift
@@ -34,7 +34,6 @@ struct InteractiveVideoView: View {
                             initialTranslation = translation
                         }
                 )
-                .ignoresSafeArea()
         }
     }
 
