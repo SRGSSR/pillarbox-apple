26f76803e20b427bb8f68582e748f171446976dd
Samuel Défago <defagos@gmail.com>
2023-12-21 01:00:21 +0100

    Update tutorial sample code

--- Demo/Tutorials/SupportingBasicPictureInPicture.swift
+++ Demo/Tutorials/SupportingBasicPictureInPicture.swift
@@ -1,7 +1,13 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
 import Player
 import SwiftUI
 
-struct ContentView: View {
+struct SupportingBasicPictureInPicture: View {
     @StateObject private var player = Player(
         item: .simple(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_ts/master.m3u8")!)
     )
@@ -15,3 +21,4 @@ struct ContentView: View {
         .onAppear(perform: player.play)
     }
 }
+
