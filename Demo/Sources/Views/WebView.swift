023aed233ef06b24ee36fa7f2abc7470c4a8036a
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-01-02 17:55:42 +0100

    Fix tvOS

--- Demo/Sources/Views/WebView.swift
+++ Demo/Sources/Views/WebView.swift
@@ -5,6 +5,8 @@
 //
 
 import SwiftUI
+
+#if os(iOS)
 import WebKit
 
 private struct _WebView: UIViewRepresentable {
@@ -18,13 +20,16 @@ private struct _WebView: UIViewRepresentable {
 
     func updateUIView(_ uiView: UIViewType, context: Context) {}
 }
+#endif
 
 struct WebView: View {
     let url: URL
 
     var body: some View {
         ZStack(alignment: .topLeading) {
+#if os(iOS)
             _WebView(url: url)
+#endif
             CloseButton()
         }
         .ignoresSafeArea(edges: .bottom)
