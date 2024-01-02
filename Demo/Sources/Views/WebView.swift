f590f6bf4958a291439f489c7e07a8379eae45bd
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-01-02 13:53:48 +0100

    Introduce WebView

---
+++ Demo/Sources/Views/WebView.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> some UIView {
+        let webView = WKWebView(frame: .zero)
+        webView.load(.init(url: url))
+        return webView
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
+}
