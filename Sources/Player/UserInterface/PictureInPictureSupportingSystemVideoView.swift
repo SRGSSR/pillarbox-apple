b3e9635337300081d7792e10750108ceeed0f3a1
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-01-05 08:31:57 +0100

    Use a coordinator to be able to listen the player events

--- Sources/Player/UserInterface/PictureInPictureSupportingSystemVideoView.swift
+++ Sources/Player/UserInterface/PictureInPictureSupportingSystemVideoView.swift
@@ -27,8 +27,13 @@ struct PictureInPictureSupportingSystemVideoView: UIViewControllerRepresentable 
         PictureInPicture.shared.system.relinquish(for: uiViewController)
     }
 
-    func makeUIViewController(context: Context) -> AVPlayerViewController {
+    func makeCoordinator() -> Coordinator {
         let controller = PictureInPicture.shared.system.playerViewController ?? PlayerViewController()
+        return Coordinator(player: player, controller: controller)
+    }
+
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        let controller = context.coordinator.controller
         controller.allowsPictureInPicturePlayback = true
         PictureInPicture.shared.system.acquire(for: controller)
         return controller
@@ -37,24 +42,39 @@ struct PictureInPictureSupportingSystemVideoView: UIViewControllerRepresentable 
     func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
         uiViewController.player = player.systemPlayer
         uiViewController.videoGravity = gravity
-        configureSpeeds(controller: uiViewController)
+#if os(tvOS)
+        context.coordinator.configureSpeeds(controller: uiViewController)
+#endif
     }
+}
 
-    private func configureSpeeds(controller: AVPlayerViewController) {
-        let speedActions = AVPlaybackSpeed.systemDefaultSpeeds.map(\.rate).map { speed in
-            UIAction(title: String(format: "%g×", speed), state: player.effectivePlaybackSpeed == speed ? .on : .off) { action in
-                player.setDesiredPlaybackSpeed(speed)
-                action.state = .on
-            }
+extension PictureInPictureSupportingSystemVideoView {
+    class Coordinator {
+        let player: Player
+        let controller: AVPlayerViewController
+
+        init(player: Player, controller: AVPlayerViewController) {
+            self.player = player
+            self.controller = controller
         }
 
-        controller.transportBarCustomMenuItems = [
-            UIMenu(
-                title: "Speed",
-                image: UIImage(systemName: "speedometer"),
-                options: [.singleSelection],
-                children: speedActions
-            )
-        ]
+        @available(iOS, unavailable)
+        func configureSpeeds(controller: AVPlayerViewController) {
+            let speedActions = AVPlaybackSpeed.systemDefaultSpeeds.map(\.rate).map { [weak self] speed in
+                UIAction(title: String(format: "%g×", speed), state: self?.player.effectivePlaybackSpeed == speed ? .on : .off) { action in
+                    self?.player.setDesiredPlaybackSpeed(speed)
+                    action.state = .on
+                }
+            }
+
+            controller.transportBarCustomMenuItems = [
+                UIMenu(
+                    title: "Speed",
+                    image: UIImage(systemName: "speedometer"),
+                    options: [.singleSelection],
+                    children: speedActions
+                )
+            ]
+        }
     }
 }
