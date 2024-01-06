5b249498cdab04f23b55326db7f26fe9caf4b3ca
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-01-06 19:40:07 +0100

    Move the coordinator in a dedicated file

---
+++ Sources/Player/UserInterface/AVPlayerViewControllerSpeedCoordinator~tvos.swift
@@ -0,0 +1,74 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import AVKit
+import Combine
+import UIKit
+
+@available(iOS, unavailable)
+class AVPlayerViewControllerSpeedCoordinator {
+    let player: Player
+    let controller: AVPlayerViewController
+    private var cancellables = Set<AnyCancellable>()
+
+    init(player: Player, controller: AVPlayerViewController) {
+        self.player = player
+        self.controller = controller
+        configurePlaybackSpeedPublisher(player: player, controller: controller)
+    }
+
+    private func configurePlaybackSpeedPublisher(player: Player, controller: AVPlayerViewController) {
+        Publishers.CombineLatest(
+            player.playbackSpeedUpdatePublisher(),
+            player.$_playbackSpeed
+        )
+        .receiveOnMainThread()
+        .sink { speedUpdate, speed in
+            if case .range(1...1) = speedUpdate {
+                controller.transportBarCustomMenuItems = []
+            }
+            else if case let .range(range) = speedUpdate, let range {
+                Self.configureSpeeds(for: player, controller: controller, range: range, speed: speed.effectiveValue)
+            }
+        }
+        .store(in: &cancellables)
+    }
+}
+
+extension AVPlayerViewControllerSpeedCoordinator {
+    private static func allowedSpeeds(from range: ClosedRange<Float>) -> Set<Float> {
+        Set(
+            AVPlaybackSpeed.systemDefaultSpeeds
+                .map(\.rate)
+                .filter { rate in
+                    range.lowerBound <= rate && rate <= range.upperBound
+                }
+        )
+    }
+
+    private static func configureSpeeds(
+        for player: Player,
+        controller: AVPlayerViewController,
+        range: ClosedRange<Float>,
+        speed: Float
+    ) {
+        let speedActions = allowedSpeeds(from: range).sorted().map { allowedSpeed in
+            UIAction(title: String(format: "%g×", allowedSpeed), state: speed == allowedSpeed ? .on : .off) { [weak player] action in
+                player?.playbackSpeed.wrappedValue = allowedSpeed
+                action.state = .on
+            }
+        }
+
+        controller.transportBarCustomMenuItems = [
+            UIMenu(
+                title: "Playback Speed",
+                image: UIImage(systemName: "speedometer"),
+                options: [.singleSelection],
+                children: speedActions
+            )
+        ]
+    }
+}
--- Sources/Player/UserInterface/PictureInPictureSupportingSystemVideoView.swift
+++ Sources/Player/UserInterface/PictureInPictureSupportingSystemVideoView.swift
@@ -5,7 +5,6 @@
 //
 
 import AVKit
-import Combine
 import SwiftUI
 
 private class PlayerViewController: AVPlayerViewController {
@@ -24,17 +23,29 @@ struct PictureInPictureSupportingSystemVideoView: UIViewControllerRepresentable 
     let player: Player
     let gravity: AVLayerVideoGravity
 
+#if os(iOS)
     static func dismantleUIViewController(_ uiViewController: AVPlayerViewController, coordinator: ()) {
         PictureInPicture.shared.system.relinquish(for: uiViewController)
     }
-
-    func makeCoordinator() -> Coordinator {
-        let controller = PictureInPicture.shared.system.playerViewController ?? PlayerViewController()
-        return Coordinator(player: player, controller: controller)
+#else
+    static func dismantleUIViewController(_ uiViewController: AVPlayerViewController, coordinator: AVPlayerViewControllerSpeedCoordinator) {
+        PictureInPicture.shared.system.relinquish(for: uiViewController)
     }
 
+    func makeCoordinator() -> AVPlayerViewControllerSpeedCoordinator {
+        .init(
+            player: player,
+            controller: PictureInPicture.shared.system.playerViewController ?? PlayerViewController()
+        )
+    }
+#endif
+
     func makeUIViewController(context: Context) -> AVPlayerViewController {
+#if os(iOS)
+        let controller = PictureInPicture.shared.system.playerViewController ?? PlayerViewController()
+#else
         let controller = context.coordinator.controller
+#endif
         controller.allowsPictureInPicturePlayback = true
         PictureInPicture.shared.system.acquire(for: controller)
         return controller
@@ -43,76 +54,5 @@ struct PictureInPictureSupportingSystemVideoView: UIViewControllerRepresentable 
     func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
         uiViewController.player = player.systemPlayer
         uiViewController.videoGravity = gravity
-    }
-}
-
-extension PictureInPictureSupportingSystemVideoView {
-    class Coordinator {
-        let player: Player
-        let controller: AVPlayerViewController
-#if os(tvOS)
-        private var cancellables = Set<AnyCancellable>()
-#endif
-
-        init(player: Player, controller: AVPlayerViewController) {
-            self.player = player
-            self.controller = controller
-
-#if os(tvOS)
-            configurePlaybackSpeedPublisher(player: player, controller: controller)
-#endif
-        }
-
-#if os(tvOS)
-        private func configurePlaybackSpeedPublisher(player: Player, controller: AVPlayerViewController) {
-            Publishers.CombineLatest(
-                player.playbackSpeedUpdatePublisher(),
-                player.$_playbackSpeed
-            )
-            .receiveOnMainThread()
-            .sink { speedUpdate, speed in
-                if case .range(1...1) = speedUpdate {
-                    controller.transportBarCustomMenuItems = []
-                }
-                else if case let .range(range) = speedUpdate, let range {
-                    Self.configureSpeeds(for: player, controller: controller, range: range, speed: speed.effectiveValue)
-                }
-            }
-            .store(in: &cancellables)
-        }
-
-        private static func allowedSpeeds(from range: ClosedRange<Float>) -> Set<Float> {
-            Set(
-                AVPlaybackSpeed.systemDefaultSpeeds
-                    .map(\.rate)
-                    .filter { rate in
-                        range.lowerBound <= rate && rate <= range.upperBound
-                    }
-            )
-        }
-
-        private static func configureSpeeds(
-            for player: Player,
-            controller: AVPlayerViewController,
-            range: ClosedRange<Float>,
-            speed: Float
-        ) {
-            let speedActions = allowedSpeeds(from: range).sorted().map { allowedSpeed in
-                UIAction(title: String(format: "%g×", allowedSpeed), state: speed == allowedSpeed ? .on : .off) { [weak player] action in
-                    player?.playbackSpeed.wrappedValue = allowedSpeed
-                    action.state = .on
-                }
-            }
-
-            controller.transportBarCustomMenuItems = [
-                UIMenu(
-                    title: "Playback Speed",
-                    image: UIImage(systemName: "speedometer"),
-                    options: [.singleSelection],
-                    children: speedActions
-                )
-            ]
-        }
-#endif
     }
 }
