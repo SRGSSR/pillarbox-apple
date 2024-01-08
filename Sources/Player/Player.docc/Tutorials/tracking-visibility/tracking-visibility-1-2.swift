df25e2ba4944dedd7715f66a36bdd88350134c30
Samuel Défago <defagos@gmail.com>
2024-01-08 11:26:43 +0100

    Update remaining rate checks

--- Sources/Player/Player.docc/Tutorials/tracking-visibility/tracking-visibility-1-2.swift
+++ Sources/Player/Player.docc/Tutorials/tracking-visibility/tracking-visibility-1-2.swift
@@ -28,7 +28,7 @@ struct ContentView: View {
     @ViewBuilder
     private func playbackButton() -> some View {
         Button(action: player.togglePlayPause) {
-            Image(systemName: player.playbackState == .playing ? "pause.circle" : "play.circle")
+            Image(systemName: !player.isPlaybackActive ? "play.circle" : "pause.circle")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(height: 60)
