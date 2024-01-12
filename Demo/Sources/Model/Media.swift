ca968a673b382bbe5644ab47dd705545d28f5661
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-01-12 11:52:39 +0100

    Add images for URLs on demo app

--- Demo/Sources/Model/Media.swift
+++ Demo/Sources/Model/Media.swift
@@ -5,6 +5,7 @@
 //
 
 import CoreMedia
+import AVFoundation
 import Combine
 import Foundation
 import PillarboxCoreBusiness
@@ -25,21 +26,16 @@ struct Media: Hashable {
     let title: String
     let description: String?
     let image: URL?
+    let uiImage: UIImage?
     let type: `Type`
     let isMonoscopic: Bool
     let startTime: CMTime
 
-    init(
-        title: String,
-        description: String? = nil,
-        image: URL? = nil,
-        type: `Type`,
-        isMonoscopic: Bool = false,
-        startTime: CMTime = .zero
-    ) {
+    init(title: String, description: String? = nil, image: URL? = nil, uiImage: UIImage? = nil, type: `Type`, isMonoscopic: Bool = false, startTime: CMTime = .zero) {
         self.title = title
         self.description = description
         self.image = image
+        self.uiImage = uiImage
         self.type = type
         self.isMonoscopic = isMonoscopic
         self.startTime = startTime
@@ -59,23 +55,11 @@ struct Media: Hashable {
     func playerItem() -> PlayerItem {
         switch type {
         case let .url(url):
-            return .simple(url: url, metadata: self, trackerAdapters: [
-                DemoTracker.adapter { media in
-                    DemoTracker.Metadata(title: media.title)
-                }
-            ]) { item in
+            return playerItem(for: url) { item in
                 item.seek(at(startTime))
             }
         case let .unbufferedUrl(url):
-            return .simple(
-                url: url,
-                metadata: self,
-                trackerAdapters: [
-                    DemoTracker.adapter { media in
-                        DemoTracker.Metadata(title: media.title)
-                    }
-                ]
-            ) { item in
+            return playerItem(for: url) { item in
                 item.automaticallyPreservesTimeOffsetFromLive = true
                 item.preferredForwardBufferDuration = 1
                 item.seek(at(startTime))
@@ -94,6 +78,34 @@ struct Media: Hashable {
 
 extension Media: AssetMetadata {
     func nowPlayingMetadata() -> NowPlayingMetadata {
-        .init(title: title, subtitle: description)
+        .init(title: title, subtitle: description, image: uiImage)
+    }
+
+    private func playerItem(for url: URL, configuration: @escaping (AVPlayerItem) -> Void = { _ in }) -> PlayerItem {
+        .init(
+            publisher: imagePublisher()
+                .map { image in
+                    .simple(
+                        url: url,
+                        metadata: Media(title: title, description: description, uiImage: image, type: type),
+                        configuration: configuration
+                    )
+                }
+                .eraseToAnyPublisher(),
+            trackerAdapters: [
+                DemoTracker.adapter { media in
+                    DemoTracker.Metadata(title: media.title)
+                }
+            ]
+        )
+    }
+
+    private func imagePublisher() -> AnyPublisher<UIImage?, Never> {
+        guard let image else { return Just(nil).eraseToAnyPublisher() }
+        return URLSession.shared.dataTaskPublisher(for: image)
+            .map(\.data)
+            .map { UIImage(data: $0) }
+            .replaceError(with: nil)
+            .eraseToAnyPublisher()
     }
 }
