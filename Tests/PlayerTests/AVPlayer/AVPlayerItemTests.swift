99d26b60b3d2ef19e2c99113277f53c1af49a754
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-02-06 09:18:41 +0100

    Fix test

--- Tests/PlayerTests/AVPlayer/AVPlayerItemTests.swift
+++ Tests/PlayerTests/AVPlayer/AVPlayerItemTests.swift
@@ -28,12 +28,13 @@ final class AVPlayerItemTests: TestCase {
             PlayerItem.simple(url: Stream.shortOnDemand.url),
             PlayerItem.simple(url: Stream.live.url)
         ]
-        items.forEach { PlayerItem.load(id: $0.id) }
-        let urls = AVPlayerItem.playerItems(from: items, length: 3, fresh: false).compactMap { item -> URL? in
-            guard let asset = item.asset as? AVURLAsset else { return nil }
-            return asset.url
+        expect {
+            AVPlayerItem.playerItems(from: items, length: 3, fresh: false).compactMap { item -> URL? in
+                guard let asset = item.asset as? AVURLAsset else { return nil }
+                return asset.url
+            }
         }
-        expect(urls).to(equal([
+        .toEventually(equal([
             Stream.onDemand.url,
             Stream.shortOnDemand.url,
             Stream.live.url
