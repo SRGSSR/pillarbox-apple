33f2b6fd549b0be57b091d215123033240371ccc
Samuel Défago <defagos@gmail.com>
2024-02-16 07:30:57 +0100

    Replace corner radii with capsule clipping

--- Demo/Sources/Views/PlaybackSlider~ios.swift
+++ Demo/Sources/Views/PlaybackSlider~ios.swift
@@ -72,7 +72,7 @@ struct PlaybackSlider<ValueLabel>: View where ValueLabel: View {
             }
         }
         .frame(height: progressTracker.isInteracting ? 16 : 8)
-        .cornerRadius(progressTracker.isInteracting ? 8 : 4)
+        .clipShape(Capsule())
         .animation(.easeInOut(duration: 0.4), value: progressTracker.isInteracting)
         .onReceive(player: progressTracker.player, assign: \.buffer, to: $buffer)
     }
