7e45a09971ceaf0cd8fe801cef6cb59d9a5abd48
Samuel Défago <defagos@gmail.com>
2024-02-16 07:27:51 +0100

    Immediately trigger slider interactions on press

--- Demo/Sources/Views/PlaybackSlider~ios.swift
+++ Demo/Sources/Views/PlaybackSlider~ios.swift
@@ -62,7 +62,7 @@ struct PlaybackSlider<ValueLabel>: View where ValueLabel: View {
                 rectangle(width: geometry.size.width * CGFloat(progressTracker.progress))
             }
             .gesture(
-                DragGesture(minimumDistance: 1)
+                DragGesture(minimumDistance: 0)
                     .updating($gestureValue) { value, state, _ in
                         state = value
                     }
