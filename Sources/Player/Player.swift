8fcbfa4d3ddd5d1d65ed6b804ee3cbe383bbe7b9
Samuel Défago <defagos@gmail.com>
2024-02-20 10:29:26 +0100

    Delete file associated with Current type, now removed

--- Sources/Player/Player.swift
+++ Sources/Player/Player.swift
@@ -345,3 +345,34 @@ private extension Player {
         .store(in: &cancellables)
     }
 }
+
+private extension Player {
+    func queueItemsPublisher() -> AnyPublisher<[AVPlayerItem], Never> {
+        Publishers.Merge(
+            assetsPublisher()
+                .map { ItemQueueUpdate.assets($0) },
+            queuePlayer.itemTransitionPublisher()
+                .map { ItemQueueUpdate.itemTransition($0) }
+        )
+        .scan(ItemQueue.initial) { queue, update in
+            queue.updated(with: update)
+        }
+        .withPrevious(ItemQueue.initial)
+        .compactMap { [configuration] previous, current in
+            switch current.itemTransition {
+            case let .advance(item):
+                return AVPlayerItem.playerItems(
+                    for: current.assets,
+                    replacing: previous.assets,
+                    currentItem: item,
+                    length: configuration.preloadedItems
+                )
+            case let .stop(item):
+                return [item]
+            case .finish:
+                return nil
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
