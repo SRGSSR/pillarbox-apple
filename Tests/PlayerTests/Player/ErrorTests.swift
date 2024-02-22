e307850a30a733ee1f8ee61d9333bdace03006f7
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-02-22 09:31:05 +0100

    Add testValidStream

--- Tests/PlayerTests/Player/ErrorTests.swift
+++ Tests/PlayerTests/Player/ErrorTests.swift
@@ -13,8 +13,22 @@ import PillarboxCircumspect
 import PillarboxStreams
 
 final class ErrorTests: TestCase {
+    private static func errorCodePublisher(for player: Player) -> AnyPublisher<URLError.Code?, Never> {
+        player.$error
+            .map { error in
+                guard let error else { return nil }
+                return .init(rawValue: (error as NSError).code)
+            }
+            .eraseToAnyPublisher()
+    }
+
     func testNoStream() {
         let player = Player()
+        expectNothingPublishedNext(from: player.$error, during: .milliseconds(500))
+    }
+
+    func testValidStream() {
+        let player = Player(item: .simple(url: Stream.onDemand.url))
         expectNothingPublishedNext(from: player.$error, during: .milliseconds(500))
     }
 }
