SRGSSR/pillarbox-apple

commit 49b3c949be924c8d6bce07b8892836575d95f2fd

Support standard actions at item playback end (#780)
Co-authored-by: Samuel Défago <defagos@gmail.com>

Sources/Player/Publishers/AVPlayerItemPublishers.swift
@@ -38,24 +38,6 @@ extension AVPlayerItem {
         .eraseToAnyPublisher()
     }
 
-    func statusPublisher() -> AnyPublisher<ItemStatus, Never> {
-        Publishers.Merge(
-            publisher(for: \.status)
-                .map { status in
-                    switch status {
-                    case .readyToPlay:
-                        return .readyToPlay
-                    default:
-                        return .unknown
-                    }
-                },
-            NotificationCenter.default.weakPublisher(for: .AVPlayerItemDidPlayToEndTime, object: self)
-                .map { _ in .ended }
-        )
-        .removeDuplicates()
-        .eraseToAnyPublisher()
-    }
-
     private func timePropertiesPublisher() -> AnyPublisher<TimeProperties, Never> {
         Publishers.CombineLatest3(
             publisher(for: \.loadedTimeRanges),
@@ -107,6 +89,45 @@ extension AVPlayerItem {
 }
 
 extension AVPlayerItem {
+    func statusPublisher() -> AnyPublisher<ItemStatus, Never> {
+        Publishers.CombineLatest(
+            publisher(for: \.status),
+            isEndedPublisher()
+        )
+        .map { status, isEnded -> ItemStatus in
+            switch status {
+            case .readyToPlay:
+                return isEnded ? .ended : .readyToPlay
+            default:
+                return .unknown
+            }
+        }
+        .removeDuplicates()
+        .eraseToAnyPublisher()
+    }
+
+    private func isEndedPublisher() -> AnyPublisher<Bool, Never> {
+        Publishers.Merge(endTimeNotificationPublisher(), timebaseUpdateNotificationPublisher())
+            .prepend(false)
+            .removeDuplicates()
+            .eraseToAnyPublisher()
+    }
+
+    private func endTimeNotificationPublisher() -> AnyPublisher<Bool, Never> {
+        NotificationCenter.default.weakPublisher(for: AVPlayerItem.didPlayToEndTimeNotification, object: self)
+            .map { _ in true }
+            .eraseToAnyPublisher()
+    }
+
+    private func timebaseUpdateNotificationPublisher() -> AnyPublisher<Bool, Never> {
+        publisher(for: \.timebase)
+            .compactMap { $0 }
+            .map { _ in false }
+            .eraseToAnyPublisher()
+    }
+}
+
+extension AVPlayerItem {
     func errorPublisher() -> AnyPublisher<Error, Never> {
         Publishers.Merge(
             intrinsicErrorPublisher(),
@@ -126,7 +147,7 @@ extension AVPlayerItem {
     }
 
     private func playbackErrorPublisher() -> AnyPublisher<Error, Never> {
-        NotificationCenter.default.weakPublisher(for: .AVPlayerItemFailedToPlayToEndTime, object: self)
+        NotificationCenter.default.weakPublisher(for: AVPlayerItem.didPlayToEndTimeNotification, object: self)
             .compactMap { notification in
                 guard let error = notification.userInfo?[AVPlayerItemFailedToPlayToEndTimeErrorKey] as? Error else {
                     return nil

Demo/Resources/Localizable.xcstrings
@@ -40,13 +40,19 @@
         }
       }
     },
-    "Active" : {
+    "Action at item end" : {
+
+    },
+    "Active (AirPlay / Control Center)" : {
 
     },
     "Add" : {
 
     },
     "Add a stream to the playlist" : {
+
+    },
+    "Advance" : {
 
     },
     "Android" : {
@@ -127,7 +133,13 @@
     "Mode" : {
 
     },
+    "None" : {
+
+    },
     "Opt-in features" : {
+
+    },
+    "Pause" : {
 
     },
     "Pauses" : {

Demo/Sources/Showcase/OptInView.swift
@@ -20,6 +20,7 @@ struct OptInView: View {
     @State private var isActive = false
     @State private var supportsPictureInPicture = false
     @State private var audiovisualBackgroundPlaybackPolicy: AVPlayerAudiovisualBackgroundPlaybackPolicy = .automatic
+    @State private var actionAtItemEnd: AVPlayer.ActionAtItemEnd = .advance
 
     var body: some View {
         VStack {
@@ -28,12 +29,13 @@ struct OptInView: View {
                 .background(.black)
             List {
                 Toggle(isOn: $isActive) {
-                    Text("Active")
+                    Text("Active (AirPlay / Control Center)")
                 }
                 Toggle(isOn: $supportsPictureInPicture) {
                     Text("Picture in Picture")
                 }
                 audiovisualBackgroundPlaybackPolicyPicker()
+                actionAtItemEndPicker()
 
                 Section {
                     Toggle(isOn: $player.isTrackingEnabled) {
@@ -55,6 +57,9 @@ struct OptInView: View {
         .onChange(of: audiovisualBackgroundPlaybackPolicy) { newValue in
             player.audiovisualBackgroundPlaybackPolicy = newValue
         }
+        .onChange(of: actionAtItemEnd) { newValue in
+            player.actionAtItemEnd = newValue
+        }
         .onAppear(perform: play)
         .tracked(name: "tracking")
     }
@@ -65,6 +70,15 @@ struct OptInView: View {
             Text("Automatic").tag(AVPlayerAudiovisualBackgroundPlaybackPolicy.automatic)
             Text("Continues if possible").tag(AVPlayerAudiovisualBackgroundPlaybackPolicy.continuesIfPossible)
             Text("Pauses").tag(AVPlayerAudiovisualBackgroundPlaybackPolicy.pauses)
+        }
+    }
+
+    @ViewBuilder
+    private func actionAtItemEndPicker() -> some View {
+        Picker("Action at item end", selection: $actionAtItemEnd) {
+            Text("Advance").tag(AVPlayer.ActionAtItemEnd.advance)
+            Text("Pause").tag(AVPlayer.ActionAtItemEnd.pause)
+            Text("None").tag(AVPlayer.ActionAtItemEnd.none)
         }
     }
 

Demo/Sources/Showcase/Stories/StoriesViewModel.swift
@@ -34,7 +34,9 @@ final class StoriesViewModel: ObservableObject {
     }
 
     private static func player(for story: Story) -> Player {
-        Player(item: Media(from: story.template).playerItem(), configuration: .externalPlaybackDisabled)
+        let player = Player(item: Media(from: story.template).playerItem(), configuration: .externalPlaybackDisabled)
+        player.actionAtItemEnd = .pause
+        return player
     }
 
     private static func players(

Sources/Analytics/ComScore/ComScoreLabels.swift
@@ -57,6 +57,11 @@ public struct ComScoreLabels {
 
     // MARK: Streaming labels
 
+    /// The value of `ns_st_id` (media player session identifier).
+    public var ns_st_id: String? {
+        extract()
+    }
+
     /// The value of `ns_st_ev` (streaming event).
     public var ns_st_ev: String? {
         extract()

Sources/Analytics/ComScore/ComScoreTracker.swift
@@ -22,22 +22,12 @@ public final class ComScoreTracker: PlayerItemTracker {
 
     public func enable(for player: Player) {
         self.player = player
-        streamingAnalytics.createPlaybackSession()
-        streamingAnalytics.setMediaPlayerName("Pillarbox")
-        streamingAnalytics.setMediaPlayerVersion(Player.version)
+        createPlaybackSession()
     }
 
     public func updateMetadata(with metadata: [String: String]) {
         self.metadata = metadata
-        let builder = SCORStreamingContentMetadataBuilder()
-        if let globals = Analytics.shared.comScoreGlobals {
-            builder.setCustomLabels(metadata.merging(globals.labels) { _, new in new })
-        }
-        else {
-            builder.setCustomLabels(metadata)
-        }
-        let contentMetadata = SCORStreamingContentMetadata(builder: builder)
-        streamingAnalytics.setMetadata(contentMetadata)
+        addMetadata(metadata)
     }
 
     public func updateProperties(with properties: PlayerProperties) {
@@ -58,11 +48,36 @@ public final class ComScoreTracker: PlayerItemTracker {
         case (false, false):
             streamingAnalytics.notifyBufferStop()
             streamingAnalytics.notifyEvent(for: properties.playbackState, at: properties.rate)
+            renewPlaybackSessionIfNeeded(for: properties.playbackState)
         }
     }
 
     public func disable() {
         streamingAnalytics = ComScoreStreamingAnalytics()
         player = nil
+    }
+
+    private func renewPlaybackSessionIfNeeded(for playbackState: PlaybackState) {
+        guard playbackState == .ended else { return }
+        createPlaybackSession()
+        addMetadata(metadata)
+    }
+
+    private func createPlaybackSession() {
+        streamingAnalytics.createPlaybackSession()
+        streamingAnalytics.setMediaPlayerName("Pillarbox")
+        streamingAnalytics.setMediaPlayerVersion(Player.version)
+    }
+
+    private func addMetadata(_ metadata: [String: String]) {
+        let builder = SCORStreamingContentMetadataBuilder()
+        if let globals = Analytics.shared.comScoreGlobals {
+            builder.setCustomLabels(metadata.merging(globals.labels) { _, new in new })
+        }
+        else {
+            builder.setCustomLabels(metadata)
+        }
+        let contentMetadata = SCORStreamingContentMetadata(builder: builder)
+        streamingAnalytics.setMetadata(contentMetadata)
     }
 }

Sources/Analytics/CommandersAct/CommandersActStreamingAnalytics.swift
@@ -54,10 +54,10 @@ final class CommandersActStreamingAnalytics {
         guard event != lastEvent else { return }
 
         switch (lastEvent, event) {
-        case (.pause, .seek), (.pause, .eof), (.seek, .pause), (.seek, .eof), (.eof, _), (.stop, _):
+        case (.pause, .seek), (.pause, .eof), (.seek, .pause), (.seek, .eof), (.stop, _):
             break
-        case let (.none, event) where event != .play:
-            return
+        case (.none, _) where event != .play, (.eof, _) where event != .play:
+            break
         default:
             sendEvent(event)
         }

Sources/Player/Player.swift
@@ -108,6 +108,18 @@ public final class Player: ObservableObject, Equatable {
         queuePlayer
     }
 
+    /// The action that the player should perform when playback of an item ends.
+    ///
+    /// The default value is `.advance`.
+    public var actionAtItemEnd: AVPlayer.ActionAtItemEnd {
+        get {
+            queuePlayer.actionAtItemEnd
+        }
+        set {
+            queuePlayer.actionAtItemEnd = newValue
+        }
+    }
+
     /// A policy that determines how playback of audiovisual media continues when the app transitions
     /// to the background.
     public var audiovisualBackgroundPlaybackPolicy: AVPlayerAudiovisualBackgroundPlaybackPolicy {

commit 9e16f0c229edb1daccabb552b10ac2d5f1283415

Fix incorrect notification monitoring registration introduced in #780

Sources/Player/Publishers/AVPlayerItemPublishers.swift
@@ -147,7 +147,7 @@ extension AVPlayerItem {
     }
 
     private func playbackErrorPublisher() -> AnyPublisher<Error, Never> {
-        NotificationCenter.default.weakPublisher(for: AVPlayerItem.didPlayToEndTimeNotification, object: self)
+        NotificationCenter.default.weakPublisher(for: AVPlayerItem.failedToPlayToEndTimeNotification, object: self)
             .compactMap { notification in
                 guard let error = notification.userInfo?[AVPlayerItemFailedToPlayToEndTimeErrorKey] as? Error else {
                     return nil