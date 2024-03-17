78e3edee24746c0b854e2406a9ff801fc855db5c
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-03-17 15:37:21 +0100

    Retrieve image url

--- Demo/Sources/PlayerItem/LemanBleu.swift
+++ Demo/Sources/PlayerItem/LemanBleu.swift
@@ -12,6 +12,17 @@ enum LemanBleuError: Error {
     case mediaUrlNotFound
 }
 
+private struct LemanBleu {
+    private static let baseUrl = "https://www.lemanbleu.ch"
+    let imageURL: URL
+    let mediaURL: URL
+
+    init(imagePath: String, mediaPath: String) {
+        self.imageURL = URL(string: Self.baseUrl + imagePath)!
+        self.mediaURL = URL(string: Self.baseUrl + mediaPath)!
+    }
+}
+
 extension PlayerItem {
     static func lemanBleu(for url: URL) -> Self {
         self.init(publisher: asset(for: url))
@@ -21,17 +32,17 @@ extension PlayerItem {
 extension PlayerItem {
     private static func asset(for url: URL) -> AnyPublisher<Asset<Never>, any Error> {
         mediaUrlPublisher(for: url)
-            .tryMap { Asset.simple(url: $0) }
+            .tryMap { Asset.simple(url: $0.mediaURL) }
             .eraseToAnyPublisher()
     }
 
-    private static func mediaUrlPublisher(for url: URL) -> AnyPublisher<URL, LemanBleuError> {
+    private static func mediaUrlPublisher(for url: URL) -> AnyPublisher<LemanBleu, LemanBleuError> {
         URLSession.shared.dataTaskPublisher(for: url)
             .map(\.data)
-            .map { String(data: $0, encoding: .utf8) }
+            .map { String(data: $0, encoding: .utf8)! }
             .tryMap { html in
-                if let mediaURL = try #/data-video-url=".*,(?<url>.*\.mp4)/#.firstMatch(in: html!)?.url {
-                    URL(string: "https://www.lemanbleu.ch\(mediaURL)")!
+                if let data = try #/data-video-url="(?<imagePath>.*),(?<mediaPath>.*)\|/#.firstMatch(in: html) {
+                    LemanBleu(imagePath: String(data.imagePath), mediaPath: String(data.mediaPath))
                 }
                 else {
                     throw LemanBleuError.mediaUrlNotFound
