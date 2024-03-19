46e054e1aa00d071204f43709ef70d356617a830
Samuel Défago <defagos@gmail.com>
2024-03-19 19:57:24 +0100

    Fix linting issues

--- Sources/Player/ControlCenter/NowPlayingInfo.swift
+++ Sources/Player/ControlCenter/NowPlayingInfo.swift
@@ -5,4 +5,7 @@
 //
 
 /// Metadata describing what is currently being played.
+///
+/// Refer to the [official documentation](https://developer.apple.com/documentation/mediaplayer/mpnowplayinginfocenter#1674387)
+/// for a list of available keys.
 public typealias NowPlayingInfo = [String: Any]
