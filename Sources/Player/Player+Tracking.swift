0ee9fb5ca7914e99cc17ab9e115386978dc54549
Samuel Défago <defagos@gmail.com>
2024-03-21 15:30:57 +0100

    Improve naming consistency

--- Sources/Player/Player+Tracking.swift
+++ Sources/Player/Player+Tracking.swift
@@ -8,13 +8,13 @@ import Combine
 import PillarboxCore
 
 extension Player {
-    func trackerPublisher() -> AnyPublisher<Tracker?, Never> {
+    func currentTrackerPublisher() -> AnyPublisher<CurrentTracker?, Never> {
         Publishers.CombineLatest(queuePublisher.slice(at: \.item), $isTrackingEnabled)
             .map { (item: $0, isTrackingEnabled: $1) }
             .scan(nil) { [weak self] tracker, update in
                 if update.isTrackingEnabled, let item = update.item {
                     guard tracker?.item != item else { return tracker }
-                    return Tracker(item: item, player: self)
+                    return CurrentTracker(item: item, player: self)
                 }
                 else {
                     return nil
