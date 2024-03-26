4fa73670be0e5104356d1e474d9626dde674cb67
Samuel Défago <defagos@gmail.com>
2024-03-26 11:27:39 +0100

    Fix linting issue

--- Sources/Player/Types/PlayerMetadata.swift
+++ Sources/Player/Types/PlayerMetadata.swift
@@ -7,14 +7,14 @@
 import AVFoundation
 
 public struct PlayerMetadata: Equatable {
+    static let empty = Self(mediaItemInfo: .init(), metadataItems: [])
+
+    public let mediaItemInfo: NowPlayingInfo
+    public let metadataItems: [AVMetadataItem]
+
     // FIXME: Make NowPlayingInfo a proper equatable type, then remove
     public static func == (lhs: Self, rhs: Self) -> Bool {
         // swiftlint:disable:next legacy_objc_type
         NSDictionary(dictionary: lhs.mediaItemInfo).isEqual(to: rhs.mediaItemInfo) && lhs.metadataItems == rhs.metadataItems
     }
-
-    static let empty = Self(mediaItemInfo: .init(), metadataItems: [])
-
-    public let mediaItemInfo: NowPlayingInfo
-    public let metadataItems: [AVMetadataItem]
 }
