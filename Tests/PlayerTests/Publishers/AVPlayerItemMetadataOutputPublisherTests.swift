a907994ceac563e29d76fd1fc87c7b95125ad186
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-04-03 09:11:31 +0200

    Setup AVPlayerItemMetadataOutputPublisherTests

---
+++ Tests/PlayerTests/Publishers/AVPlayerItemMetadataOutputPublisherTests.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@testable import PillarboxPlayer
+
+import AVFoundation
+import Combine
+import PillarboxStreams
+
+final class AVPlayerItemMetadataOutputPublisherTests: TestCase {
+}
