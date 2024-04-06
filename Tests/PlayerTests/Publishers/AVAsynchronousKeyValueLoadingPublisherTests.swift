e7439b9d19feb0451f6ddfd6b800c2eeae87b8fb
Samuel Défago <defagos@gmail.com>
2024-04-06 21:08:37 +0200

    Test metadata item extraction

--- Tests/PlayerTests/Publishers/AVAsynchronousKeyValueLoadingPublisherTests.swift
+++ Tests/PlayerTests/Publishers/AVAsynchronousKeyValueLoadingPublisherTests.swift
@@ -13,13 +13,13 @@ import PillarboxStreams
 
 // swiftlint:disable:next type_name
 final class AVAsynchronousKeyValueLoadingPublisherTests: TestCase {
-    func testFetch() throws {
+    func testAssetFetch() throws {
         let asset = AVURLAsset(url: Stream.onDemand.url)
         let duration = try waitForSingleOutput(from: asset.propertyPublisher(.duration))
         expect(duration).to(equal(Stream.onDemand.duration, by: beClose(within: 1)))
     }
 
-    func testRepeatedFetch() throws {
+    func testAssetRepeatedFetch() throws {
         let asset = AVURLAsset(url: Stream.onDemand.url)
 
         let duration1 = try waitForSingleOutput(from: asset.propertyPublisher(.duration))
@@ -29,22 +29,34 @@ final class AVAsynchronousKeyValueLoadingPublisherTests: TestCase {
         expect(duration2).to(equal(Stream.onDemand.duration, by: beClose(within: 1)))
     }
 
-    func testFailedFetch() throws {
+    func testAssetFailedFetch() throws {
         let asset = AVURLAsset(url: Stream.unavailable.url)
         let error = try waitForFailure(from: asset.propertyPublisher(.duration))
         expect(error).notTo(beNil())
     }
 
-    func testMultipleFetch() throws {
+    func testAssetMultipleFetch() throws {
         let asset = AVURLAsset(url: Stream.onDemand.url)
         let (duration, preferredRate) = try waitForSingleOutput(from: asset.propertyPublisher(.duration, .preferredRate))
         expect(duration).to(equal(Stream.onDemand.duration, by: beClose(within: 1)))
         expect(preferredRate).to(equal(1))
     }
 
-    func testFailedMultipleFetch() throws {
+    func testAssetFailedMultipleFetch() throws {
         let asset = AVURLAsset(url: Stream.unavailable.url)
         let error = try waitForFailure(from: asset.propertyPublisher(.duration, .preferredRate))
         expect(error).notTo(beNil())
+    }
+
+    func testMetadataItemFetch() throws {
+        let item = AVMetadataItem(for: .commonIdentifierTitle, value: "Title")!
+        let title = try waitForSingleOutput(from: item.propertyPublisher(.stringValue))
+        expect(title).to(equal("Title"))
+    }
+
+    func testMetadataItemFetchWithTypeMismatch() throws {
+        let item = AVMetadataItem(for: .commonIdentifierTitle, value: "Title")!
+        let title = try waitForSingleOutput(from: item.propertyPublisher(.dateValue))
+        expect(title).to(beNil())
     }
 }
