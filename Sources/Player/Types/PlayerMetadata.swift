f7146bf237ac896f99cbac05e76743b042b013cc
Samuel Défago <defagos@gmail.com>
2024-04-09 18:25:49 +0200

    Use first non-empty metadata

--- Sources/Player/Types/PlayerMetadata.swift
+++ Sources/Player/Types/PlayerMetadata.swift
@@ -7,28 +7,28 @@
 import AVFoundation
 
 public struct PlayerMetadata {
-    public struct Data {
+    struct Data {
         static let empty = Self(items: [], timedGroups: [], chapterGroups: [])
 
-        public let items: [MetadataItem]
-        public let timedGroups: [TimedMetadataGroup]
-        public let chapterGroups: [TimedMetadataGroup]
+        let items: [MetadataItem]
+        let timedGroups: [TimedMetadataGroup]
+        let chapterGroups: [TimedMetadataGroup]
     }
 
     static let empty = Self(content: .empty, resource: .empty)
 
-    public let content: Data
-    public let resource: Data
+    let content: Data
+    let resource: Data
 
     public var items: [MetadataItem] {
-        content.items + resource.items
+        !content.items.isEmpty ? content.items : resource.items
     }
 
     public var timedGroups: [TimedMetadataGroup] {
-        content.timedGroups + resource.timedGroups
+        !content.timedGroups.isEmpty ? content.timedGroups : resource.timedGroups
     }
 
     public var chapterGroups: [TimedMetadataGroup] {
-        content.chapterGroups + resource.chapterGroups
+        !content.chapterGroups.isEmpty ? content.chapterGroups : resource.chapterGroups
     }
 }
