8a5491afad662294f3d80bba5902c922fb64fa26
Samuel Défago <defagos@gmail.com>
2024-04-10 08:59:43 +0200

    Remove unused attribute

--- Sources/Player/Types/PlayerMetadata.swift
+++ Sources/Player/Types/PlayerMetadata.swift
@@ -33,12 +33,10 @@ public struct PlayerMetadata {
         !content.chapterGroups.isEmpty ? content.chapterGroups : resource.chapterGroups
     }
 
-    // FIXME: Probably twisting metadata here. Have honest mapping between related AV / MP keys. Map as much as possible.
     var nowPlayingInfo: NowPlayingInfo {
         var nowPlayingInfo = NowPlayingInfo()
         nowPlayingInfo[MPMediaItemPropertyTitle] = Self.metadataItem(with: .commonIdentifierTitle, in: items)?.stringValue
         nowPlayingInfo[MPMediaItemPropertyArtist] = Self.metadataItem(with: .iTunesMetadataTrackSubTitle, in: items)?.stringValue
-        nowPlayingInfo[MPMediaItemPropertyComments] = Self.metadataItem(with: .commonIdentifierDescription, in: items)?.stringValue
 
         if let imageData = Self.metadataItem(with: .commonIdentifierArtwork, in: items)?.dataValue, let image = UIImage(data: imageData) {
             nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size) { _ in image }
