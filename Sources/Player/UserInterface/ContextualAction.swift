e3a9f5231d0a95407200b01c4f451b921bbe894d
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-04-26 10:53:03 +0200

    Introduce ContextualAction

---
+++ Sources/Player/UserInterface/ContextualAction.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import UIKit
+
+/// Actions to present contextually during playback.
+public struct ContextualAction {
+    /// Short display title.
+    public let title: String
+
+    /// Image that can appear next to this action.
+    public let image: UIImage?
+
+    /// The action to execute.
+    public let action: () -> Void
+
+    /// Creates a new contextual action.
+    ///
+    /// - Parameters:
+    ///   - title: Short display title.
+    ///   - image: Image that can appear next to this action.
+    ///   - action: The action to execute.
+    public init(title: String, image: UIImage? = nil, action: @escaping () -> Void) {
+        self.title = title
+        self.image = image
+        self.action = action
+    }
+}
