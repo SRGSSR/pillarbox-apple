b1a9bd614a4ff6e35b9a04e99903d77f78e65ed3
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-04-29 08:59:38 +0200

    Improve testSeekInBlockedTimeRange

--- Tests/PlayerTests/Player/SeekBlockedTimeRangeTests.swift
+++ Tests/PlayerTests/Player/SeekBlockedTimeRangeTests.swift
@@ -11,10 +11,12 @@ import Nimble
 import PillarboxCircumspect
 import PillarboxStreams
 
+private let kBlockedTimeRange = CMTimeRange(start: .init(value: 20, timescale: 1), end: .init(value: 60, timescale: 1))
+
 private struct MockMetadata: AssetMetadata {
     var playerMetadata: PlayerMetadata {
         .init(timeRanges: [
-            .init(kind: .blocked, start: .init(value: 20, timescale: 1), end: .init(value: 60, timescale: 1))
+            .init(kind: .blocked, start: kBlockedTimeRange.start, end: kBlockedTimeRange.end)
         ])
     }
 }
@@ -24,7 +26,8 @@ final class SeekBlockedTimeRangeTests: TestCase {
         let player = Player(item: .simple(url: Stream.onDemand.url, metadata: MockMetadata()))
         expect(player.streamType).toEventually(equal(.onDemand))
         player.seek(at(.init(value: 30, timescale: 1)))
-        expect(20...60).toNever(contain(Int(player.time.seconds)), until: .seconds(3))
+        expect(kBlockedTimeRange.containsTime(player.time)).toNever(beTrue(), until: .seconds(3))
+        expect(player.time).to(equal(kBlockedTimeRange.end))
     }
 
     func testOnDemandStartInBlockedTimeRange() {
