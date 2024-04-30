182b28807eb00794b57232b9a90baf8bd6456d55
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-04-30 13:46:49 +0200

    Update nextUnblockedTimePublisher

--- Sources/Player/Player+TimePublisher.swift
+++ Sources/Player/Player+TimePublisher.swift
@@ -33,17 +33,15 @@ public extension Player {
 
 extension Player {
     func nextUnblockedTimePublisher() -> AnyPublisher<CMTime, Never> {
-        metadataPublisher.map { [queuePlayer] metadata -> AnyPublisher<CMTime, Never> in
-            guard !metadata.blockedTimeRanges.isEmpty else { return Empty().eraseToAnyPublisher() }
-            return Publishers.PeriodicTimePublisher(for: queuePlayer, interval: .init(value: 1, timescale: 10))
-                .compactMap { time in
-                    time.after(timeRanges: metadata.blockedTimeRanges.map { timeRange in
-                        CMTimeRange(start: timeRange.start, end: timeRange.end)
-                    })
-                }
-                .eraseToAnyPublisher()
-        }
-        .switchToLatest()
-        .eraseToAnyPublisher()
+        metadataPublisher
+            .slice(at: \.blockedTimeRanges)
+            .map { [queuePlayer] blockedTimeRanges -> AnyPublisher<CMTime, Never> in
+                guard !blockedTimeRanges.isEmpty else { return Empty().eraseToAnyPublisher() }
+                return Publishers.PeriodicTimePublisher(for: queuePlayer, interval: .init(value: 1, timescale: 10))
+                    .compactMap { $0.after(timeRanges: blockedTimeRanges) }
+                    .eraseToAnyPublisher()
+            }
+            .switchToLatest()
+            .eraseToAnyPublisher()
     }
 }
