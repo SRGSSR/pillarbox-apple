dee76221fdf0e6970cb6738e3208dc629754564f
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-05-07 08:14:31 +0200

    Add a test for nested time range (#868)

--- Tests/PlayerTests/Player/BlockedTimeRangeTests.swift
+++ Tests/PlayerTests/Player/BlockedTimeRangeTests.swift
@@ -13,6 +13,7 @@ import PillarboxStreams
 
 private let kBlockedTimeRange = CMTimeRange(start: .init(value: 20, timescale: 1), end: .init(value: 60, timescale: 1))
 private let kOverlappingBlockedTimeRange = CMTimeRange(start: .init(value: 50, timescale: 1), end: .init(value: 100, timescale: 1))
+private let kNestedBlockedTimeRange = CMTimeRange(start: .init(value: 30, timescale: 1), end: .init(value: 50, timescale: 1))
 
 private struct MetadataWithBlockedTimeRange: AssetMetadata {
     var playerMetadata: PlayerMetadata {
@@ -27,6 +28,15 @@ private struct MetadataWithOverlappingBlockedTimeRanges: AssetMetadata {
         .init(timeRanges: [
             .init(kind: .blocked, start: kBlockedTimeRange.start, end: kBlockedTimeRange.end),
             .init(kind: .blocked, start: kOverlappingBlockedTimeRange.start, end: kOverlappingBlockedTimeRange.end)
+        ])
+    }
+}
+
+private struct MetadataWithNestedBlockedTimeRanges: AssetMetadata {
+    var playerMetadata: PlayerMetadata {
+        .init(timeRanges: [
+            .init(kind: .blocked, start: kBlockedTimeRange.start, end: kBlockedTimeRange.end),
+            .init(kind: .blocked, start: kNestedBlockedTimeRange.start, end: kNestedBlockedTimeRange.end)
         ])
     }
 }
@@ -46,6 +56,14 @@ final class BlockedTimeRangeTests: TestCase {
         player.seek(at(.init(value: 30, timescale: 1)))
         expect(kOverlappingBlockedTimeRange.containsTime(player.time)).toNever(beTrue(), until: .seconds(2))
         expect(player.time).to(equal(kOverlappingBlockedTimeRange.end))
+    }
+
+    func testSeekInNestedBlockedTimeRange() {
+        let player = Player(item: .simple(url: Stream.onDemand.url, metadata: MetadataWithNestedBlockedTimeRanges()))
+        expect(player.streamType).toEventually(equal(.onDemand))
+        player.seek(at(.init(value: 40, timescale: 1)))
+        expect(kNestedBlockedTimeRange.containsTime(player.time)).toNever(beTrue(), until: .seconds(2))
+        expect(player.time).to(equal(kBlockedTimeRange.end))
     }
 
     func testBlockedTimeRangeTraversal() {
