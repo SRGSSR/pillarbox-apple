e04413f9182107dc3f26e7ab0a0609c67e263773
Samuel Défago <defagos@gmail.com>
2024-05-14 14:52:30 +0200

    Add UIImage construction from source

---
+++ Sources/Player/UserInterface/LazyUIImage.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import UIKit
+
+/// A `UImage` that displays an image from an associated source.
+///
+/// The image is loaded when needed and delivered through usual `PlayerMetadata` updates.
+public func LazyUIImage(source: ImageSource) -> UIImage? {
+    source.image
+}
