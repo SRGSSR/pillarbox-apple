1c7cf62cbbd2997e2a066f00520dee93c01a07d9
Samuel Défago <defagos@gmail.com>
2024-05-21 09:06:00 +0200

    Hide implementation details

--- Sources/CoreBusiness/Model/ImageWidth.swift
+++ Sources/CoreBusiness/Model/ImageWidth.swift
@@ -6,23 +6,11 @@
 
 import Foundation
 
-/// An width supported for image resizing.
-public enum ImageWidth: Int {
-    // 240 pixels.
+enum ImageWidth: Int {
     case width240 = 240
-
-    // 320 pixels.
     case width320 = 320
-
-    // 480 pixels.
     case width480 = 480
-
-    // 720 pixels.
     case width720 = 720
-
-    // 960 pixels.
     case width960 = 960
-
-    // 1920 pixels.
     case width1920 = 1920
 }
