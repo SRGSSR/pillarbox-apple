618ac24e72e5ef58ac0d733977c06856133f3df9
Samuel Défago <defagos@gmail.com>
2024-06-13 10:01:18 +0200

    Refactor, continued

--- Sources/Player/Metrics/MetricsState.swift
+++ Sources/Player/Metrics/MetricsState.swift
@@ -12,21 +12,28 @@ struct MetricsState: Equatable {
     private let cache: MetricsCache
     let metrics: Metrics
 
+    private func adding(event: AccessLogEvent, to cache: MetricsCache) -> Self {
+        let total = cache.total.adding(.values(from: event))
+        return .init(
+            cache: cache,
+            metrics: metrics.updated(with: event, total: total)
+        )
+    }
+
+    private func returning(event: AccessLogEvent, in cache: MetricsCache) -> Self {
+        return .init(
+            cache: cache,
+            metrics: metrics.updated(with: event, total: cache.total)
+        )
+    }
+
     func updated(with log: AccessLog) -> Self {
         let cache = cache.updated(with: log.closedEvents)
-
         if let openEvent = log.openEvent {
-            let total = cache.total.adding(.values(from: openEvent))
-            return .init(
-                cache: cache,
-                metrics: metrics.updated(with: openEvent, total: total)
-            )
+            return adding(event: openEvent, to: cache)
         }
         else if let lastClosedEvent = log.closedEvents.last {
-            return .init(
-                cache: cache,
-                metrics: metrics.updated(with: lastClosedEvent, total: cache.total)
-            )
+            return returning(event: lastClosedEvent, in: cache)
         }
         else {
             return self
