7d2baab6ee92549178399e6f90621e722be5b356
Samuel Défago <defagos@gmail.com>
2024-06-17 11:24:57 +0200

    Disable linter warning

--- Demo/Sources/Players/PlaybackView.swift
+++ Demo/Sources/Players/PlaybackView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 #if os(iOS)
 
 // Behavior: h-exp, v-exp
+// swiftlint:disable:next type_body_length
 private struct MainView: View {
     @ObservedObject var player: Player
     @Binding var layout: PlaybackView.Layout
