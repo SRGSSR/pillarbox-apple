a3d96af5c8f6f472acfc791e1417f4b64f5672c6
Samuel Défago <defagos@gmail.com>
2024-07-05 11:16:21 +0200

    Rewrite test to match its purpose

--- Tests/PlayerTests/Player/PlayerTests.swift
+++ Tests/PlayerTests/Player/PlayerTests.swift
@@ -47,8 +47,9 @@ final class PlayerTests: TestCase {
 
     func testMetadataUpdatesMustNotChangePlayerItem() {
         let player = Player(item: .mock(url: Stream.onDemand.url, withMetadataUpdateAfter: 1))
-        let publisher = player.queuePublisher.slice(at: \.itemState.item?.url).compactMap { $0 }
-        expectEqualPublishedNext(values: [Stream.onDemand.url], from: publisher, during: .seconds(2))
+        expect(player.queuePlayer.currentItem?.url).toEventually(equal(Stream.onDemand.url))
+        let currentItem = player.queuePlayer.currentItem
+        expect(player.queuePlayer.currentItem).toAlways(equal(currentItem), until: .seconds(2))
     }
 
     func testRetrieveCurrentValueOnSubscription() {
