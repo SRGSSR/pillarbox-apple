9d79a38972505e8e9ffed7701d611be5cd0649ce
Samuel Défago <defagos@gmail.com>
2024-07-05 17:33:29 +0200

    Provide error domain information

---
+++ Sources/Player/Metrics/ErrorDomain.swift
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+
+/// An error domain.
+public enum ErrorDomain: String {
+    /// Asset-related error.
+    case asset
+
+    /// Resource-related error.
+    case resource
+}
