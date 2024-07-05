c5b6733d6e5c9f41666ffc4644752badf0e34765
Samuel Défago <defagos@gmail.com>
2024-07-05 14:53:44 +0200

    Inhibit empty log updates with dedicated state-update method

--- Sources/Player/Player+Metrics.swift
+++ Sources/Player/Player+Metrics.swift
@@ -14,19 +14,23 @@ public extension Player {
     /// Returns a periodically updated metrics history associated to the current item.
     ///
     /// - Parameters:
-    ///   - interval: The interval at which events must be emitted, according to progress of the current time of the timebase.
+    ///   - interval: The interval at which the history should be updated, according to progress of the current time of
+    ///     the timebase.
     ///   - queue: The queue on which values are published.
     /// - Returns: The publisher.
     ///
     /// Additional non-periodic updates will be published when time jumps or when playback starts or stops. Each
-    /// included ``Metrics/increment`` collates data since the previous periodic update.
+    /// included ``Metrics/increment`` collates data since the entry that precedes it in the history. No updates are
+    /// published if no metrics are provided for the item being played.
     func periodicMetricsPublisher(forInterval interval: CMTime, queue: DispatchQueue = .main) -> AnyPublisher<[Metrics], Never> {
         currentPlayerItemPublisher()
             .map { [queuePlayer] item -> AnyPublisher<[Metrics], Never> in
                 guard let item else { return Just([]).eraseToAnyPublisher() }
                 return Publishers.PeriodicTimePublisher(for: queuePlayer, interval: interval, queue: kMetricsQueue)
-                    .scan(MetricsState.empty) { state, time in
-                        state.updated(with: item.accessLog(), at: time)
+                    .compactMap { _ in item.accessLog()?.events }
+                    .filter { !$0.isEmpty }
+                    .scan(MetricsState.empty) { state, events in
+                        state.updated(with: events, at: item.currentTime())
                     }
                     .withPrevious(MetricsState.empty)
                     .map { $0.current.metrics(from: $0.previous) }
