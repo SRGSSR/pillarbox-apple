2d485a57061991b8428be6a5ac38399c51a96835
Samuel Défago <defagos@gmail.com>
2024-07-09 11:38:04 +0200

    Prepare current tracker

--- Sources/Player/Tracking/CurrentTracker.swift
+++ Sources/Player/Tracking/CurrentTracker.swift
@@ -4,43 +4,12 @@
 //  License information is available from the LICENSE file.
 //
 
-import Combine
+import Foundation
 
-/// Tracks the provided item during its lifecycle.
-///
-/// This class implements a Resource Acquisition Is Initialization (RAII) approach to ensure lifecycle events are
-/// properly balanced.
 final class CurrentTracker {
-    let item: PlayerItem
-    private var cancellables = Set<AnyCancellable>()
+    weak var player: Player?
 
-    init(item: PlayerItem, player: Player) {
-        self.item = item
-        item.enableTrackers(for: player)
-
-        player.propertiesPublisher
-            .receiveOnMainThread()
-            .sink { properties in
-                item.updateTrackerProperties(properties)
-            }
-            .store(in: &cancellables)
-
-        Publishers.Merge(
-            player.queuePublisher
-                .slice(at: \.itemState.item)
-                .compactMap { $0 }
-                .map { $0.metricLog.eventPublisher() }
-                .switchToLatest(),
-            item.metricLog.eventPublisher()
-        )
-        .receiveOnMainThread()
-        .sink { event in
-            item.receiveMetricEvent(event)
-        }
-        .store(in: &cancellables)
-    }
-
-    deinit {
-        item.disableTrackers()
+    init(player: Player) {
+        self.player = player
     }
 }
