0140fbf75667f15ec53d06b9bc19ae0cbd425c0a
Samuel Défago <defagos@gmail.com>
2024-07-13 11:50:07 +0200

    Add AsyncSequencePublisher

---
+++ Sources/Core/AsyncSequencePublisher.swift
@@ -0,0 +1,75 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Combine
+
+@available(iOS 18.0, tvOS 18.0, *)
+public struct AsyncSequencePublisher<Output, Failure>: Publisher where Failure: Error {
+    private let sequence: any AsyncSequence<Output, Failure>
+
+    public init<A>(from sequence: A) where A: AsyncSequence, A.Element == Output, A.Failure == Failure {
+        self.sequence = sequence
+    }
+
+    public func receive<S>(subscriber: S) where S: Subscriber, S.Failure == Failure, S.Input == Output {
+        let subscription = _Subscription(subscriber: subscriber, sequence: sequence)
+        subscriber.receive(subscription: subscription)
+    }
+}
+
+@available(iOS 18.0, tvOS 18.0, *)
+private extension AsyncSequencePublisher {
+    final class _Subscription<S>: Subscription where S: Subscriber, S.Input == Output, S.Failure == Failure {
+        private var subscriber: S?
+        private let sequence: any AsyncSequence<Output, Failure>
+
+        private let buffer = DemandBuffer<Output>()
+        private var task: Task<Void, Error>?
+
+        init(subscriber: S, sequence: any AsyncSequence<Output, Failure>) {
+            self.subscriber = subscriber
+            self.sequence = sequence
+        }
+
+        private func send(_ output: Output) {
+            process(buffer.append(output))
+        }
+
+        private func consume(sequence: any AsyncSequence<Output, Failure>) async throws {
+            do {
+                for try await output in sequence {
+                    self.send(output)
+                }
+                subscriber?.receive(completion: .finished)
+            }
+            catch {
+                subscriber?.receive(completion: .failure(error))
+            }
+        }
+
+        func request(_ demand: Subscribers.Demand) {
+            if task == nil {
+                task = Task {
+                    try await consume(sequence: sequence)
+                }
+            }
+            process(buffer.request(demand))
+        }
+
+        func cancel() {
+            task?.cancel()
+            subscriber = nil
+        }
+
+        private func process(_ values: [Output]) {
+            guard let subscriber else { return }
+            values.forEach { value in
+                let demand = subscriber.receive(value)
+                request(demand)
+            }
+        }
+    }
+}
