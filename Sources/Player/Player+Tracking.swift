2ec430aaa132cf1f6706fb4ae54a0e58a3acdbf9
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-07-19 15:15:01 +0200

    Revert "Release current tracker"
    This reverts commit 137431782dacaa81dc1e52ad58827793e3bb14fb.

--- Sources/Player/Player+Tracking.swift
+++ Sources/Player/Player+Tracking.swift
@@ -12,18 +12,11 @@ extension Player {
         Publishers.CombineLatest(queuePublisher.slice(at: \.item), $isTrackingEnabled)
             .map { (item: $0, isTrackingEnabled: $1) }
             .scan(nil) { [weak self] tracker, next in
-                if let self, !next.isTrackingEnabled {
-                    tracker?.release(player: self)
-                }
                 if let self, next.isTrackingEnabled, let item = next.item {
                     guard tracker?.item !== item else { return tracker }
-                    tracker?.release(player: self)
                     return CurrentTracker(item: item, player: self)
                 }
                 else {
-                    if let self {
-                        tracker?.release(player: self)
-                    }
                     return nil
                 }
             }
