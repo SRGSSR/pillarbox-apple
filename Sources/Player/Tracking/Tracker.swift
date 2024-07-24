5285b677ec577e778f6a52b1e5a270dc281eae6c
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-07-24 09:31:22 +0200

    Remove old player tracker implementation

--- Sources/Player/Tracking/Tracker.swift
+++ Sources/Player/Tracking/Tracker.swift
@@ -4,60 +4,13 @@
 //  License information is available from the LICENSE file.
 //
 
+import AVFoundation
 import Combine
-import Foundation
 
 final class Tracker {
-    private var cancellables = Set<AnyCancellable>()
-    private weak var player: Player?
+    private let player: AVPlayer
 
-    private var item: PlayerItem? {
-        willSet {
-            guard item != newValue else { return }
-            item?.disableTrackers()
-        }
-        didSet {
-            guard let player, item != oldValue else { return }
-            item?.enableTrackers(for: player)
-        }
-    }
-
-    init(player: Player) {
+    init(player: AVPlayer) {
         self.player = player
-
-        configureItemPublisher(for: player)
-        configurePropertiesPublisher(for: player)
-        configureMetricEventPublisher(for: player)
-    }
-
-    private func configureItemPublisher(for player: Player) {
-        player.queuePublisher
-            .slice(at: \.item)
-            .receiveOnMainThread()
-            .sink { [weak self] item in
-                self?.item = item
-            }
-            .store(in: &cancellables)
-    }
-
-    private func configurePropertiesPublisher(for player: Player) {
-        player.propertiesPublisher
-            .receiveOnMainThread()
-            .sink { [weak self] properties in
-                self?.item?.updateTrackerProperties(properties)
-            }
-            .store(in: &cancellables)
-    }
-
-    private func configureMetricEventPublisher(for player: Player) {
-        player.metricEventPublisher
-            .sink { [weak self] event in
-                self?.item?.receiveMetricEvent(event)
-            }
-            .store(in: &cancellables)
-    }
-
-    deinit {
-        item?.disableTrackers()
     }
 }
