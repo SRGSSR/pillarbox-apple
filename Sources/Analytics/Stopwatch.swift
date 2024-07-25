41f25d8863c6b3e5e8e84f16f4f0446e4495b298
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-07-25 08:02:24 +0200

    Add a Stopwatch object

---
+++ Sources/Analytics/Stopwatch.swift
@@ -0,0 +1,32 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+
+final class Stopwatch {
+    var date: Date?
+    var total: TimeInterval = 0
+
+    func start() {
+        guard date == nil else { return }
+        date = Date()
+    }
+
+    func stop() {
+        guard let date else { return }
+        total += Date().timeIntervalSince(date)
+        self.date = nil
+    }
+
+    func time() -> TimeInterval {
+        if let date {
+            return total + Date().timeIntervalSince(date)
+        }
+        else {
+            return total
+        }
+    }
+}
