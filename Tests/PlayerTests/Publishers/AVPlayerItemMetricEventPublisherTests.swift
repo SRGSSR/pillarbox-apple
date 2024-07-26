e00205a9029b16f0f9f34f6a6980bf517204b847
Samuel Défago <defagos@gmail.com>
2024-07-26 21:29:01 +0200

    Implement failure metric event publisher

--- Tests/PlayerTests/Publishers/AVPlayerItemMetricEventPublisherTests.swift
+++ Tests/PlayerTests/Publishers/AVPlayerItemMetricEventPublisherTests.swift
@@ -11,7 +11,7 @@ import Combine
 import PillarboxStreams
 
 final class AVPlayerItemMetricEventPublisherTests: TestCase {
-    func testPlayableItemResourceLoading() {
+    func testPlayableItemResourceLoadingMetricEvent() {
         let item = AVPlayerItem(url: Stream.onDemand.url)
         _ = AVPlayer(playerItem: item)
         expectOnlySimilarPublished(
@@ -22,9 +22,26 @@ final class AVPlayerItemMetricEventPublisherTests: TestCase {
         )
     }
 
-    func testFailingItemResourceLoading() {
+    func testFailingItemResourceLoadingMetricEvent() {
         let item = AVPlayerItem(url: Stream.unavailable.url)
         _ = AVPlayer(playerItem: item)
         expectNothingPublished(from: item.resourceLoadingMetricEventPublisher(), during: .milliseconds(500))
+    }
+
+    func testPlayableItemFailureMetricEvent() {
+        let item = AVPlayerItem(url: Stream.onDemand.url)
+        _ = AVPlayer(playerItem: item)
+        expectNothingPublished(from: item.failureMetricEventPublisher(), during: .milliseconds(500))
+    }
+
+    func testFailingItemFailureMetricEvent() {
+        let item = AVPlayerItem(url: Stream.unavailable.url)
+        _ = AVPlayer(playerItem: item)
+        expectOnlySimilarPublished(
+            values: [
+                .init(kind: .anyFailure)
+            ],
+            from: item.failureMetricEventPublisher()
+        )
     }
 }
