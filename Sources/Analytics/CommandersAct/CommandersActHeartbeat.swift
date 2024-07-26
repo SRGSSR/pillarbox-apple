4931bd8230921e0e058fea9da48d3d89c03f94b2
Samuel Défago <defagos@gmail.com>
2024-07-26 08:52:21 +0200

    Design Commanders Act heartbeat API

---
+++ Sources/Analytics/CommandersAct/CommandersActHeartbeat.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Combine
+import Foundation
+import PillarboxPlayer
+
+final class CommandersActHeartbeat {
+    init(delay: TimeInterval = 30, posInterval: TimeInterval = 30, uptimeInterval: TimeInterval = 60) {
+
+    }
+
+    func update(with properties: PlayerProperties) {
+
+    }
+}
