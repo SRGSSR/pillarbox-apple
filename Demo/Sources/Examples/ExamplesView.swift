2b4f6d6a1b480c6fc7e1703be02714c449596565
Samuel Défago <defagos@gmail.com>
2024-07-26 15:22:40 +0200

    Add BBC and Mux test streams to the demo (#949)

--- Demo/Sources/Examples/ExamplesView.swift
+++ Demo/Sources/Examples/ExamplesView.swift
@@ -206,8 +206,10 @@ struct ExamplesView: View {
     private func thirdPartySections() -> some View {
         section(title: "Apple streams", medias: model.appleMedias)
         section(title: "Third-party streams", medias: model.thirdPartyMedias)
-        section(title: "Bitmovin streams streams", medias: model.bitmovinMedias)
-        section(title: "Unified Streaming", medias: model.unifiedStreamingSourceMedias)
+        section(title: "Bitmovin streams", medias: model.bitmovinMedias)
+        section(title: "Unified Streaming streams", medias: model.unifiedStreamingMedias)
+        section(title: "BBC Test Card streams", medias: model.bbcTestCardMedias)
+        section(title: "Mux streams", medias: model.muxMedias)
     }
 
     @ViewBuilder
