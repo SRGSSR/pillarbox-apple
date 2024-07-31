cf5e307d4bd98b7279c6268d5b9d924a79bd111f
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-07-31 11:06:48 +0200

    Test the tracker metric events publisher when the player is not empty

--- Tests/PlayerTests/Tracking/PlayerItemTrackerMetricPublisherTests.swift
+++ Tests/PlayerTests/Tracking/PlayerItemTrackerMetricPublisherTests.swift
@@ -11,8 +11,19 @@ import PillarboxCircumspect
 import PillarboxStreams
 
 final class PlayerItemTrackerMetricPublisherTests: TestCase {
+    private static let assetLoading: MetricEvent = .init(kind: .assetLoading(.init()))
+    private static let resourceLoading: MetricEvent = .init(kind: .resourceLoading(.init()))
+
     func testEmptyPlayer() {
         let player = Player()
         expectNothingPublished(from: player.tracker.metricEventsPublisher, during: .milliseconds(100))
+    }
+
+    func testNotEmptyPlayer() {
+        let player = Player(item: .simple(url: Stream.shortOnDemand.url))
+        expectAtLeastSimilarPublished(values: [
+            [Self.assetLoading],
+            [Self.assetLoading, Self.resourceLoading]
+        ], from: player.tracker.metricEventsPublisher)
     }
 }
