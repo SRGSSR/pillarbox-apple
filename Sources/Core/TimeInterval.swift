3657346a2a698a44f2a6faa34992be4a8d8a919e
Samuel Défago <defagos@gmail.com>
2024-08-01 11:01:05 +0200

    Apply usual case binding conventions

--- Sources/Core/TimeInterval.swift
+++ Sources/Core/TimeInterval.swift
@@ -9,13 +9,13 @@ import Foundation
 extension TimeInterval {
     init(from stride: DispatchQueue.SchedulerTimeType.Stride) {
         switch stride.timeInterval {
-        case .seconds(let time):
+        case let .seconds(time):
             self = TimeInterval(time)
-        case .milliseconds(let time):
+        case let .milliseconds(time):
             self = TimeInterval(time) / 1_000
-        case .microseconds(let time):
+        case let .microseconds(time):
             self = TimeInterval(time) / 1_000_000
-        case .nanoseconds(let time):
+        case let .nanoseconds(time):
             self = TimeInterval(time) / 1_000_000_000
         default:
             self = .nan
