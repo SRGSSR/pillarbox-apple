88a2a37e4e854d422c7985fc5fee263499f05c87
Samuel Défago <defagos@gmail.com>
2024-08-02 21:02:12 +0200

    Update tests

--- Tests/PlayerTests/Tracking/PlayerItemTrackerMetricPublisherTests.swift
+++ Tests/PlayerTests/Tracking/PlayerItemTrackerMetricPublisherTests.swift
@@ -13,68 +13,38 @@ import PillarboxStreams
 final class PlayerItemTrackerMetricPublisherTests: TestCase {
     func testEmptyPlayer() {
         let player = Player()
-        expectNothingPublished(from: player.metricEventsPublisher, during: .milliseconds(100))
+        expectSimilarPublished(values: [[]], from: player.metricEventsPublisher, during: .milliseconds(500))
     }
 
-    func testNotEmptyPlayer() {
+    func testItemPlayback() {
         let player = Player(item: .simple(url: Stream.shortOnDemand.url))
         expectAtLeastSimilarPublished(values: [
+            [],
             [.anyAssetLoading],
-            [.anyAssetLoading, .anyResourceLoading]
-        ], from: player.metricEventsPublisher)
+            [.anyAssetLoading, .anyResourceLoading],
+            []
+        ], from: player.metricEventsPublisher) {
+            player.play()
+        }
     }
 
     func testError() {
         let player = Player(item: .simple(url: Stream.unavailable.url))
         expectAtLeastSimilarPublished(values: [
+            [],
             [.anyAssetLoading],
             [.anyAssetLoading, .anyFailure]
         ], from: player.metricEventsPublisher)
-    }
-
-    func testReplay() {
-        let player = Player(item: .simple(url: Stream.shortOnDemand.url))
-        expectSimilarPublished(
-            values: [
-                [.anyAssetLoading],
-                [.anyAssetLoading, .anyResourceLoading],
-                [.anyAssetLoading],
-                [.anyAssetLoading, .anyResourceLoading]
-            ],
-            from: player.metricEventsPublisher,
-            during: .milliseconds(1500)
-        ) {
-            player.play()
-            expect(player.playbackState).toEventually(equal(.playing))
-            expect(player.playbackState).toEventually(equal(.idle))
-            player.replay()
-        }
     }
 
     func testPlaylist() {
         let player = Player(items: [.simple(url: Stream.shortOnDemand.url), .simple(url: Stream.mediumOnDemand.url)])
         expectSimilarPublished(
             values: [
+                [],
                 [.anyAssetLoading],
                 [.anyAssetLoading, .anyResourceLoading],
-                [.anyAssetLoading],
                 [.anyAssetLoading, .anyResourceLoading]
-            ],
-            from: player.metricEventsPublisher,
-            during: .milliseconds(1500)
-        ) {
-            player.play()
-        }
-    }
-
-    func testPlaylistWithError() {
-        let player = Player(items: [.simple(url: Stream.shortOnDemand.url), .simple(url: Stream.unauthorized.url)])
-        expectSimilarPublished(
-            values: [
-                [.anyAssetLoading],
-                [.anyAssetLoading, .anyResourceLoading],
-                [.anyAssetLoading],
-                [.anyAssetLoading, .anyFailure]
             ],
             from: player.metricEventsPublisher,
             during: .milliseconds(1500)
