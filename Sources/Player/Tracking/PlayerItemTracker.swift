b9bb4f6f4daf8fec1487c122bb024c0413f09e9b
Samuel Défago <defagos@gmail.com>
2024-08-14 13:18:48 +0200

    Fix parameter order
    Co-authored-by: Walid Kayhal <3347810+waliid@users.noreply.github.com>

--- Sources/Player/Tracking/PlayerItemTracker.swift
+++ Sources/Player/Tracking/PlayerItemTracker.swift
@@ -97,8 +97,8 @@ public extension PlayerItemTracker where Configuration == Void {
     /// Creates an adapter for the receiver.
     /// 
     /// - Parameters:
-    ///   - mapper: A closure that maps an item metadata to tracker metadata.
     ///   - behavior: The tracking behavior.
+    ///   - mapper: A closure that maps an item metadata to tracker metadata.
     /// - Returns: The tracker adapter.
     static func adapter<M>(behavior: TrackingBehavior = .optional, mapper: @escaping (M) -> Metadata) -> TrackerAdapter<M> {
         .init(trackerType: Self.self, configuration: (), behavior: behavior, mapper: mapper)
