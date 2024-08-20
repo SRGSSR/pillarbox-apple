f1b5e6c75844e9a134595a4f872e0caacdda7c72
Samuel Défago <defagos@gmail.com>
2024-08-20 10:12:46 +0200

    Start playlist implementation anew

--- Demo/Sources/Showcase/Playlist/PlaylistView.swift
+++ Demo/Sources/Showcase/Playlist/PlaylistView.swift
@@ -7,138 +7,6 @@
 import PillarboxPlayer
 import SwiftUI
 
-// Behavior: h-exp, v-exp
-private struct MediaCell: View {
-    let media: Media
-
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text(media.title)
-            if let subtitle = media.subtitle {
-                Text(subtitle)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
-        }
-    }
-}
-
-// Behavior: h-exp, v-exp
-private struct PlaylistSelectionView: View {
-    let model: PlaylistViewModel
-    @State private var selectedTemplates: Set<Template> = []
-    @Environment(\.dismiss) private var dismiss
-
-    var body: some View {
-        NavigationView {
-            List(selection: $selectedTemplates) {
-                section(title: "Original items", templates: model.templates)
-                section(title: "Standard items", templates: model.otherStandardTemplates)
-            }
-            .environment(\.editMode, .constant(.active))
-            .navigationBarTitle("Add a stream to the playlist")
-#if os(iOS)
-            .navigationBarTitleDisplayMode(.inline)
-#endif
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button("Cancel", action: cancel)
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Add", action: add)
-                        .disabled(selectedTemplates.isEmpty)
-                }
-            }
-        }
-        .tracked(name: "selection", levels: ["playlist"])
-    }
-
-    @ViewBuilder
-    private func section(title: String, templates: [Template]) -> some View {
-        if !templates.isEmpty {
-            Section(title) {
-                ForEach(templates, id: \.self) { template in
-                    Text(template.title)
-                }
-            }
-        }
-    }
-
-    private func cancel() {
-        dismiss()
-    }
-
-    private func add() {
-        model.add(from: Array(selectedTemplates))
-        dismiss()
-    }
-}
-
-// Behavior: h-exp, v-hug
-private struct Toolbar: View {
-    @ObservedObject var player: Player
-    @ObservedObject var model: PlaylistViewModel
-
-    @State private var isSelectionPlaylistPresented = false
-
-    var body: some View {
-        HStack {
-            previousButton()
-            Spacer()
-            managementButtons()
-            Spacer()
-            nextButton()
-        }
-        .padding()
-        .frame(maxWidth: .infinity)
-        .sheet(isPresented: $isSelectionPlaylistPresented) {
-            PlaylistSelectionView(model: model)
-        }
-    }
-
-    @ViewBuilder
-    private func previousButton() -> some View {
-        Button(action: player.returnToPrevious) {
-            Image(systemName: "arrow.left")
-        }
-        .disabled(!player.canReturnToPrevious())
-    }
-
-    @ViewBuilder
-    private func managementButtons() -> some View {
-        HStack(spacing: 30) {
-            Button(action: model.shuffle) {
-                Image(systemName: "shuffle")
-            }
-            .disabled(model.isEmpty)
-            Button(action: add) {
-                Image(systemName: "plus")
-            }
-            Button(action: model.reload) {
-                Image(systemName: "arrow.triangle.2.circlepath")
-            }
-            .disabled(!model.canReload)
-            Button(action: model.trash) {
-                Image(systemName: "trash")
-            }
-            .disabled(model.isEmpty)
-        }
-    }
-
-    @ViewBuilder
-    private func nextButton() -> some View {
-        Button(action: player.advanceToNext) {
-            Image(systemName: "arrow.right")
-        }
-        .disabled(!player.canAdvanceToNext())
-    }
-
-    private func add() {
-        isSelectionPlaylistPresented.toggle()
-    }
-}
-
-// Behavior: h-exp, v-exp
 struct PlaylistView: View {
     let templates: [Template]
 
@@ -152,16 +20,12 @@ struct PlaylistView: View {
                 .supportsPictureInPicture()
 #if os(iOS)
             if layout != .maximized {
-                Toolbar(player: model.player, model: model)
-                List($model.medias, id: \.self, editActions: .all, selection: $model.currentMedia) { $media in
-                    MediaCell(media: media)
-                }
+                Color.red
             }
 #endif
         }
         .animation(.defaultLinear, value: layout)
         .onAppear {
-            model.templates = templates
             model.play()
         }
         .enabledForInAppPictureInPicture(persisting: model)
--- Demo/Sources/Showcase/Playlist/PlaylistViewModel.swift
+++ Demo/Sources/Showcase/Playlist/PlaylistViewModel.swift
@@ -9,146 +9,14 @@ import OrderedCollections
 import PillarboxPlayer
 
 final class PlaylistViewModel: ObservableObject, PictureInPicturePersistable {
-    static let standardTemplates = [
-        URLTemplate.onDemandVideoHLS,
-        URLTemplate.shortOnDemandVideoHLS,
-        URLTemplate.onDemandVideoMP4,
-        URLTemplate.liveVideoHLS,
-        URLTemplate.dvrVideoHLS,
-        URLTemplate.liveTimestampVideoHLS,
-        URLTemplate.onDemandAudioMP3,
-        URLTemplate.liveAudioMP3,
-        URNTemplate.onDemandHorizontalVideo,
-        URNTemplate.onDemandSquareVideo,
-        URNTemplate.onDemandVerticalVideo,
-        URNTemplate.liveVideo,
-        URNTemplate.dvrVideo,
-        URNTemplate.dvrAudio,
-        URLTemplate.appleBasic_4_3_HLS,
-        URLTemplate.appleBasic_16_9_TS_HLS,
-        URLTemplate.appleAdvanced_16_9_TS_HLS,
-        URLTemplate.appleAdvanced_16_9_fMP4_HLS,
-        URLTemplate.appleAdvanced_16_9_HEVC_h264_HLS,
-        URLTemplate.appleWWDCKeynote2023,
-        URLTemplate.appleDolbyAtmos,
-        URLTemplate.appleTvMorningShowSeason1Trailer,
-        URLTemplate.appleTvMorningShowSeason2Trailer,
-        URLTemplate.uhdVideoHLS,
-        URNTemplate.gothard_360,
-        URLTemplate.bitmovin_360,
-        URLTemplate.unauthorized,
-        URLTemplate.unknown,
-        URLTemplate.unavailableMp3,
-        URNTemplate.expired,
-        URNTemplate.unknown
-    ]
-
-    @Published var currentMedia: Media? {
-        didSet {
-            guard let currentMedia, let index = items.keys.firstIndex(of: currentMedia) else { return }
-            if player.currentIndex != index {
-                try? player.setCurrentIndex(index)
-            }
-            else {
-                player.skipToDefault()
-            }
-        }
-    }
-
-    @Published private var items = OrderedDictionary<Media, PlayerItem>() {
-        didSet {
-            player.items = items.values.elements
-        }
-    }
-
     let player = Player(configuration: .standard)
-
-    var medias: [Media] {
-        get {
-            Array(items.keys)
-        }
-        set {
-            items = Self.updated(initialItems: items, with: newValue)
-        }
-    }
-
-    var otherStandardTemplates: [Template] {
-        Array(OrderedSet(Self.standardTemplates).subtracting(Set(templates)))
-    }
-
-    var templates: [Template] = [] {
-        didSet {
-            guard medias.isEmpty else { return }
-            medias = Template.medias(from: templates)
-        }
-    }
-
-    var isEmpty: Bool {
-        medias.isEmpty
-    }
-
-    var isMonoscopic: Bool {
-        currentMedia?.isMonoscopic ?? false
-    }
-
-    var canReload: Bool {
-        !templates.isEmpty
-    }
-
-    init() {
-        configureCurrentMediaPublisher()
-    }
-
-    private static func updated(
-        initialItems: OrderedDictionary<Media, PlayerItem>,
-        with medias: [Media]
-    ) -> OrderedDictionary<Media, PlayerItem> {
-        var items = initialItems
-        let changes = medias.difference(from: initialItems.keys).inferringMoves()
-        changes.forEach { change in
-            switch change {
-            case let .insert(offset: offset, element: element, associatedWith: associatedWith):
-                if let associatedWith {
-                    let previousPlayerItem = initialItems.elements[associatedWith].value
-                    items.updateValue(previousPlayerItem, forKey: element, insertingAt: offset)
-                }
-                else {
-                    items.updateValue(element.playerItem(), forKey: element, insertingAt: offset)
-                }
-            case let .remove(offset: offset, element: _, associatedWith: _):
-                items.remove(at: offset)
-            }
-        }
-        return items
-    }
-
-    func add(from templates: [Template]) {
-        medias += Template.medias(from: templates)
-    }
 
     func play() {
         player.becomeActive()
         player.play()
     }
 
-    func shuffle() {
-        items.shuffle()
-    }
-
-    func reload() {
-        medias = Template.medias(from: templates)
-    }
-
-    func trash() {
-        medias = []
-    }
-
-    private func configureCurrentMediaPublisher() {
-        Publishers.CombineLatest(player.$currentIndex, $items)
-            .map { index, items in
-                guard let index, index < items.count else { return nil }
-                return items.keys[index]
-            }
-            .assign(to: &$currentMedia)
+    var isMonoscopic: Bool {
+        false
     }
 }
