82692dc9818d7624c43ecb5cd797d264a3bd7385
Samuel Défago <defagos@gmail.com>
2024-08-23 10:52:09 +0200

    Write missing test

--- Tests/PlayerTests/Playlist/RepeatModeTests.swift
+++ Tests/PlayerTests/Playlist/RepeatModeTests.swift
@@ -21,5 +21,17 @@ final class RepeatModeTests: TestCase {
         expect(player.currentItem).toEventually(equal(item2))
     }
 
-    func testRepeatAll() {}
+    func testRepeatAll() {
+        let item1 = PlayerItem.simple(url: Stream.shortOnDemand.url)
+        let item2 = PlayerItem.simple(url: Stream.shortOnDemand.url)
+        let player = Player(items: [item1, item2])
+        player.repeatMode = .all
+        player.play()
+        expect(player.currentItem).toEventually(equal(item1))
+        expect(player.currentItem).toEventually(equal(item2))
+        expect(player.currentItem).toEventually(equal(item1))
+        player.repeatMode = .off
+        expect(player.currentItem).toEventually(equal(item2))
+        expect(player.currentItem).toNever(equal(item1), until: .seconds(2))
+    }
 }
