ef2432d8fc25d684fb726f9362a970252b2b3e8d
Samuel Défago <defagos@gmail.com>
2024-09-10 23:06:18 +0200

    Remove nested layers to avoid animation issues

--- Sources/Player/UserInterface/VideoLayerView.swift
+++ Sources/Player/UserInterface/VideoLayerView.swift
@@ -8,44 +8,16 @@ import AVFoundation
 import UIKit
 
 final class VideoLayerView: UIView {
-    let playerLayer: AVPlayerLayer
+    override static var layerClass: AnyClass {
+        AVPlayerLayer.self
+    }
+
+    var playerLayer: AVPlayerLayer {
+        layer as! AVPlayerLayer
+    }
 
     var player: AVPlayer? {
         get { playerLayer.player }
         set { playerLayer.player = newValue }
-    }
-
-    init(from playerLayer: AVPlayerLayer? = nil) {
-        self.playerLayer = playerLayer ?? .init()
-        super.init(frame: .zero)
-        layer.addSublayer(self.playerLayer)
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func layoutSublayers(of layer: CALayer) {
-        super.layoutSublayers(of: layer)
-        layer.synchronizeLayoutChanges {
-            playerLayer.frame = layer.bounds
-        }
-    }
-}
-
-private extension CALayer {
-    func synchronizeLayoutChanges(_ changes: () -> Void) {
-        if let positionAnimation = animation(forKey: "position") {
-            CATransaction.begin()
-            CATransaction.setAnimationDuration(positionAnimation.duration)
-            CATransaction.setAnimationTimingFunction(positionAnimation.timingFunction)
-            changes()
-            CATransaction.commit()
-        }
-        else {
-            changes()
-            sublayers?.forEach { $0.removeAllAnimations() }
-        }
     }
 }
