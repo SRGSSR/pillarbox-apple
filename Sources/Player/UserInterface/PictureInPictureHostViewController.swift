72e35f857903b55a7d185ec0e86d1917c1072d7b
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-09-24 11:04:32 +0200

    Introduce PictureInPictureHostViewController

---
+++ Sources/Player/UserInterface/PictureInPictureHostViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import AVKit
+import UIKit
+
+final class PictureInPictureHostViewController: UIViewController {
+    weak var viewController: AVPlayerViewController?
+
+    func addViewController(_ viewController: AVPlayerViewController) {
+        addChild(viewController)
+        view.addSubview(viewController.view)
+
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            viewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            viewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            viewController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            viewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        viewController.didMove(toParent: self)
+    }
+
+    override func removeFromParent() {
+        super.removeFromParent()
+        if let viewController, view === viewController.view {
+            let duplicate = AVPlayerViewController()
+            duplicate.player = viewController.player
+            duplicate.videoGravity = viewController.videoGravity
+            addViewController(duplicate)
+        }
+    }
+}
