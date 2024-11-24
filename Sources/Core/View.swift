c7be7adb39751669463a765890e97b363f63474a
Samuel Défago <defagos@gmail.com>
2024-11-24 14:49:01 +0100

    Add method to respond to state changes at the user interface level (#1070)

--- Sources/Core/View.swift
+++ Sources/Core/View.swift
@@ -14,16 +14,30 @@ public extension View {
     ///   - publisher: The publisher to subscribe to.
     ///   - keyPath: The key path to extract.
     ///   - binding: The binding to which values must be assigned.
-    /// - Returns: A view that fills the given binding when the `publisher` emits an event.
     func onReceive<P, T>(
         _ publisher: P,
         assign keyPath: KeyPath<P.Output, T>,
         to binding: Binding<T>
     ) -> some View where P: Publisher, P.Failure == Never, T: Equatable {
-            onReceive(publisher.slice(at: keyPath).receiveOnMainThread()) { output in
-                if binding.wrappedValue != output {
-                    binding.wrappedValue = output
-                }
+        onReceive(publisher.slice(at: keyPath).receiveOnMainThread()) { output in
+            if binding.wrappedValue != output {
+                binding.wrappedValue = output
             }
+        }
+    }
+
+    /// Observes values emitted by the given publisher at the specified key path.
+    ///
+    /// - Parameters:
+    ///   - publisher: The publisher to subscribe to.
+    ///   - keyPath: The key path to extract.
+    ///   - action: A closure to run when the value changes, executed on the main thread. The action is not called for
+    ///     the first value that the publisher might provide upon subscription.
+    func onReceive<P, T>(
+        _ publisher: P,
+        at keyPath: KeyPath<P.Output, T>,
+        perform action: @escaping (T) -> Void
+    ) -> some View where P: Publisher, P.Failure == Never, T: Equatable {
+        onReceive(publisher.slice(at: keyPath).dropFirst().receiveOnMainThread(), perform: action)
     }
 }
