5cbb987e6999fddd8520b182ff4d2fd00802b0c1
Samuel Défago <defagos@gmail.com>
2024-12-12 11:23:52 +0100

    Extract Irdeto error message

--- Sources/CoreBusiness/Extensions/URLSession.swift
+++ Sources/CoreBusiness/Extensions/URLSession.swift
@@ -6,6 +6,15 @@
 
 import Foundation
 
+private struct IrdetoData: Decodable {
+    let code: Int
+    let message: String
+
+    var fullMessage: String {
+        "\(message) (\(code))"
+    }
+}
+
 extension URLSession {
     /// Loads data for a request, throwing when HTTP errors are encountered.
     ///
@@ -15,10 +24,15 @@ extension URLSession {
     /// - Returns: Data and response.
     func httpData(for request: URLRequest, delegate: URLSessionTaskDelegate? = nil) async throws -> (Data, URLResponse) {
         let (data, response) = try await data(for: request, delegate: delegate)
-        if let httpError = DataError.http(from: response) {
+        if let irdetoData = try? JSONDecoder().decode(IrdetoData.self, from: data) {
+            throw DataError.blocked(withMessage: irdetoData.fullMessage)
+        }
+        else if let httpError = DataError.http(from: response) {
             throw httpError
         }
-        return (data, response)
+        else {
+            return (data, response)
+        }
     }
 
     /// Loads data for a URL, throwing when HTTP errors are encountered.
