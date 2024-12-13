4340c261bec92f867f9620782d5a1a3264e7740a
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2024-12-13 15:19:32 +0100

    Factorize interactive part

--- Scripts/archive-demo.sh
+++ Scripts/archive-demo.sh
@@ -32,37 +32,12 @@ fi
 echo -e "Archiving $PLATFORM demo..."
 
 if [[ $IS_INTERACTIVE == true ]]; then
-    TEAM_ID=""
-    KEY_ISSUER_ID=""
-    KEY_ID=""
-    APPLE_API_KEY_BASE64=""
-
-    echo "Please provide information related to your Apple account:"
-    echo
-    read -rp "  TEAM_ID: " TEAM_ID
-    read -rp "  KEY_ISSUER_ID: " KEY_ISSUER_ID
-    read -rp "  KEY_ID: " KEY_ID
-    read -rp "  APPLE_API_KEY_BASE64: " APPLE_API_KEY_BASE64
-
-    if [[ -z "$TEAM_ID" || -z "$KEY_ISSUER_ID" || -z "$KEY_ID" || -z "$APPLE_API_KEY_BASE64" ]]; then
-        echo
-        [[ -z "$TEAM_ID" ]] && echo "  TEAM_ID is missing!"
-        [[ -z "$KEY_ISSUER_ID" ]] && echo "  KEY_ISSUER_ID is missing!"
-        [[ -z "$KEY_ID" ]] && echo "  KEY_ID is missing!"
-        [[ -z "$APPLE_API_KEY_BASE64" ]] && echo "  APPLE_API_KEY_BASE64 is missing!"
-        exit 1
-    fi
-    echo
-
-    export TEAM_ID
-    export KEY_ISSUER_ID
-    export KEY_ID
-
-    "$(dirname "$0")/configure-environment.sh" "$APPLE_API_KEY_BASE64"
+    source Scripts/set-apple-api-env-interactively.sh
+    Scripts/configure-environment.sh "$APPLE_API_KEY_BASE64"
 fi
 
-"$(dirname "$0")/install-pkgx.sh"
-"$(dirname "$0")/install-bundler.sh"
+Scripts/install-pkgx.sh
+Scripts/install-bundler.sh
 
 pkgx bundle exec fastlane "archive_demo_$PLATFORM"
 echo "... done"
