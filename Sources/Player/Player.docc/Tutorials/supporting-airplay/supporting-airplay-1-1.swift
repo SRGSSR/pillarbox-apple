ec5932cb95ed3e4e20ba552b524a45d95501a5ff
Samuel Défago <defagos@gmail.com>
2024-12-31 17:46:18 +0100

    Improve AirPlay tutorial

--- Sources/Player/Player.docc/Tutorials/supporting-airplay/supporting-airplay-1-1.swift
+++ Sources/Player/Player.docc/Tutorials/supporting-airplay/supporting-airplay-1-1.swift
@@ -1,5 +1,23 @@
 import AVFAudio
+import SwiftUI
+import UIKit
 
-func configureAudioSession() {
-    try? AVAudioSession.sharedInstance().setCategory(.playback)
+private final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        try? AVAudioSession.sharedInstance().setCategory(.playback)
+        return true
+    }
+}
+
+struct Application: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            // ...
+        }
+    }
 }
