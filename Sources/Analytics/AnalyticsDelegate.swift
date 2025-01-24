ffc74e63518534001875991d8e76c22660c978ab
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-01-24 08:34:38 +0100

    Introduce AnalyticsDelegate to notify events related to analytics

---
+++ Sources/Analytics/AnalyticsDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+/// A delegate for analytics events.
+public protocol AnalyticsDelegate: AnyObject {
+}
