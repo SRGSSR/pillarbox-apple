23ef4eca8fe7228cbadcc803d1ff4ea20648b76a
Samuel Défago <defagos@gmail.com>
2025-02-04 09:31:13 +0100

    Fix slider background and animations (#1131)

--- Demo/Sources/Players/PlaybackView.swift
+++ Demo/Sources/Players/PlaybackView.swift
@@ -696,8 +696,9 @@ private struct TimeSlider: View {
             }
             .frame(height: progressTracker.isInteracting ? 16 : 8)
             .clipShape(.capsule)
+            .shadow(color: .init(white: 0.2, opacity: 0.8), radius: 15)
             .opacity(isVisible ? 1 : 0)
-            .animation(.easeInOut(duration: 0.3), values: progressTracker.isInteracting, isVisible)
+            .animation(.easeInOut(duration: 0.3), value: progressTracker.isInteracting)
         }
         .onEditingChanged { isEditing in
             progressTracker.isInteracting = isEditing
