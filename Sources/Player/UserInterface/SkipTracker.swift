27f17a15baa5b03d5d3ec75b6e60af7b036eec20
Samuel Défago <defagos@gmail.com>
2025-03-15 15:24:48 +0100

    Add skip tracker

---
+++ Sources/Player/UserInterface/SkipTracker.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Combine
+
+/// An observable object managing skips.
+@available(iOS 16, *)
+@available(tvOS, unavailable)
+public final class SkipTracker: ObservableObject {
+}
---
+++ Tests/PlayerTests/UserInterface/SkipTrackerTests.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@testable import PillarboxPlayer
+
+#if os(iOS)
+final class SkipTrackerTests: TestCase {
+}
+#endif
