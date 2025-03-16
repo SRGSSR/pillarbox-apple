fea8b92df9d516ef2c7f98419acfd3ade41657f1
Samuel Défago <defagos@gmail.com>
2025-03-16 13:34:32 +0100

    Add methods to handle skips based on their type

---
+++ Sources/Player/Types/Skip.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+/// A skip.
+public enum Skip {
+    /// Backward skip.
+    case backward
+
+    /// Forward skip.
+    case forward
+}
