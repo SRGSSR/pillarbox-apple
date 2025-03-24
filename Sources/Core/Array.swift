c1825e5455b251f1718cf16d9a46298272712928
Samuel Défago <defagos@gmail.com>
2025-03-24 13:38:08 +0100

    Fix typo

--- Sources/Core/Array.swift
+++ Sources/Core/Array.swift
@@ -9,8 +9,8 @@ public extension Array where Element: Hashable {
     ///
     /// - Returns: The array without duplicate items.
     func removeDuplicates() -> [Element] {
-        var itemDictionnary = [Element: Bool]()
-        return filter { itemDictionnary.updateValue(true, forKey: $0) == nil }
+        var itemDictionary = [Element: Bool]()
+        return filter { itemDictionary.updateValue(true, forKey: $0) == nil }
     }
 }
 
