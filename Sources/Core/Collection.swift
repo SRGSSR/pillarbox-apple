5dc15fabb79ba1652138f3813f14797e656b9fb0
Samuel Défago <defagos@gmail.com>
2025-03-26 08:23:38 +0100

    Provide more generic collection helpers (#1174)

--- Sources/Core/Collection.swift
+++ Sources/Core/Collection.swift
@@ -4,23 +4,23 @@
 //  License information is available from the LICENSE file.
 //
 
-public extension Array where Element: Hashable {
+public extension Collection where Element: Hashable {
     /// Removes duplicates from the receiver, preserving the initial item order.
     ///
     /// - Returns: The array without duplicate items.
     func removeDuplicates() -> [Element] {
-        var itemDictionnary = [Element: Bool]()
-        return filter { itemDictionnary.updateValue(true, forKey: $0) == nil }
+        var elements = [Element: Bool]()
+        return filter { elements.updateValue(true, forKey: $0) == nil }
     }
 }
 
-public extension Array {
+public extension Collection {
     /// Safely returns the item at the specified index.
     /// 
     /// - Parameter index: The index.
     /// - Returns: The item at the specified index or `nil` if the index is not within range.
-    subscript(safeIndex index: Int) -> Element? {
-        guard (0..<endIndex).contains(index) else { return nil }
+    subscript(safeIndex index: Index) -> Element? {
+        guard indices.contains(index) else { return nil }
         return self[index]
     }
 }
