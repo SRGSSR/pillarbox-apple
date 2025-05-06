3875961411fe1aa858d216d40d12d52b0c62e624
Samuel Défago <defagos@gmail.com>
2025-05-06 09:03:47 +0200

    Fix localization comment

--- Sources/Player/MediaSelection/MediaSelectionOption.swift
+++ Sources/Player/MediaSelection/MediaSelectionOption.swift
@@ -25,9 +25,9 @@ public enum MediaSelectionOption: Hashable {
     public var displayName: String {
         switch self {
         case .automatic:
-            return String(localized: "Auto (Recommended)", bundle: .module, comment: "Subtitle selection option")
+            return String(localized: "Auto (Recommended)", bundle: .module, comment: "Media selection option")
         case .off:
-            return String(localized: "Off", bundle: .module, comment: "Subtitle selection option")
+            return String(localized: "Off", bundle: .module, comment: "Media selection option")
         case let .on(option):
             return option.displayName
         }
