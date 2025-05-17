76a9c2a1c8c9b7e3281bb41df03218575699bec1
Samuel Défago <defagos@gmail.com>
2025-05-17 23:00:26 +0200

    Restrict immediate seek behavior to video content only

--- Sources/Player/Types/SeekBehavior.swift
+++ Sources/Player/Types/SeekBehavior.swift
@@ -7,6 +7,8 @@
 /// A behavior for progress updates during a seek operation.
 public enum SeekBehavior {
     /// A behavior updating progress immediately during a seek.
+    ///
+    /// > Note: This behavior is only applied to video content.
     case immediate
 
     /// A behavior deferring progress update after a seek ends.
