b8afc6b44413456423877a26a899e4e34072f20b
Samuel Défago <defagos@gmail.com>
2025-06-20 18:12:23 +0200

    Improve implementation

--- Sources/Core/Types/LimitedBuffer.swift
+++ Sources/Core/Types/LimitedBuffer.swift
@@ -9,10 +9,10 @@ import os
 /// A thread-safe buffer able to hold a maximum number of values.
 final class LimitedBuffer<T> {
     private let size: Int
-    private let _values = OSAllocatedUnfairLock(initialState: [T]())
+    private let state = OSAllocatedUnfairLock(initialState: State(values: []))
 
     var values: [T] {
-        _values.withLock(\.self)
+        state.withLock(\.values)
     }
 
     init(size: Int) {
@@ -22,10 +22,20 @@ final class LimitedBuffer<T> {
 
     func append(_ t: T) {
         guard size > 0 else { return }
-        _values.withLock { values in
+        state.withLock { state in
+            state.append(t, withLimit: size)
+        }
+    }
+}
+
+private extension LimitedBuffer {
+    struct State {
+        private(set) var values = [T]()
+
+        mutating func append(_ t: T, withLimit limit: Int) {
             values.append(t)
-            if values.count > size {
-                values.removeFirst(values.count - size)
+            if values.count > limit {
+                values.removeFirst(values.count - limit)
             }
         }
     }
