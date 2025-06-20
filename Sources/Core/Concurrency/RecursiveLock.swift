33c05fd9cb14c472a033044f6c5b50634805ce92
Samuel Défago <defagos@gmail.com>
2025-06-20 20:46:10 +0200

    Revisit periodic time publisher implementation

--- Sources/Core/Concurrency/RecursiveLock.swift
+++ Sources/Core/Concurrency/RecursiveLock.swift
@@ -6,17 +6,25 @@
 
 import Foundation
 
+/// A wrapper around a recursive lock that locks around accesses to a stored object.
 public class RecursiveLock<State>: @unchecked Sendable {
     private var state: State
     private let lock = NSRecursiveLock()
 
+    /// Initializes a lock with an initial state.
     public init(initialState: State) {
         self.state = initialState
     }
 
+    /// Perform a closure while holding the lock.
     public func withLock<R>(_ body: (inout State) throws -> R) rethrows -> R {
         try lock.withLock {
             try body(&state)
         }
+    }
+
+    /// Read the stored object through the lock.
+    public func locked() -> State {
+        withLock(\.self)
     }
 }
--- Sources/Player/Publishers/PeriodicTimePublisher.swift
+++ Sources/Player/Publishers/PeriodicTimePublisher.swift
@@ -38,51 +38,50 @@ extension Publishers {
 
 private extension _PeriodicTimePublisher {
     final class PeriodicTimeSubscription<S>: Subscription where S: Subscriber, S.Input == Output, S.Failure == Failure {
-        private var subscriber: S?
+        private let subscriber: RecursiveLock<S?>
         private let player: AVPlayer
         private let interval: CMTime
         private let queue: DispatchQueue
 
         private let buffer = DemandBuffer<CMTime>()
-        private var timeObserver: Any?
-        private let lock = NSRecursiveLock()
+        private let timeObserver: RecursiveLock<Any?> = .init(initialState: nil)
 
         init(subscriber: S, player: AVPlayer, interval: CMTime, queue: DispatchQueue) {
-            self.subscriber = subscriber
+            self.subscriber = .init(initialState: subscriber)
             self.player = player
             self.interval = interval
             self.queue = queue
         }
 
         func request(_ demand: Subscribers.Demand) {
-            lock.withLock {
+            timeObserver.withLock { timeObserver in
                 if timeObserver == nil {
                     timeObserver = player.addPeriodicTimeObserver(forInterval: interval, queue: queue) { [weak self] time in
                         self?.send(time)
                     }
                 }
-                process(buffer.request(demand))
             }
+            process(buffer.request(demand))
         }
 
         private func send(_ time: CMTime) {
-            lock.withLock {
-                process(buffer.append(time))
-            }
+            process(buffer.append(time))
         }
 
         func cancel() {
-            lock.withLock {
+            timeObserver.withLock { timeObserver in
                 if let timeObserver {
                     player.removeTimeObserver(timeObserver)
-                    self.timeObserver = nil
                 }
+                timeObserver = nil
+            }
+            subscriber.withLock { subscriber in
                 subscriber = nil
             }
         }
 
         private func process(_ values: [CMTime]) {
-            guard let subscriber = lock.withLock({ subscriber }) else { return }
+            guard let subscriber = subscriber.locked() else { return }
             values.forEach { value in
                 let demand = subscriber.receive(value)
                 request(demand)
