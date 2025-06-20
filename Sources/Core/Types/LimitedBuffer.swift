1484ba52764f04877e527fc0866c460737af055f
Samuel Défago <defagos@gmail.com>
2025-06-20 15:28:46 +0200

    Protect state atomically

--- Sources/Core/Types/LimitedBuffer.swift
+++ Sources/Core/Types/LimitedBuffer.swift
@@ -9,10 +9,11 @@ import os
 /// A thread-safe buffer able to hold a maximum number of values.
 final class LimitedBuffer<T> {
     private let size: Int
-    private let _values = OSAllocatedUnfairLock(initialState: [T]())
+
+    private let state = OSAllocatedUnfairLock(initialState: [T]())
 
     var values: [T] {
-        _values.withLock { $0 }
+        state.withLock { $0 }
     }
 
     init(size: Int) {
@@ -22,7 +23,7 @@ final class LimitedBuffer<T> {
 
     func append(_ t: T) {
         guard size > 0 else { return }
-        _values.withLock { values in
+        state.withLock { values in
             values.append(t)
             values = values.suffix(size)
         }
