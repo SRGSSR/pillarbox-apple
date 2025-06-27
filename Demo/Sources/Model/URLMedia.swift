ecfdb06a977f69044d0f525376efb2e15f9330ae
Samuel Défago <defagos@gmail.com>
2025-06-27 08:02:40 +0200

    Update RTS on-demand example URL (#1251)

--- Demo/Sources/Model/URLMedia.swift
+++ Demo/Sources/Model/URLMedia.swift
@@ -23,7 +23,7 @@ enum URLMedia {
         title: "Sacha part à la rencontre d'univers atypiques",
         subtitle: "VOD - HLS",
         imageUrl: "https://www.rts.ch/2024/06/13/11/34/14970435.image/16x9",
-        type: .url("https://rts-vod-amd.akamaized.net/ww/14970442/96fc7429-64c1-34b0-8c05-62cf114695ac/master.m3u8")
+        type: .url("https://rts-vod-amd.akamaized.net/ww/14970442/4dcba1d3-8cc8-3667-a7d2-b3b92c4243d9/master.m3u8")
     )
     static let shortOnDemandVideoHLS = Media(
         title: "Des violents orages ont touché Ajaccio, chef-lieu de la Corse, jeudi",
