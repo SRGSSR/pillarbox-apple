6c258635e78a70df1ca307a42fa29ef506335232
Samuel Défago <defagos@gmail.com>
2025-08-06 11:28:00 +0200

    Hide playback configuration properties

--- Sources/Player/Types/PlaybackConfiguration.swift
+++ Sources/Player/Types/PlaybackConfiguration.swift
@@ -11,25 +11,25 @@ public struct PlaybackConfiguration {
     /// The default configuration.
     public static let `default` = Self()
 
-    /// The position to start playback at.
+    private let position: () -> Position
+    private let automaticallyPreservesTimeOffsetFromLive: Bool
+    private let preferredForwardBufferDuration: TimeInterval
+
+    /// Creates a playback configuration.
+    ///
+    /// - Parameters:
+    ///   - position: The position to start playback at.
+    ///   - automaticallyPreservesTimeOffsetFromLive: A Boolean value that indicates whether the player preserves its
+    ///     time offset from the live time after a buffering operation.
+    ///   - preferredForwardBufferDuration: The duration the player should buffer media from the network ahead of the
+    ///     playhead to guard against playback disruption.
     ///
     /// When the position time is `.zero`, playback efficiently starts at the default position:
     ///
     /// - Zero for an on-demand stream.
     /// - Live edge for a livestream supporting DVR.
     ///
-    /// Note that starting at the default position is always efficient, no matter which tolerances have been requested.
-    public let position: () -> Position
-
-    /// A Boolean value that indicates whether the player preserves its time offset from the live time after a
-    /// buffering operation.
-    public let automaticallyPreservesTimeOffsetFromLive: Bool
-
-    /// The duration the player should buffer media from the network ahead of the playhead to guard against playback
-    /// disruption.
-    public let preferredForwardBufferDuration: TimeInterval
-
-    /// Creates a playback configuration.
+    /// > Note: Starting at the default position is always efficient, no matter which tolerances have been requested.
     public init(
         position: @escaping @autoclosure () -> Position = at(.zero),
         automaticallyPreservesTimeOffsetFromLive: Bool = false,
