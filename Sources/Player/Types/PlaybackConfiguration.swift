d36cfebf766140c558aa28745f75253f1b757d25
Samuel Défago <defagos@gmail.com>
2025-08-07 17:19:32 +0200

    Revert closure-based position retrieval API (#1264)

--- Sources/Player/Types/PlaybackConfiguration.swift
+++ Sources/Player/Types/PlaybackConfiguration.swift
@@ -11,27 +11,27 @@ public struct PlaybackConfiguration {
     /// The default configuration.
     public static let `default` = Self()
 
-    private let position: () -> Position
-    private let automaticallyPreservesTimeOffsetFromLive: Bool
-    private let preferredForwardBufferDuration: TimeInterval
-
-    /// Creates a playback configuration.
-    ///
-    /// - Parameters:
-    ///   - position: The position to start playback at.
-    ///   - automaticallyPreservesTimeOffsetFromLive: A Boolean value that indicates whether the player preserves its
-    ///     time offset from the live time after a buffering operation.
-    ///   - preferredForwardBufferDuration: The duration the player should buffer media from the network ahead of the
-    ///     playhead to guard against playback disruption.
+    /// The position to start playback at.
     ///
     /// When the position time is `.zero`, playback efficiently starts at the default position:
     ///
     /// - Zero for an on-demand stream.
     /// - Live edge for a livestream supporting DVR.
     ///
-    /// > Note: Starting at the default position is always efficient, no matter which tolerances have been requested.
+    /// Note that starting at the default position is always efficient, no matter which tolerances have been requested.
+    public let position: Position
+
+    /// A Boolean value that indicates whether the player preserves its time offset from the live time after a
+    /// buffering operation.
+    public let automaticallyPreservesTimeOffsetFromLive: Bool
+
+    /// The duration the player should buffer media from the network ahead of the playhead to guard against playback
+    /// disruption.
+    public let preferredForwardBufferDuration: TimeInterval
+
+    /// Creates a playback configuration.
     public init(
-        position: @escaping @autoclosure () -> Position = at(.zero),
+        position: Position = at(.zero),
         automaticallyPreservesTimeOffsetFromLive: Bool = false,
         preferredForwardBufferDuration: TimeInterval = 0
     ) {
@@ -41,9 +41,8 @@ public struct PlaybackConfiguration {
     }
 
     func apply(to item: AVPlayerItem, with metadata: PlayerMetadata) {
-        let position = position()
-        let seekPosition = position.after(metadata.blockedTimeRanges) ?? position
-        item.seek(to: seekPosition.time, toleranceBefore: seekPosition.toleranceBefore, toleranceAfter: seekPosition.toleranceAfter, completionHandler: nil)
+        let position = position.after(metadata.blockedTimeRanges) ?? position
+        item.seek(to: position.time, toleranceBefore: position.toleranceBefore, toleranceAfter: position.toleranceAfter, completionHandler: nil)
         item.automaticallyPreservesTimeOffsetFromLive = automaticallyPreservesTimeOffsetFromLive
         item.preferredForwardBufferDuration = preferredForwardBufferDuration
     }
