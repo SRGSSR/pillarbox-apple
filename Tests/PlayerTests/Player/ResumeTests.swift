0efe55f1043c4df9a8e3ea58adaa3a75374d16f1
Samuel Défago <defagos@gmail.com>
2025-08-08 15:57:14 +0200

    Do nothing when the item is unknown

--- Tests/PlayerTests/Player/ResumeTests.swift
+++ Tests/PlayerTests/Player/ResumeTests.swift
@@ -10,6 +10,8 @@ import CoreMedia
 import Nimble
 import PillarboxStreams
 
+// TODO: Check resumeContext against nil/not-nil to verify cleanup
+
 final class ResumeTests: TestCase {
     func testResumeInFirstItemAtCreationTime() {
         let item = PlayerItem.simple(url: Stream.onDemand.url)
@@ -39,9 +41,24 @@ final class ResumeTests: TestCase {
     }
 
     func testResumeInNonCurrentItem() {
+        let item1 = PlayerItem.simple(url: Stream.onDemand.url)
+        let item2 = PlayerItem.simple(url: Stream.onDemand.url)
+        let player = Player(items: [item1, item2])
+        expect(player.playbackState).toEventually(equal(.paused))
+        let time = CMTime(value: 11, timescale: 1)
+        player.resume(at(time), in: item2)
+        expect(player.currentItem).to(equal(item2))
+        expect(player.time()).toEventually(equal(time))
     }
 
     func testResumeInUnknownItem() {
+        let item = PlayerItem.simple(url: Stream.onDemand.url)
+        let unknownItem = PlayerItem.simple(url: Stream.onDemand.url)
+        let player = Player(item: item)
+        expect(player.playbackState).toEventually(equal(.paused))
+        player.resume(at(.init(value: 11, timescale: 1)), in: unknownItem)
+        expect(player.currentItem).to(equal(item))
+        expect(player.time()).to(equal(.zero))
     }
 
     func testResumeIsAppliedOnce() {
