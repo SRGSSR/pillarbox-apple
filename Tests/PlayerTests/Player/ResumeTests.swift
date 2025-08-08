fdbdbd7c828365a0bbb29d9005827acc860ca737
Samuel Défago <defagos@gmail.com>
2025-08-08 11:40:16 +0200

    Prepare tests

---
+++ Tests/PlayerTests/Player/ResumeTests.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@testable import PillarboxPlayer
+
+final class ResumeTests: TestCase {
+    func testResumeAfterCreation() {
+    }
+
+    func testResumeInOtherItemDuringPlayback() {
+    }
+
+    func testResumeInCurrentItemDuringPlayback() {
+    }
+
+    func testResumeInUnknownItem() {
+    }
+
+    func testResumeIsAppliedOnce() {
+    }
+
+    func testResumeOverridesPlaybackConfiguration() {
+    }
+
+    func testResumeIsPreservedByCompatibleItemUpdate() {
+    }
+
+    func testResumeIsDiscardedByIncompatibleItemUpdate() {
+    }
+}
