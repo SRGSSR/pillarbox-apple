6fa1100df4a3a9586d020d4d395feab6402ae7d3
Samuel Défago <defagos@gmail.com>
2025-08-11 15:07:36 +0200

    Add playback configuration tests

---
+++ Tests/PlayerTests/Types/PlaybackConfigurationTests.swift
@@ -0,0 +1,60 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@testable import PillarboxPlayer
+
+import AVFoundation
+import CoreMedia
+import Nimble
+import PillarboxStreams
+
+final class PlaybackConfigurationTests: TestCase {
+    func testDefaultValues() {
+        let configuration = PlaybackConfiguration()
+        expect(configuration.position.time).to(equal(.zero))
+        expect(configuration.automaticallyPreservesTimeOffsetFromLive).to(beFalse())
+        expect(configuration.preferredForwardBufferDuration).to(equal(0))
+    }
+
+    func testCustomValues() {
+        let time = CMTime(value: 7, timescale: 1)
+        let configuration = PlaybackConfiguration(
+            position: at(time),
+            automaticallyPreservesTimeOffsetFromLive: true,
+            preferredForwardBufferDuration: 11
+        )
+        expect(configuration.position.time).to(equal(time))
+        expect(configuration.automaticallyPreservesTimeOffsetFromLive).to(beTrue())
+        expect(configuration.preferredForwardBufferDuration).to(equal(11))
+    }
+
+    func testApplyToPlayerItem() {
+        let time = CMTime(value: 7, timescale: 1)
+        let configuration = PlaybackConfiguration(
+            position: at(time),
+            automaticallyPreservesTimeOffsetFromLive: true,
+            preferredForwardBufferDuration: 11
+        )
+        let playerItem = AVPlayerItem(url: Stream.onDemand.url)
+        configuration.apply(to: playerItem, with: .init())
+        expect(playerItem.currentTime()).to(equal(time))
+        expect(playerItem.automaticallyPreservesTimeOffsetFromLive).to(beTrue())
+        expect(playerItem.preferredForwardBufferDuration).to(equal(11))
+    }
+
+    func testApplyToPlayerItemWithPositionInBlockedTimeRange() {
+        let blockedTimeRangeEnd = CMTime(value: 17, timescale: 1)
+        let configuration = PlaybackConfiguration(position: at(.init(value: 7, timescale: 1)))
+        let playerItem = AVPlayerItem(url: Stream.onDemand.url)
+        configuration.apply(
+            to: playerItem,
+            with: .init(timeRanges: [
+                .init(kind: .blocked, start: .init(value: 2, timescale: 1), end: blockedTimeRangeEnd)
+            ])
+        )
+        expect(playerItem.currentTime()).to(equal(blockedTimeRangeEnd))
+    }
+}
