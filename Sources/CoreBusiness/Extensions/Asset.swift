a137b4edccd9b696c9c1ad491577a118b54ef816
Samuel Défago <defagos@gmail.com>
2025-08-14 13:45:05 +0200

    Add secret flag to enable modern content key session APIs

--- Sources/CoreBusiness/Extensions/Asset.swift
+++ Sources/CoreBusiness/Extensions/Asset.swift
@@ -7,6 +7,15 @@
 import Foundation
 import PillarboxPlayer
 
+private var kUsesContentKeySession = false
+
+/// Sets whether encrypted assets are delivered using modern content key session APIs or legacy APIs.
+///
+/// > Important: This setting is reserved to the Pillarbox development team. Please never modify.
+@_spi(Private) public func setUsesContentKeySession(_ usesContentKeySession: Bool) {
+    kUsesContentKeySession = usesContentKeySession
+}
+
 extension Asset {
     static func tokenProtected(url: URL, metadata: M, configuration: PlaybackConfiguration) -> Self {
         let id = UUID()
@@ -22,17 +31,21 @@ extension Asset {
         // FIXME: An issue affects `AVContentKeySession`, preventing key request creation after several hours (FB19383686).
         //        In the meantime we use a `.custom` asset but when the issue has been fixed we should use an `.encrypted`
         //        asset (removing `IrdetoResourceLoaderDelegate` in the process).
-        // .encrypted(
-        //     url: url,
-        //     delegate: IrdetoContentKeySessionDelegate(certificateUrl: certificateUrl),
-        //     metadata: metadata,
-        //     configuration: configuration
-        // )
-        .custom(
-            url: url,
-            delegate: IrdetoResourceLoaderDelegate(certificateUrl: certificateUrl),
-            metadata: metadata,
-            configuration: configuration
-        )
+        if kUsesContentKeySession {
+             .encrypted(
+                 url: url,
+                 delegate: IrdetoContentKeySessionDelegate(certificateUrl: certificateUrl),
+                 metadata: metadata,
+                 configuration: configuration
+             )
+        }
+        else {
+            .custom(
+                url: url,
+                delegate: IrdetoResourceLoaderDelegate(certificateUrl: certificateUrl),
+                metadata: metadata,
+                configuration: configuration
+            )
+        }
     }
 }
