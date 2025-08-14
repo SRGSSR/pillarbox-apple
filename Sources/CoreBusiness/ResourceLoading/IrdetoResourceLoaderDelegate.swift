29dbf17fd2b2d54090bd47ecc6813b787c5636c4
Samuel Défago <defagos@gmail.com>
2025-08-14 07:18:42 +0200

    Prepare legacy implementation

---
+++ Sources/CoreBusiness/ResourceLoading/IrdetoResourceLoaderDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import AVFoundation
+
+final class IrdetoResourceLoaderDelegate: NSObject, AVAssetResourceLoaderDelegate {
+    private let certificateUrl: URL
+
+    init(certificateUrl: URL) {
+        self.certificateUrl = certificateUrl
+    }
+
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest
+    ) -> Bool {
+        processRequest(loadingRequest)
+    }
+
+    func resourceLoader(
+        _ resourceLoader: AVAssetResourceLoader,
+        shouldWaitForRenewalOfRequestedResource renewalRequest: AVAssetResourceRenewalRequest
+    ) -> Bool {
+        processRequest(renewalRequest)
+    }
+
+    private func processRequest(_ loadingRequest: AVAssetResourceLoadingRequest) -> Bool {
+        return false
+    }
+}
