933da387691b59d2e3b06d55ccbdfa619bf69e58
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-09-03 15:38:14 +0200

    Improve error view

--- Demo/Sources/Players/PlaybackView.swift
+++ Demo/Sources/Players/PlaybackView.swift
@@ -834,34 +834,6 @@ private struct MainSystemView: View {
 
 #endif
 
-private struct PlaybackMessageView: View {
-    let title: String
-    let subtitle: String?
-
-    var body: some View {
-        VStack {
-            Text(title)
-                .foregroundColor(.white)
-                .font(.body)
-            if let subtitle {
-                Text(subtitle)
-                    .foregroundColor(Color(uiColor: .lightGray))
-                    .font(.subheadline)
-#if os(iOS)
-                    .textSelection(.enabled)
-#endif
-            }
-        }
-        .padding()
-        .multilineTextAlignment(.center)
-    }
-
-    init(title: String, subtitle: String? = nil) {
-        self.title = title
-        self.subtitle = subtitle
-    }
-}
-
 private struct PlaybackButton: View {
     @ObservedObject var player: Player
 
@@ -921,18 +893,82 @@ private struct ErrorView: View {
     private var subtitle: String? {
         let sessionIdentifiers = player.currentSessionIdentifiers(trackedBy: MetricsTracker.self)
         guard !sessionIdentifiers.isEmpty else { return nil }
-        return  "Monitoring: \(sessionIdentifiers.joined(separator: ", "))"
+        return "Monitoring: \(sessionIdentifiers.joined(separator: ", "))"
+    }
+
+    private var imageName: String {
+        switch error {
+        case let error as DataError:
+            switch error {
+            case let .blocked(reason: reason):
+                return Self.imageName(for: reason)
+            case .http:
+                return "cloud.fill"
+            case .noResourceAvailable:
+                return "exclamationmark.triangle.fill"
+            }
+        case let error as NSError where error.domain == NSURLErrorDomain && error.code == URLError.notConnectedToInternet.rawValue:
+            return "network.slash"
+        default:
+            return "exclamationmark.triangle.fill"
+        }
     }
 
     var body: some View {
         VStack {
+            messageView()
             PlaybackButton(player: player)
-            PlaybackMessageView(title: error.localizedDescription, subtitle: subtitle)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .overlay(alignment: .topLeading) {
             CloseButton(topBarStyle: true)
         }
+    }
+
+    // swiftlint:disable:next cyclomatic_complexity
+    private static func imageName(for reason: MediaComposition.BlockingReason) -> String {
+        switch reason {
+        case .ageRating12:
+            return "12.circle.fill"
+        case .ageRating18:
+            return "18.circle.fill"
+        case .commercial:
+            return "megaphone.fill"
+        case .endDate:
+            return "clock.fill"
+        case .geoblocked:
+            return "globe.europe.africa.fill"
+        case .journalistic:
+            return "newspaper.fill"
+        case .legal:
+            return "hand.raised.fill"
+        case .startDate:
+            return "key.icloud.fill"
+        case .unknown:
+            return "exclamationmark.triangle.fill"
+        case .vpnOrProxyDetected:
+            return "network.slash"
+        }
+    }
+
+    private func messageView() -> some View {
+        UnavailableView {
+            Label {
+                Text(error.localizedDescription)
+            } icon: {
+                Image(systemName: imageName)
+            }
+        } description: {
+            if let subtitle {
+                Text(subtitle)
+                    .foregroundStyle(.secondary)
+#if os(iOS)
+                    .textSelection(.enabled)
+#endif
+            }
+        }
+        .foregroundStyle(.white)
+        .fixedSize(horizontal: false, vertical: true)
     }
 }
 
@@ -959,11 +995,12 @@ struct PlaybackView: View {
                     .persistentSystemOverlays(.hidden)
             }
             else {
-                PlaybackMessageView(title: "No content")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .overlay(alignment: .topLeading) {
-                        CloseButton(topBarStyle: true)
-                    }
+                UnavailableView {
+                    Text("No content")
+                }
+                .overlay(alignment: .topLeading) {
+                    CloseButton(topBarStyle: true)
+                }
             }
         }
         .background(.black)
