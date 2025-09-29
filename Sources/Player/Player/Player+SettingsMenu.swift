adc3d46c287bdeca9aa2aa537a39260fa1ea9620
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-09-29 10:38:13 +0200

    Simplify menu labels

--- Sources/Player/Player/Player+SettingsMenu.swift
+++ Sources/Player/Player/Player+SettingsMenu.swift
@@ -93,11 +93,12 @@ private struct SettingsMenuContent: View {
                 action(.playbackSpeed(speed))
             }
         } label: {
-            Button(action: {}) {
+            Label {
                 Text("Playback Speed", bundle: .module, comment: "Playback setting menu title")
-                Text("\(player.playbackSpeed, specifier: "%g×")", bundle: .module, comment: "Speed multiplier")
+            } icon: {
                 Image(systemName: "speedometer")
             }
+            Text("\(player.playbackSpeed, specifier: "%g×")", bundle: .module, comment: "Speed multiplier")
         }
     }
 
@@ -105,11 +106,12 @@ private struct SettingsMenuContent: View {
         Menu {
             mediaSelectionMenuContent(characteristic: .audible)
         } label: {
-            Button(action: {}) {
+            Label {
                 Text("Audio", bundle: .module, comment: "Playback setting menu title")
-                Text(player.selectedMediaOption(for: .audible).displayName)
+            } icon: {
                 Image(systemName: "waveform.circle")
             }
+            Text(player.selectedMediaOption(for: .audible).displayName)
         }
     }
 
@@ -117,11 +119,12 @@ private struct SettingsMenuContent: View {
         Menu {
             mediaSelectionMenuContent(characteristic: .legible)
         } label: {
-            Button(action: {}) {
+            Label {
                 Text("Subtitles", bundle: .module, comment: "Playback setting menu title")
-                Text(player.selectedMediaOption(for: .legible).displayName)
+            } icon: {
                 Image(systemName: "captions.bubble")
             }
+            Text(player.selectedMediaOption(for: .legible).displayName)
         }
     }
 
