c97f77c5f0f09db0e528d40eb68dfc2f842e7fa1
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2025-11-05 10:30:21 +0100

    Introduce InfoViewAction

---
+++ Sources/Player/UserInterface/InfoViewAction.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import UIKit
+
+/// Action which show up in the info tab.
+@available(iOS, unavailable)
+@available(tvOS 16, *)
+public struct InfoViewAction {
+    /// Short display title.
+    public let title: String
+
+    /// Image that can appear next to this action.
+    public let image: UIImage?
+
+    /// The handler to invoke.
+    public let handler: () -> Void
+
+    /// Creates a new info view action.
+    ///
+    /// - Parameters:
+    ///   - title: Short display title.
+    ///   - image: Image that can appear next to this action.
+    ///   - handler: The handler to invoke.
+    public init(title: String, image: UIImage? = nil, handler: @escaping () -> Void) {
+        self.title = title
+        self.image = image
+        self.handler = handler
+    }
+}
