7cce384bfe7eee5ef64e84a4d07da570a8a91c90
Samuel Défago <defagos@gmail.com>
2025-11-21 11:44:19 +0100

    Force actions to be associated with an identifier

--- Demo/Sources/Players/PlaybackView.swift
+++ Demo/Sources/Players/PlaybackView.swift
@@ -735,7 +735,7 @@ private struct MainSystemView: View {
     @SystemVideoViewActionsContentBuilder7
     func contextualActionsActionsContent() -> SystemVideoViewActionsContent {
         if let skippableTimeRange = player.skippableTimeRange(at: progressTracker.time) {
-            .init(title: "Skip") {
+            .init(title: "Skip", identifier: .skip) {
                 player.seek(to: skippableTimeRange.end)
             }
         }
@@ -744,7 +744,7 @@ private struct MainSystemView: View {
     @SystemVideoViewActionsContentBuilder2
     func infoViewActionsContent() -> SystemVideoViewActionsContent {
         if player.canSkipToDefault() {
-            .init(title: skipInfoViewActionTitle, systemImage: skipInfoViewActionSystemImage) {
+            .init(title: skipInfoViewActionTitle, systemImage: skipInfoViewActionSystemImage, identifier: .skipToDefault) {
                 player.skipToDefault()
             }
         }
@@ -1165,3 +1165,12 @@ private extension Player {
 #Preview {
     PlaybackView(player: Player(item: URLMedia.onDemandVideoLocalHLS.item()))
 }
+
+#if os(tvOS)
+
+private extension UIAction.Identifier {
+    static let skip = UIAction.Identifier(rawValue: "skip")
+    static let skipToDefault = UIAction.Identifier(rawValue: "skip-to-default")
+}
+
+#endif
--- Sources/Player/UserInterface/SystemVideoViewAction.swift
+++ Sources/Player/UserInterface/SystemVideoViewAction.swift
@@ -11,6 +11,7 @@ import UIKit
 public struct SystemVideoViewAction {
     private let title: String
     private let image: UIImage?
+    private let identifier: UIAction.Identifier
     private let handler: () -> Void
 
     /// Creates an action.
@@ -18,11 +19,13 @@ public struct SystemVideoViewAction {
     /// - Parameters:
     ///   - title: Short display title.
     ///   - image: Image that can appear next to this action.
+    ///   - identifier: A unique identifier for the action.
     ///   - handler: The handler to invoke.
     @_disfavoredOverload
-    public init<S>(title: S, image: UIImage? = nil, handler: @escaping () -> Void) where S: StringProtocol {
+    public init<S>(title: S, image: UIImage? = nil, identifier: UIAction.Identifier, handler: @escaping () -> Void) where S: StringProtocol {
         self.title = String(title)
         self.image = image
+        self.identifier = identifier
         self.handler = handler
     }
 
@@ -31,9 +34,10 @@ public struct SystemVideoViewAction {
     /// - Parameters:
     ///   - title: Short display title.
     ///   - image: Image that can appear next to this action.
+    ///   - identifier: A unique identifier for the action.
     ///   - handler: The handler to invoke.
-    public init(title: LocalizedStringResource, image: UIImage? = nil, handler: @escaping () -> Void) {
-        self.init(title: String(localized: title), image: image, handler: handler)
+    public init(title: LocalizedStringResource, image: UIImage? = nil, identifier: UIAction.Identifier, handler: @escaping () -> Void) {
+        self.init(title: String(localized: title), image: image, identifier: identifier, handler: handler)
     }
 
     /// Creates an action.
@@ -41,10 +45,11 @@ public struct SystemVideoViewAction {
     /// - Parameters:
     ///   - title: Short display title.
     ///   - systemImage: System image name that can appear next to this action.
+    ///   - identifier: A unique identifier for the action.
     ///   - handler: The handler to invoke.
     @_disfavoredOverload
-    public init<S>(title: S, systemImage: String, handler: @escaping () -> Void) where S: StringProtocol {
-        self.init(title: title, image: UIImage(systemName: systemImage), handler: handler)
+    public init<S>(title: S, systemImage: String, identifier: UIAction.Identifier, handler: @escaping () -> Void) where S: StringProtocol {
+        self.init(title: title, image: UIImage(systemName: systemImage), identifier: identifier, handler: handler)
     }
 
     /// Creates an action.
@@ -52,21 +57,22 @@ public struct SystemVideoViewAction {
     /// - Parameters:
     ///   - title: Short display title.
     ///   - systemImage: System image name that can appear next to this action.
+    ///   - identifier: A unique identifier for the action.
     ///   - handler: The handler to invoke.
-    public init(title: LocalizedStringResource, systemImage: String, handler: @escaping () -> Void) {
-        self.init(title: title, image: UIImage(systemName: systemImage), handler: handler)
+    public init(title: LocalizedStringResource, systemImage: String, identifier: UIAction.Identifier, handler: @escaping () -> Void) {
+        self.init(title: title, image: UIImage(systemName: systemImage), identifier: identifier, handler: handler)
     }
 }
 
 extension SystemVideoViewAction {
     func toContextualAction() -> UIAction {
-        UIAction(title: title, image: image) { _ in
+        UIAction(title: title, image: image, identifier: identifier) { _ in
             handler()
         }
     }
 
     func toInfoViewAction(dismissing playerViewController: AVPlayerViewController) -> UIAction {
-        UIAction(title: title, image: image) { [weak playerViewController] _ in
+        UIAction(title: title, image: image, identifier: identifier) { [weak playerViewController] _ in
             playerViewController?.dismiss(animated: true)
             handler()
         }
