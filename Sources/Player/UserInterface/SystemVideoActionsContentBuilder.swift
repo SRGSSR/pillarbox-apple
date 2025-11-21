575fc419a3db8fd94e6f1fed4eb20c7878382379
Samuel Défago <defagos@gmail.com>
2025-11-21 11:23:06 +0100

    Add builder supporting up to 7 components

--- Sources/Player/UserInterface/SystemVideoActionsContentBuilder.swift
+++ Sources/Player/UserInterface/SystemVideoActionsContentBuilder.swift
@@ -19,13 +19,83 @@ public enum SystemVideoViewActionsContentBuilder2 {
     }
 
     // swiftlint:disable:next missing_docs
-    public static func buildBlock(_ component: Component) -> Component {
-        component
+    public static func buildBlock(_ c0: Component) -> Component {
+        c0
     }
 
     // swiftlint:disable:next missing_docs
     public static func buildBlock(_ c0: Component, _ c1: Component) -> Component {
         c0 + c1
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildOptional(_ component: Component?) -> Component {
+        component ?? []
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildEither(first component: Component) -> Component {
+        component
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildEither(second component: Component) -> Component {
+        component
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildFinalResult(_ component: Component) -> SystemVideoViewActionsContent {
+        .init(actions: component)
+    }
+}
+
+/// A result builder that enables declarative construction of one up to seven `SystemVideoViewAction`s.
+@resultBuilder
+public enum SystemVideoViewActionsContentBuilder7 {
+    /// Type of a statement expression.
+    public typealias Expression = SystemVideoViewAction
+
+    /// Type of a partial result.
+    public typealias Component = [SystemVideoViewAction]
+
+    // swiftlint:disable:next missing_docs
+    public static func buildExpression(_ expression: Expression) -> Component {
+        [expression]
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildBlock(_ c0: Component) -> Component {
+        c0
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildBlock(_ c0: Component, _ c1: Component) -> Component {
+        c0 + c1
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildBlock(_ c0: Component, _ c1: Component, _ c2: Component) -> Component {
+        c0 + c1 + c2
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildBlock(_ c0: Component, _ c1: Component, _ c2: Component, _ c3: Component) -> Component {
+        c0 + c1 + c2 + c3
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildBlock(_ c0: Component, _ c1: Component, _ c2: Component, _ c3: Component, _ c4: Component) -> Component {
+        c0 + c1 + c2 + c3 + c4
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildBlock(_ c0: Component, _ c1: Component, _ c2: Component, _ c3: Component, _ c4: Component, _ c5: Component) -> Component {
+        c0 + c1 + c2 + c3 + c4 + c5
+    }
+
+    // swiftlint:disable:next missing_docs
+    public static func buildBlock(_ c0: Component, _ c1: Component, _ c2: Component, _ c3: Component, _ c4: Component, _ c5: Component, _ c6: Component) -> Component {
+        c0 + c1 + c2 + c3 + c4 + c5 + c6
     }
 
     // swiftlint:disable:next missing_docs
---
+++ Tests/PlayerTests/UserInterface/SystemVideoViewActionsContentBuilder7CompilationChecks.swift
@@ -0,0 +1,118 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import PillarboxPlayer
+
+// swiftlint:disable:next type_name
+private enum SystemVideoViewActionsContentBuilder7CompilationChecks {
+    private static func Action() -> SystemVideoViewAction {
+        .init(title: "") {}
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func oneExpression() -> SystemVideoViewActionsContent {
+        Action()
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func twoExpressions() -> SystemVideoViewActionsContent {
+        Action()
+        Action()
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func threeExpressions() -> SystemVideoViewActionsContent {
+        Action()
+        Action()
+        Action()
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func fourExpressions() -> SystemVideoViewActionsContent {
+        Action()
+        Action()
+        Action()
+        Action()
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func fiveExpressions() -> SystemVideoViewActionsContent {
+        Action()
+        Action()
+        Action()
+        Action()
+        Action()
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func sixExpressions() -> SystemVideoViewActionsContent {
+        Action()
+        Action()
+        Action()
+        Action()
+        Action()
+        Action()
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func sevenExpressions() -> SystemVideoViewActionsContent {
+        Action()
+        Action()
+        Action()
+        Action()
+        Action()
+        Action()
+        Action()
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func expressionWithIfOnly() -> SystemVideoViewActionsContent {
+        if true {
+            Action()
+        }
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func expressionWithNestedIf() -> SystemVideoViewActionsContent {
+        if true {
+            if true {
+                Action()
+            }
+        }
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func expressionWithIfElse() -> SystemVideoViewActionsContent {
+        if true {
+            Action()
+        }
+        else {
+            Action()
+        }
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func expressionWithSwitch() -> SystemVideoViewActionsContent {
+        switch Int.random(in: 0...2) {
+        case 0:
+            Action()
+        case 1:
+            Action()
+        default:
+            Action()
+        }
+    }
+
+    @SystemVideoViewActionsContentBuilder7
+    static func expressionWithIfAvailable() -> SystemVideoViewActionsContent {
+        if #available(tvOS 16, *) {
+            Action()
+        }
+        else {
+            Action()
+        }
+    }
+}
