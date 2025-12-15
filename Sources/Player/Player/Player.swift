503f88701068867a80362d8252a60066f4051ef3
Samuel Défago <defagos@gmail.com>
2025-12-15 10:21:48 +0100

    Fix deallocation on iOS 18

--- Sources/Player/Player/Player.swift
+++ Sources/Player/Player/Player.swift
@@ -274,8 +274,9 @@ public final class Player: ObservableObject, Equatable {
 
     // Proactively clear items to avoid leak on iPadOS 18, see https://github.com/SRGSSR/pillarbox-apple/issues/1371.
     // TODO: Remove when iPadOS 18 is not supported anymore
-    private func clearItems_iPadOS18() {
-        guard ProcessInfo.processInfo.operatingSystemVersion.majorVersion == 18, UIDevice.current.userInterfaceIdiom == .pad else {
+    private func clearItems_iOS18() {
+#if os(iOS)
+        guard ProcessInfo.processInfo.operatingSystemVersion.majorVersion == 18 else {
             return
         }
 
@@ -283,11 +284,12 @@ public final class Player: ObservableObject, Equatable {
         DispatchQueue.main.async { [queuePlayer] in
             queuePlayer.removeAllItems()
         }
+#endif
     }
 
     deinit {
         uninstallRemoteCommands()
-        clearItems_iPadOS18()
+        clearItems_iOS18()
     }
 }
 
