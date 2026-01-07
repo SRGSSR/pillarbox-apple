ec3105d384c575c6b5bdf1f1fe714a5640e95364
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-01-07 11:33:34 +0100

    Use an identifier instead of the title

--- Sources/Player/UserInterface/DSL/SystemVideoViewTabs/Tab.swift
+++ Sources/Player/UserInterface/DSL/SystemVideoViewTabs/Tab.swift
@@ -9,22 +9,25 @@ import SwiftUI
 /// A type that describing the content displayed in a custom info view controller on tvOS.
 public struct Tab<Content>: InfoViewTabsElement where Content: View {
     let title: String
+    let identifier: String
     let content: Content
 
     /// Creates a new `Self` instance
     /// - Parameters:
     ///   - title: The title of the custom info view.
     ///   - views: A `ViewBuilder` closure that provides the SwiftUI view to be displayed inside the custom info view controller.
-    public init(title: String, @ViewBuilder _ content: () -> Content) {
+    public init(title: String, identifier: String? = nil, @ViewBuilder _ content: () -> Content) {
         self.title = title
+        self.identifier = identifier ?? title
         self.content = content()
     }
 
     // swiftlint:disable:next missing_docs
     public func viewController(reusing viewControllers: [UIViewController]) -> UIViewController {
-        let viewController = viewControllers.first { $0.title == title }
+        let viewController = viewControllers.first { $0.restorationIdentifier == identifier }
         let hostingController = hostingController(reusing: viewController)
         hostingController.title = title
+        hostingController.restorationIdentifier = identifier
         return hostingController
     }
 
