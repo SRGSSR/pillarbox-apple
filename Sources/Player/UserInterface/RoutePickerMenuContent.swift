fe184a6b376048d5dc29704f24fb516726eb919a
Samuel Défago <defagos@gmail.com>
2026-01-27 10:38:06 +0100

    Provide menu helper for route selection (#1410)

---
+++ Sources/Player/UserInterface/RoutePickerMenuContent.swift
@@ -0,0 +1,57 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import AVKit
+import SwiftUI
+
+@propertyWrapper
+private class Control {
+    fileprivate weak var source: UIControl?
+
+    var wrappedValue: Control {
+        self
+    }
+
+    func sendAction() {
+        source?.sendActions(for: .touchUpInside)
+    }
+}
+
+private struct RoutePickerWrapper: UIViewRepresentable {
+    let control: Control
+
+    func makeUIView(context: Context) -> AVRoutePickerView {
+        let view = AVRoutePickerView()
+        view.isHidden = true
+        return view
+    }
+
+    func updateUIView(_ uiView: AVRoutePickerView, context: Context) {
+        guard let button = uiView.subviews.first(where: { $0 is UIButton }) as? UIButton else { return }
+        control.source = button
+    }
+}
+
+struct RoutePickerMenuContent: View {
+    @Control private var control
+
+    var body: some View {
+        SwiftUI.Button(action: sendAction) {
+            Label {
+                Text(verbatim: "AirPlay")
+            } icon: {
+                Image(systemName: "airplay.video")
+            }
+            .background {
+                RoutePickerWrapper(control: control)
+            }
+        }
+    }
+
+    private func sendAction() {
+        control.sendAction()
+    }
+}
