d0b25f2646089813dd5a1e2e8ce52ab52cbc2ae0
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-02-09 08:55:05 +0100

    Make assetProvider as trailing closure

--- Sources/StandardConnector/Extensions/PlayerItem.swift
+++ Sources/StandardConnector/Extensions/PlayerItem.swift
@@ -13,13 +13,13 @@ public extension PlayerItem {
     /// - Parameters:
     ///   - request: The URL request.
     ///   - type: The custom data type to decode the response.
-    ///   - assetProvider: A closure for creating an `Asset` from a `PlayerData`.
     ///   - trackerAdapters: An array of `TrackerAdapter` instances to use for tracking playback events.
+    ///   - assetProvider: A closure for creating an `Asset` from a `PlayerData`.
     static func standard<CustomData: Decodable>(
         request: URLRequest,
         type: CustomData.Type = EmptyCustomData.self,
-        assetProvider: @escaping (PlayerData<CustomData>) -> Asset<PlayerData<CustomData>>,
         trackerAdapters: [TrackerAdapter<PlayerData<CustomData>>] = [],
+        assetProvider: @escaping (PlayerData<CustomData>) -> Asset<PlayerData<CustomData>>
     ) -> Self {
         .init(
             publisher: publisher(request: request, type: type, assetProvider: assetProvider),
