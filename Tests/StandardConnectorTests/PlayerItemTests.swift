bfcfa3dfbe025e87858cf4f2653d3e40767ed890
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-02-13 10:40:27 +0100

    Add testHttpError

--- Tests/StandardConnectorTests/PlayerItemTests.swift
+++ Tests/StandardConnectorTests/PlayerItemTests.swift
@@ -6,11 +6,43 @@
 
 import Foundation
 import Nimble
-import PillarboxStandardConnector
+import PillarboxPlayer
+
+@_spi(StandardConnectorPrivate)
+@testable import PillarboxStandardConnector
+
 import XCTest
 
+private enum ErrorMock: Error {
+    case unknown
+}
+
 final class PlayerItemTests: XCTestCase {
-    func testSimple() {
-        expect(true).to(beTrue())
+    override func setUp() {
+        super.setUp()
+        URLProtocol.registerClass(URLProtocolMock.self)
+    }
+
+    override func tearDown() {
+        URLProtocol.registerClass(URLProtocolMock.self)
+        URLProtocolMock.responseHandler = nil
+        super.tearDown()
+    }
+
+    func testHttpError() {
+        URLProtocolMock.responseHandler = { request in
+            HttpResponseHandler(
+                data: nil,
+                response: HTTPURLResponse(url: request.url!, statusCode: 404, httpVersion: nil, headerFields: nil),
+                error: nil
+            )
+        }
+
+        let playerItem = PlayerItem.standard(request: URLRequest(url: URL(string: "https://standard.connector.pillarbox.ch")!)) { playerData in
+            Asset.unavailable(with: ErrorMock.unknown, metadata: playerData)
+        }
+
+        let player = Player(item: playerItem)
+        expect { (player.error as? HttpError)?.statusCode }.toEventually(equal(404))
     }
 }
