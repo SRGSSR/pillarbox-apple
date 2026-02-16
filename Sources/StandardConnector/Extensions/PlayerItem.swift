c5083d274cb91cd9b9839f576a6d9da29669e5f6
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-02-16 10:07:39 +0100

    Add a json decoder as parameter

--- Sources/StandardConnector/Extensions/PlayerItem.swift
+++ Sources/StandardConnector/Extensions/PlayerItem.swift
@@ -13,16 +13,18 @@ public extension PlayerItem {
     /// - Parameters:
     ///   - request: The URL request.
     ///   - type: The custom data type to decode the response.
+    ///   - decoder: A JSON decoder.
     ///   - trackerAdapters: An array of `TrackerAdapter` instances to use for tracking playback events.
     ///   - assetProvider: A closure for creating an `Asset` from a `PlayerData`.
     static func standard<CustomData>(
         request: URLRequest,
         type: CustomData.Type = EmptyCustomData.self,
+        decoder: JSONDecoder = JSONDecoder(),
         trackerAdapters: [TrackerAdapter<PlayerData<CustomData>>] = [],
         assetProvider: @escaping (PlayerData<CustomData>) -> Asset<PlayerData<CustomData>>
     ) -> Self where CustomData: Decodable {
         .init(
-            publisher: publisher(request: request, type: type, assetProvider: assetProvider),
+            publisher: publisher(request: request, type: type, decoder: decoder, assetProvider: assetProvider),
             trackerAdapters: trackerAdapters
         )
     }
@@ -32,12 +34,13 @@ private extension PlayerItem {
     static func publisher<CustomData>(
         request: URLRequest,
         type: CustomData.Type,
+        decoder: JSONDecoder,
         assetProvider: @escaping (PlayerData<CustomData>) -> Asset<PlayerData<CustomData>>,
     ) -> AnyPublisher<Asset<PlayerData<CustomData>>, Error> where CustomData: Decodable {
         URLSession.shared.dataTaskPublisher(for: request)
             .mapHttpErrors()
             .map(\.data)
-            .decode(type: PlayerData<CustomData>.self, decoder: JSONDecoder())
+            .decode(type: PlayerData<CustomData>.self, decoder: decoder)
             .map { assetProvider($0) }
             .eraseToAnyPublisher()
     }
