18f07d7d3fcb11f8a24e2b71f43725277bc9bbb3
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-03-04 11:17:37 +0100

    Add DownloadsView

---
+++ Demo/Sources/Downloads/DownloadsView.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SwiftUI
+
+struct DownloadsView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    DownloadsView()
+}
