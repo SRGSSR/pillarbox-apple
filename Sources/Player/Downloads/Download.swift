30205740267cde2e53f27b500fa0c19af3c9ab38
Samuel Défago <defagos@gmail.com>
2026-03-05 15:06:25 +0100

    Cleanup implementation

--- Sources/Player/Downloads/Download.swift
+++ Sources/Player/Downloads/Download.swift
@@ -8,38 +8,24 @@ import AVFoundation
 
 public final class Download: ObservableObject {
     private let id = UUID()
-    let task: URLSessionTask
 
     public var title: String? {
-        task.taskDescription
+        nil
     }
 
     @Published public private(set) var state: AVAssetDownloadTask.State = .running
     @Published public private(set) var progress: Double = 0
 
     init(task: URLSessionTask) {
-        self.task = task
 
-        task.publisher(for: \.state)
-            .receiveOnMainThread()
-            .assign(to: &$state)
-
-        task.progress.publisher(for: \.fractionCompleted)
-            .map { $0.clamped(to: 0...1) }
-            .receiveOnMainThread()
-            .assign(to: &$progress)
     }
 
     public func resume() {
-        task.resume()
+
     }
 
     public func suspend() {
-        task.suspend()
-    }
 
-    func cancel() {
-        task.cancel()
     }
 }
 
--- Sources/Player/Downloads/Downloader.swift
+++ Sources/Player/Downloads/Downloader.swift
@@ -7,16 +7,9 @@
 import AVFoundation
 import OrderedCollections
 
-private enum Location {
-    case empty
-    case fileUrl(URL)
-}
-
 #if DEBUG
 @_spi(DownloaderPrivate)
 public final class Downloader: NSObject, ObservableObject {
-    @Published private var _downloads: OrderedDictionary<Download, Location> = [:]
-
     private lazy var session = AVAssetDownloadURLSession(
         configuration: .background(withIdentifier: "ch.srgssr.player.downloader"),
         assetDownloadDelegate: self,
@@ -24,74 +17,25 @@ public final class Downloader: NSObject, ObservableObject {
     )
 
     public var downloads: [Download] {
-        Array(_downloads.keys)
+        []
     }
 
-    override public init() {
-        super.init()
-        restore()
-    }
+    override public init() {}
 
-    @discardableResult
-    public func add(title: String, url: URL) -> Download {
-        let download = Download(task: downloadTask(title: title, url: url))
-        download.resume()
-        _downloads[download] = .empty
-        return download
-    }
+    public func add(title: String, url: URL) {}
 
-    public func remove(_ download: Download) {
-        download.cancel()
-        removeFile(for: download)
-        _downloads.removeValue(forKey: download)
-    }
+    public func remove(_ download: Download) {}
 
     public func fileUrl(for download: Download) -> URL? {
-        if let location = _downloads[download], case let .fileUrl(fileUrl) = location {
-            return fileUrl
-        }
-        else {
-            return nil
-        }
-    }
-
-    private func removeFile(for download: Download) {
-        guard let fileUrl = fileUrl(for: download) else { return }
-        try? FileManager.default.removeItem(at: fileUrl)
-    }
-
-    private func downloadTask(title: String, url: URL) -> AVAssetDownloadTask {
-        let asset = AVURLAsset(url: url)
-        let configuration = AVAssetDownloadConfiguration(asset: asset, title: title)
-        let task = session.makeAssetDownloadTask(downloadConfiguration: configuration)
-        task.taskDescription = title
-        return task
-    }
-
-    private func restore(from tasks: [URLSessionTask]) {
-        for task in tasks {
-            let download = Download(task: task)
-            _downloads[download] = .empty
-        }
-    }
-
-    private func restore() {
-        session.getAllTasks { [weak self] tasks in
-            self?.restore(from: tasks)
-        }
+        nil
     }
 }
 
 extension Downloader: AVAssetDownloadDelegate {
     public func urlSession(_ session: URLSession, assetDownloadTask: AVAssetDownloadTask, willDownloadTo location: URL) {
-        print("--> location: \(location)")
-        if let download = _downloads.keys.first(where: { $0.task == assetDownloadTask }) {
-            _downloads[download] = .fileUrl(location)
-        }
     }
 
     public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: (any Error)?) {
-        print("Error: --> \(error)")
     }
 }
 
