9ba2cee0d438d4a04ec054ab91f1f03b50d62773
Samuel Défago <defagos@gmail.com>
2026-03-08 14:45:44 +0100

    Remove test labels

--- Demo/Sources/Model/Media.swift
+++ Demo/Sources/Model/Media.swift
@@ -114,10 +114,7 @@ struct Media: Hashable {
                         DemoTracker.Metadata(title: metadata.mainChapter.title)
                     }
                 ],
-                context: .init(
-                    configuration: .init(position: at(startTime)),
-                    commandersActSource: .init(page: .init(identifier: "my_page"), section: .init(identifier: "my_section"))
-                )
+                context: .init(configuration: .init(position: at(startTime)))
             )
         }
     }
