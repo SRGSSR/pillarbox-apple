e6cf06c191d767bd5a8258eff576588fdc8eebfc
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-03-11 15:12:49 +0100

    Simplify allowsPartial concept

---
+++ Sources/Player/Downloads/DownloadLink.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Foundation
+
+public enum DownloadLink: Equatable {
+    case available(URL)
+    case missing
+}
