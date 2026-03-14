3f7d2e6c54baf48c8210ddd9c26957b368589874
Samuel Défago <defagos@gmail.com>
2026-03-14 10:37:21 +0100

    Improve implementation

--- Demo/Sources/Downloads/DownloadsView.swift
+++ Demo/Sources/Downloads/DownloadsView.swift
@@ -17,52 +17,62 @@ struct DownloadsView: View {
     var body: some View {
         ZStack {
             if !downloader.downloads.isEmpty {
-                List {
-                    ForEach(Array(downloader.downloads), id: \.self) { download in
-                        DownloadCell(download: download)
-                    }
-                    .onDelete { indexes in
-                        for index in indexes.reversed() {
-                            downloader.remove(downloader.downloads[index])
-                        }
-                    }
-                }
+                mainView()
             }
             else {
-                UnavailableView {
-                    Label {
-                        Text("No downloads.")
-                    } icon: {
-                        Image(systemName: "square.and.arrow.down")
-                    }
-                }
+                emptyView()
             }
         }
         .animation(.defaultLinear, value: downloader.downloads)
-        .toolbar {
-            Menu {
-                // Warning: Use /ww/ streams only since /ch/-ones are AES-encrypted and cannot be played offline.
-                button(
-                    title: "Short video",
-                    url: "https://rts-vod-amd.akamaized.net/ww/13317145/f1d49f18-f302-37ce-866c-1c1c9b76a824/master.m3u8"
-                )
-                button(
-                    title: "Medium video",
-                    url: "https://rts-vod-amd.akamaized.net/ww/cc16c4b0-1c15-326d-958b-faad09e216c1/ffc39d4f-eb00-3979-a5bd-0e3b93b99073/master.m3u8"
-                )
-                button(
-                    title: "Long video",
-                    url: "https://rts-vod-amd.akamaized.net/ww/14970442/4dcba1d3-8cc8-3667-a7d2-b3b92c4243d9/master.m3u8"
-                )
-                button(
-                    title: "MP3",
-                    url: "https://rts-aod-dd.akamaized.net/ww/13306839/63cc2653-8305-3894-a448-108810b553ef.mp3"
-                )
-            } label: {
-                Image(systemName: "plus")
+        .toolbar(content: menu)
+        .navigationTitle("Downloads")
+    }
+
+    private func mainView() -> some View {
+        List {
+            ForEach(Array(downloader.downloads), id: \.self) { download in
+                DownloadCell(download: download)
+            }
+            .onDelete { indexes in
+                for index in indexes.reversed() {
+                    downloader.remove(downloader.downloads[index])
+                }
             }
         }
-        .navigationTitle("Downloads")
+    }
+
+    private func emptyView() -> some View {
+        UnavailableView {
+            Label {
+                Text("No downloads.")
+            } icon: {
+                Image(systemName: "square.and.arrow.down")
+            }
+        }
+    }
+
+    private func menu() -> some View {
+        Menu {
+            // Warning: Use /ww/ streams only since /ch/-ones are AES-encrypted and cannot be played offline.
+            button(
+                title: "Short video",
+                url: "https://rts-vod-amd.akamaized.net/ww/13317145/f1d49f18-f302-37ce-866c-1c1c9b76a824/master.m3u8"
+            )
+            button(
+                title: "Medium video",
+                url: "https://rts-vod-amd.akamaized.net/ww/cc16c4b0-1c15-326d-958b-faad09e216c1/ffc39d4f-eb00-3979-a5bd-0e3b93b99073/master.m3u8"
+            )
+            button(
+                title: "Long video",
+                url: "https://rts-vod-amd.akamaized.net/ww/14970442/4dcba1d3-8cc8-3667-a7d2-b3b92c4243d9/master.m3u8"
+            )
+            button(
+                title: "MP3",
+                url: "https://rts-aod-dd.akamaized.net/ww/13306839/63cc2653-8305-3894-a448-108810b553ef.mp3"
+            )
+        } label: {
+            Image(systemName: "plus")
+        }
     }
 
     private func button(title: String, url: URL) -> some View {
