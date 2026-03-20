52c82b550de40f51e743ea3c356be9f65e03e52f
Samuel Défago <defagos@gmail.com>
2026-03-20 15:53:28 +0100

    Add asset loader protocol

---
+++ Sources/Player/Interfaces/AssetLoader.swift
@@ -0,0 +1,22 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import Combine
+
+/// A protocol defining how an asset is loaded.
+public protocol AssetLoader {
+    /// The input expected to load an asset.
+    associatedtype Input: Codable
+
+    /// The metadata delivered when loading an asset.
+    associatedtype Metadata
+
+    /// A publisher that delivers the asset matching a given input.
+    static func publisher(for input: Input) -> AnyPublisher<Asset<Metadata>, Error>
+
+    /// Converts metadata to player metadata.
+    static func playerMetadata(from metadata: Metadata) -> PlayerMetadata
+}
