fe0ea36c3d3d214e4cea2438ff338a433bba481d
Samuel Défago <defagos@gmail.com>
2026-03-21 18:38:44 +0100

    Polish documentation

--- Sources/Player/Interfaces/AssetLoader.swift
+++ Sources/Player/Interfaces/AssetLoader.swift
@@ -11,7 +11,7 @@ public protocol AssetLoader {
     /// The input expected to load an asset.
     associatedtype Input
 
-    /// The metadata delivered when loading an asset.
+    /// The metadata delivered with the asset.
     associatedtype Metadata
 
     /// A publisher that delivers the asset matching a given input.
@@ -19,10 +19,6 @@ public protocol AssetLoader {
 
     /// Converts metadata to player metadata.
     static func playerMetadata(from metadata: Metadata) -> PlayerMetadata
-
-    // TODO: If `Input` or `Metadata` need to be serialized we can simply add protocol methods which take an input or
-    //       metadata as parameter and must return some codable type (instead of asking for `Input` and `Metadata` to
-    //       themselves conform to Codable, which is more involved).
 }
 
 public extension AssetLoader where Metadata == PlayerMetadata {
