5279d1f29738617e594f61856e0d1bf933a3ade6
Samuel Défago <defagos@gmail.com>
2026-04-21 18:56:45 +0200

    Improve implementation

--- Demo/Sources/Search/SearchView.swift
+++ Demo/Sources/Search/SearchView.swift
@@ -26,6 +26,14 @@ struct SearchView: View {
             case .loading:
                 ProgressView()
                     .accessibilityHidden(true)
+            case let .loaded(medias: medias) where medias.isEmpty:
+                UnavailableModelView(model: model) {
+                    Label {
+                        Text("No results.")
+                    } icon: {
+                        Image(systemName: "circle.slash")
+                    }
+                }
             case let .loaded(medias: medias):
                 loadedView(medias)
             case let .failed(error):
@@ -53,46 +61,35 @@ struct SearchView: View {
 
     @ViewBuilder
     private func loadedView(_ medias: [SRGMedia]) -> some View {
-        if !medias.isEmpty {
-            CustomList(data: medias) { media in
-                if let media {
-                    Cell(
-                        size: .init(width: 520, height: 300),
-                        title: constant(iOS: MediaDescription.title(for: media), tvOS: media.show?.title),
-                        subtitle: constant(iOS: MediaDescription.subtitle(for: media), tvOS: media.title),
-                        imageUrl: SRGDataProvider.current!.url(for: media.image, size: .large),
-                        type: MediaDescription.systemImage(for: media),
-                        duration: MediaDescription.duration(for: media),
-                        date: MediaDescription.date(for: media),
-                        style: MediaDescription.style(for: media)
-                    ) {
-                        let media = Media(title: media.title, type: .urn(media.urn))
-                        router.presented = .player(media: media)
+        CustomList(data: medias) { media in
+            if let media {
+                Cell(
+                    size: .init(width: 520, height: 300),
+                    title: constant(iOS: MediaDescription.title(for: media), tvOS: media.show?.title),
+                    subtitle: constant(iOS: MediaDescription.subtitle(for: media), tvOS: media.title),
+                    imageUrl: SRGDataProvider.current!.url(for: media.image, size: .large),
+                    type: MediaDescription.systemImage(for: media),
+                    duration: MediaDescription.duration(for: media),
+                    date: MediaDescription.date(for: media),
+                    style: MediaDescription.style(for: media)
+                ) {
+                    let media = Media(title: media.title, type: .urn(media.urn))
+                    router.presented = .player(media: media)
+                }
+                .onAppear {
+                    if let index = medias.firstIndex(of: media), medias.count - index < kPageSize {
+                        model.loadMore()
                     }
-                    .onAppear {
-                        if let index = medias.firstIndex(of: media), medias.count - index < kPageSize {
-                            model.loadMore()
-                        }
-                    }
+                }
 #if os(iOS)
-                    .swipeActions { CopyActions(text: media.urn) }
-                    .refreshable { await model.refresh() }
+                .swipeActions { CopyActions(text: media.urn) }
+                .refreshable { await model.refresh() }
 #else
-                    .ignoresSafeArea(.all, edges: .horizontal)
+                .ignoresSafeArea(.all, edges: .horizontal)
 #endif
-                }
-            }
-            .scrollDismissesKeyboard(.immediately)
-        }
-        else {
-            UnavailableModelView(model: model) {
-                Label {
-                    Text("No results.")
-                } icon: {
-                    Image(systemName: "circle.slash")
-                }
             }
         }
+        .scrollDismissesKeyboard(.immediately)
     }
 }
 
