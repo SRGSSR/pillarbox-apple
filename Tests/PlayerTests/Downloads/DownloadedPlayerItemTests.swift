9539f190fb2e4fda2c04c0432178c624ffe01b2b
Samuel Défago <defagos@gmail.com>
2026-05-21 17:06:32 +0200

    Prepare item from download test suite

---
+++ Tests/PlayerTests/Downloads/DownloadedPlayerItemTests.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+@_spi(DownloaderPrivate)
+@testable import PillarboxPlayer
+
+import XCTest
+
+@available(tvOS, unavailable)
+final class DownloadPlayerItemTests: TestCase {
+    // TODO: Test PlayerItem initializer from a Download.
+}
