7eae6081f5035ac095314c2906392583b153f529
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-06-04 15:59:20 +0200

    Start implementing the URN store

--- Sources/CoreBusiness/Downloads/URNAssetDownloadStore.swift
+++ Sources/CoreBusiness/Downloads/URNAssetDownloadStore.swift
@@ -5,27 +5,78 @@
 //
 
 import Foundation
+import SwiftData
 
 // swiftlint:disable missing_docs
 
 @_spi(DownloaderPrivate)
 import PillarboxPlayer
 
+private struct DownloadError: LocalizedError {
+    let errorDescription: String?
+
+    init?(errorDescription: String?) {
+        guard let errorDescription else { return nil }
+        self.errorDescription = errorDescription
+    }
+}
+
+@available(iOS 17.0, *)
 public final class URNAssetDownloadStore {
-    public init() {}
+    let context: ModelContext
+
+    public init() {
+        let schema = Schema([Entry.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        self.context = .init(try! ModelContainer(for: schema, configurations: [modelConfiguration]))
+    }
+}
+
+@available(iOS 17.0, *)
+extension URNAssetDownloadStore {
+    @Model
+    class Entry {
+        var id: String
+        var urn: String
+        var bookmarkData: Data?
+        var progress: Double
+        var errorDescription: String?
+
+        init(id: String, urn: String, bookmarkData: Data? = nil, progress: Double, errorDescription: String? = nil) {
+            self.id = id
+            self.urn = urn
+            self.bookmarkData = bookmarkData
+            self.progress = progress
+            self.errorDescription = errorDescription
+        }
+
+        func toRecord() -> DownloadRecord<URNAssetLoader.Input, MediaMetadata> {
+            .init(
+                input: .init(urn: urn, server: .production, configuration: .default),
+                metadata: nil,
+                bookmarkData: bookmarkData,
+                progress: progress,
+                error: DownloadError(errorDescription: errorDescription)
+            )
+        }
+    }
 }
 
 @_spi(DownloaderPrivate)
+@available(iOS 17.0, *)
 extension URNAssetDownloadStore: AssetDownloadStore {
     public static func id(from input: URNAssetLoader.Input) -> String {
         input.urn
     }
 
     public func downloadRecords() -> [DownloadRecord<URNAssetLoader.Input, MediaMetadata>] {
-        []
+        guard let entries = try? context.fetch(FetchDescriptor<Entry>()) else { return [] }
+        return entries.map { $0.toRecord() }
     }
 
-    public func addDownloadRecord(using input: URNAssetLoader.Input, forId id: String) {}
+    public func addDownloadRecord(using input: URNAssetLoader.Input, forId id: String) {
+        context.insert(Entry(id: id, urn: input.urn, progress: 0))
+    }
 
     public func removeDownloadRecord(forId id: String) {}
 
