70d3fbfe95ad615600bccd00b11f45fe815acfc8
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-07-20 10:05:12 +0200

    Remove useless constraint

--- Sources/Player/Downloads/Downloader.swift
+++ Sources/Player/Downloads/Downloader.swift
@@ -18,7 +18,7 @@ public final class Downloader<S>: ObservableObject where S: AssetDownloadStore {
 
     @Published public private(set) var downloads: [Download] = []
 
-    public init<L>(configuration: URLSessionConfiguration, store: S) where L: AssetLoader, L == S.Loader {
+    public init(configuration: URLSessionConfiguration, store: S) {
         let manager = DownloadManager(store: store, session: URLDownloadSession(configuration: configuration))
         self.manager = manager
 
