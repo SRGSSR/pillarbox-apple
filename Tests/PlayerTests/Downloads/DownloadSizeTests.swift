5b5159c6dbb9ed3c5b04e63a3fc4c8ff6a08201b
Samuel Défago <defagos@gmail.com>
2026-08-10 10:29:12 +0200

    Support size extraction from local URL
    1

--- Tests/PlayerTests/Downloads/DownloadSizeTests.swift
+++ Tests/PlayerTests/Downloads/DownloadSizeTests.swift
@@ -7,7 +7,9 @@
 @_spi(DownloaderPrivate)
 @testable import PillarboxPlayer
 
+import Foundation
 import Nimble
+import PillarboxStreams
 
 @available(tvOS, unavailable)
 final class DownloadSizeTests: TestCase {
@@ -37,5 +39,34 @@ final class DownloadSizeTests: TestCase {
         let size = try unwrap(DownloadSize(completed: -10, total: 100))
         expect(size.completed).to(equal(0))
         expect(size.total).to(equal(100))
+    }
+
+    func testSingleFileUrl() throws {
+        let url = try unwrap(Bundle.module.url(forResource: "file_10_bytes", withExtension: nil, subdirectory: "FileResources"))
+        let size = try unwrap(DownloadSize(url: url))
+        expect(size.completed).to(equal(10))
+        expect(size.total).to(equal(10))
+    }
+
+    func testFolderUrl() throws {
+        let url = try unwrap(Bundle.module.url(forResource: "folder_20_bytes", withExtension: nil, subdirectory: "FileResources"))
+        let size = try unwrap(DownloadSize(url: url))
+        expect(size.completed).to(equal(20))
+        expect(size.total).to(equal(20))
+    }
+
+    func testNestedFolderUrl() throws {
+        let url = try unwrap(Bundle.module.url(forResource: "nested_folder_30_bytes", withExtension: nil, subdirectory: "FileResources"))
+        let size = try unwrap(DownloadSize(url: url))
+        expect(size.completed).to(equal(30))
+        expect(size.total).to(equal(30))
+    }
+
+    func testNonLocalUrl() {
+        expect(DownloadSize(url: Stream.onDemand.url)).to(beNil())
+    }
+
+    func testMissingFileUrl() {
+        expect(DownloadSize(url: URL(filePath: "/tmp/missing"))).to(beNil())
     }
 }
