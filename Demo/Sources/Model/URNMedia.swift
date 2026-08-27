91532261035350a7a1bf0604146067fe05e00484
Samuel Défago <defagos@gmail.com>
2026-08-27 15:49:42 +0200

    Use thumbnail matching the updated content
    Co-authored-by: Walid Kayhal <3347810+waliid@users.noreply.github.com>

--- Demo/Sources/Model/URNMedia.swift
+++ Demo/Sources/Model/URNMedia.swift
@@ -17,7 +17,7 @@ enum URNMedia {
     )
     static let onDemandVerticalVideo = Media(
         title: "Vertical video",
-        imageUrl: "https://www.rts.ch/2022/10/06/17/32/13444380.image/4x5",
+        imageUrl: "https://img.rts.ch/medias/2023/image/w2e9c5-26920079.image/9x16",
         type: .urn("urn:rts:video:13950405")
     )
     static let onDemandVideo = Media(
