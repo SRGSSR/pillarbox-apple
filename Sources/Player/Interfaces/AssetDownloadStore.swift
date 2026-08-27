5e0a3ace30c716629c421b9b1522984bab96b2e8
Samuel Défago <defagos@gmail.com>
2026-08-27 15:05:51 +0200

    Document AssetLoader and AssetDownloadStore

--- Sources/Player/Interfaces/AssetDownloadStore.swift
+++ Sources/Player/Interfaces/AssetDownloadStore.swift
@@ -4,40 +4,94 @@
 //  License information is available from the LICENSE file.
 //
 
-// swiftlint:disable missing_docs
-
 #if DEBUG
 
 import Combine
 import Foundation
 
+/// A protocol defining how downloads are loaded and persisted.
 @_spi(DownloaderPrivate)
 @available(tvOS, unavailable)
 public protocol AssetDownloadStore: AnyObject {
+    /// The type of loader used to load the underlying asset.
     associatedtype Loader: AssetLoader
+
+    /// A custom type that describes data to persist with each download.
+    ///
+    /// Persisted data can be reused when creating trackers for local player items returned by ``Downloader/playerItem(for:trackerAdapters:)``.
+    /// Any data required to create a local ``Asset`` should also be persisted.
     associatedtype CustomData
 
+    /// Creates a unique identifier for the given download input.
+    ///
+    /// - Parameter input: The input that identifies the download.
+    /// - Returns: A string that uniquely identifies the download.
     static func id(from input: Loader.Input) -> String
 
+    /// Extracts a custom subset of metadata from the asset loader metadata.
+    ///
+    /// - Parameter metadata: The original metadata provided by the asset loader.
+    /// - Returns: The metadata subset to persist with the download.
     static func customData(from metadata: Loader.Metadata) -> CustomData
+
+    /// Creates an asset from a local file URL and custom data.
+    ///
+    /// - Parameters:
+    ///   - fileUrl: The URL of the file to be played.
+    ///   - customData: Custom data associated with the download.
+    /// - Returns: A playable local asset. Implementations that create custom or encrypted assets should persist any
+    ///   information needed to distinguish or reconstruct the asset in `CustomData`.
     static func asset(fileUrl: URL, customData: CustomData) -> Asset
+
+    /// Converts input and metadata into player metadata.
+    ///
+    /// - Parameters:
+    ///   - input: The input that identifies the asset.
+    ///   - metadata: The metadata associated with the asset.
+    /// - Returns: Player metadata describing the asset.
+    ///
+    /// If this method is not implemented, the ``AssetLoader/playerMetadata(from:metadata:)`` implementation is used
+    /// instead. Implement this method only when the metadata displayed for a download needs to differ from the metadata
+    /// provided by the original asset loader.
     static func playerMetadata(from input: Loader.Input, metadata: Loader.Metadata?) -> PlayerMetadata
 
+    /// Returns all available download records.
     func downloadRecords() -> [DownloadRecord<Loader.Input, CustomData>]
 
+    /// Adds a download record to the store.
+    ///
+    /// - Parameters:
+    ///   - record: The record to add.
+    ///   - id: The unique identifier for the record.
     func addDownloadRecord(_ record: DownloadRecord<Loader.Input, CustomData>, forId id: String)
+
+    /// Removes a download record from the store.
+    ///
+    /// - Parameter id: The unique identifier of the record.
     func removeDownloadRecord(forId id: String)
 
+    /// Returns the download record matching the given identifier.
+    ///
+    /// - Parameter id: The unique identifier of the record.
+    /// - Returns: The matching record, if one exists.
     func downloadRecord(forId id: String) -> DownloadRecord<Loader.Input, CustomData>?
+
+    /// Updates a download record.
+    ///
+    /// - Parameters:
+    ///   - record: The updated record.
+    ///   - id: The unique identifier of the record.
     func updateDownloadRecord(_ record: DownloadRecord<Loader.Input, CustomData>, forId id: String)
 }
 
 @available(tvOS, unavailable)
 public extension AssetDownloadStore {
+    /// Default implementation. Returns a simple asset.
     static func asset(fileUrl: URL, customData: CustomData) -> Asset {
         .simple(url: fileUrl)
     }
 
+    /// Default implementation. Returns player metadata defined by the associated ``AssetLoader``.
     static func playerMetadata(from input: Loader.Input, metadata: Loader.Metadata?) -> PlayerMetadata {
         Loader.playerMetadata(from: input, metadata: metadata)
     }
@@ -100,5 +154,3 @@ extension AssetDownloadStore {
 }
 
 #endif
-
-// swiftlint:enable missing_docs
--- Sources/Player/Interfaces/AssetLoader.swift
+++ Sources/Player/Interfaces/AssetLoader.swift
@@ -14,12 +14,26 @@ public protocol AssetLoader {
     /// The metadata delivered with the asset.
     associatedtype Metadata
 
-    /// A publisher that delivers the metadata associated with a given input.
+    /// Defines a publisher that delivers metadata for the asset associated with the given input.
+    ///
+    /// - Parameter input: The input that identifies the asset.
+    /// - Returns: A publisher that delivers the metadata associated with the given input.
     static func metadataPublisher(for input: Input) -> AnyPublisher<Metadata, any Error>
 
-    /// Converts input and metadata to an asset.
+    /// Converts the given input and metadata into an asset.
+    ///
+    /// - Parameters:
+    ///   - input: The input that identifies the asset.
+    ///   - metadata: The metadata associated with the asset.
+    /// - Returns: An asset representing the content to be played. The input and metadata can be used to build custom or
+    ///   encrypted assets when required.
     static func asset(from input: Input, metadata: Metadata) -> Asset
 
-    /// Converts input and metadata to player metadata.
+    /// Converts the given input and metadata into player metadata.
+    ///
+    /// - Parameters:
+    ///   - input: The input that identifies the asset.
+    ///   - metadata: The metadata associated with the asset.
+    /// - Returns: Player metadata describing the asset.
     static func playerMetadata(from input: Input, metadata: Metadata?) -> PlayerMetadata
 }
