dfb5cda837042c068405bf0ac592e12c9c868361
Samuel Défago <defagos@gmail.com>
2026-08-31 11:45:05 +0200

    Separate implementation files

--- Sources/Player/Types/AssetMetadata.swift
+++ Sources/Player/Types/AssetMetadata.swift
@@ -8,12 +8,6 @@ import Combine
 
 // swiftlint:disable missing_docs
 
-public struct EmptyCustomData: Codable {
-    private var _reserved = ""
-}
-
-public typealias AssetPlayerMetadata = AssetMetadata<EmptyCustomData>
-
 public struct AssetMetadata<CustomData> {
     public let playerMetadata: PlayerMetadata
     public let customData: CustomData
@@ -38,65 +32,5 @@ public struct AssetMetadata<CustomData> {
 }
 
 extension AssetMetadata: Codable where CustomData: Codable {}
-
-// TODO: Hide
-public extension AssetPlayerMetadata {
-    init(playerMetadata: PlayerMetadata) {
-        self.playerMetadata = playerMetadata
-        self.customData = .init()
-    }
-}
-
-public extension AssetPlayerMetadata {
-    /// An identifier for the content.
-    var identifier: String? {
-        playerMetadata.identifier
-    }
-
-    /// The content title
-    ///
-    /// For example the name of the show which the content is associated with, if any, otherwise the name
-    /// of the episode itself.
-    var title: String? {
-        playerMetadata.title
-    }
-
-    /// A subtitle for the content.
-    ///
-    /// For example the name of the episode when a show name has been provided as title.
-    var subtitle: String? {
-        playerMetadata.subtitle
-    }
-
-    /// A description of the content.
-    var description: String? {
-        playerMetadata.description
-    }
-
-    /// The source of the image associated with the content.
-    var imageSource: ImageSource {
-        playerMetadata.imageSource
-    }
-
-    /// The content viewport.
-    var viewport: Viewport {
-        playerMetadata.viewport
-    }
-
-    /// Episode information associated with the content.
-    var episodeInformation: EpisodeInformation? {
-        playerMetadata.episodeInformation
-    }
-
-    /// Chapters associated with the content.
-    var chapters: [Chapter] {
-        playerMetadata.chapters
-    }
-
-    /// Time ranges associated with the content.
-    var timeRanges: [TimeRange] {
-        playerMetadata.timeRanges
-    }
-}
 
 // swiftlint:enable missing_docs
