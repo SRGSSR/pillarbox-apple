1e12b0d0d6f2d835c62b10616c678704210cc536
Samuel Défago <defagos@gmail.com>
2026-08-31 11:45:05 +0200

    Separate implementation files

---
+++ Sources/Player/Types/EmptyCustomData.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+// swiftlint:disable missing_docs
+
+public struct EmptyCustomData: Codable {
+    private var _reserved = ""
+}
+
+// swiftlint:enable missing_docs
