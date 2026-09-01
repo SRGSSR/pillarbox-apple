eba1a23525fb876af38ac408a97edd32d94afab1
Samuel Défago <defagos@gmail.com>
2026-09-01 14:36:58 +0200

    Improve implementation

--- Sources/Player/Types/PlayerMetadata.swift
+++ Sources/Player/Types/PlayerMetadata.swift
@@ -9,12 +9,27 @@ import Combine
 import CoreMedia
 import MediaPlayer
 
+/// Metadata associated with playback.
 public typealias PlayerMetadata = AssetMetadata<EmptyCustomData>
 
-public extension AssetMetadata where CustomData == EmptyCustomData {
+public extension PlayerMetadata {
     /// Empty metadata.
-    static let empty = Self(customData: .init())
+    static let empty = Self()
 
+    /// Creates metadata.
+    ///
+    /// - Parameters:
+    ///   - identifier: An identifier for the content.
+    ///   - title: The content title.
+    ///   - subtitle: A subtitle for the content.
+    ///   - description: A description of the content.
+    ///   - imageSource: The source of the image associated with the content.
+    ///   - viewport: The content viewport.
+    ///   - episodeInformation: Episode information associated with the content.
+    ///   - chapters: Chapter associated with the content.
+    ///   - timeRanges: Time ranges associated with the content.
+    ///
+    /// The image should usually be reasonable in size (less than 1000px wide / tall is in general sufficient).
     init(
         identifier: String? = nil,
         title: String? = nil,
