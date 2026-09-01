5986a1066b5ca14013a578ec9b09f3d08526500b
Samuel Défago <defagos@gmail.com>
2026-09-01 07:28:00 +0200

    Provide shortcuts more globally

--- Sources/Player/Types/AssetMetadata.swift
+++ Sources/Player/Types/AssetMetadata.swift
@@ -31,6 +31,58 @@ public struct AssetMetadata<CustomData> {
     }
 }
 
+public extension AssetMetadata {
+    /// An identifier for the content.
+    var identifier: String? {
+        playerMetadata.identifier
+    }
+
+    /// The content title
+    ///
+    /// For example the name of the show which the content is associated with, if any, otherwise the name
+    /// of the episode itself.
+    var title: String? {
+        playerMetadata.title
+    }
+
+    /// A subtitle for the content.
+    ///
+    /// For example the name of the episode when a show name has been provided as title.
+    var subtitle: String? {
+        playerMetadata.subtitle
+    }
+
+    /// A description of the content.
+    var description: String? {
+        playerMetadata.description
+    }
+
+    /// The source of the image associated with the content.
+    var imageSource: ImageSource {
+        playerMetadata.imageSource
+    }
+
+    /// The content viewport.
+    var viewport: Viewport {
+        playerMetadata.viewport
+    }
+
+    /// Episode information associated with the content.
+    var episodeInformation: EpisodeInformation? {
+        playerMetadata.episodeInformation
+    }
+
+    /// Chapters associated with the content.
+    var chapters: [Chapter] {
+        playerMetadata.chapters
+    }
+
+    /// Time ranges associated with the content.
+    var timeRanges: [TimeRange] {
+        playerMetadata.timeRanges
+    }
+}
+
 extension AssetMetadata: Codable where CustomData: Codable {}
 
 // swiftlint:enable missing_docs
