91ea1cd2fd86facf3f0fe22c3156f5306b093cfc
Samuel Défago <defagos@gmail.com>
2026-09-01 15:39:17 +0200

    Add comment

--- Sources/Player/Types/EmptyCustomData.swift
+++ Sources/Player/Types/EmptyCustomData.swift
@@ -7,6 +7,8 @@
 // swiftlint:disable missing_docs
 
 public struct EmptyCustomData: Equatable, Codable {
+    // Insert a dummy variable. Empty codable objects are otherwise not serialized when used with SwiftData, leading to crashes when restoring supposedly
+    // non-nil objects from storage.
     private var _reserved = ""
 }
 
