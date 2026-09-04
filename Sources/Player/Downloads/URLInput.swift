155ab55efbbba430bee690e0d77f2313483af148
Samuel Défago <defagos@gmail.com>
2026-09-04 15:51:06 +0200

    Expose properties publicly

--- Sources/Player/Downloads/URLInput.swift
+++ Sources/Player/Downloads/URLInput.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 public struct URLInput<CustomData> {
-    let url: URL
-    let metadata: AssetMetadata<CustomData>
+    public let url: URL
+    public let metadata: AssetMetadata<CustomData>
 }
 
 // swiftlint:enable missing_docs
