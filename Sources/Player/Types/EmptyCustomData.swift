68a6894376d1a9475d4af84d75b4091c334cea86
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-09-23 15:40:53 +0200

    Fix JSON decoding with empty custom data

--- Sources/Player/Types/EmptyCustomData.swift
+++ Sources/Player/Types/EmptyCustomData.swift
@@ -7,6 +7,6 @@
 /// Represents empty custom data.
 public struct EmptyCustomData: Equatable, Codable {
     // Insert a dummy variable. Empty codable objects are otherwise not serialized when used with SwiftData, leading to crashes when restoring supposedly
-    // non-nil objects from storage.
-    private var _reserved = ""
+    // non-nil objects from storage. The type is optional so that objects without this field can be decoded as well.
+    private var _reserved: String? = ""
 }
