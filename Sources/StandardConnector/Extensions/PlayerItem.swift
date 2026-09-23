57f760bfd2ff0c9cb375dd87025792696fc028b1
Walid Kayhal <3347810+waliid@users.noreply.github.com>
2026-09-23 14:40:24 +0200

    Update doc

--- Sources/StandardConnector/Extensions/PlayerItem.swift
+++ Sources/StandardConnector/Extensions/PlayerItem.swift
@@ -14,11 +14,6 @@ public extension PlayerItem {
     ///   - assetProviderType: The asset provider type.
     ///   - input: The input that identifies the asset.
     ///   - trackerAdapters: An array of `TrackerAdapter` instances to use for tracking playback events.
-    ///
-    /// This method fetches metadata from the provided endpoint and decodes it into a `PlayerData` object.
-    ///
-    /// The connector does not create the `Asset` automatically. Users must provide an `assetProvider` closure that transforms
-    /// the decoded `PlayerData` into a playable `Asset`.
     static func standard<Provider>(
         assetProviderType: Provider.Type,
         input: Provider.Input,
